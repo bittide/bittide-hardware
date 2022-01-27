15bdea1a4bd3af2ec1f2b5092d13f4b9ecd86304
Leon Schoorl <leon@qbaylogic.com>
2022-01-27 03:46:03 +0100

    Remove everything
    We'll add it back in more controlled PRs

--- .github/scripts/check_missing_eof_newline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xou pipefail
-
-pcregrep --exclude-dir=riscv-formal --exclude-dir=.git -LMr '\n\Z' .
-
-if [[ $? == 0 ]]; then
-    echo "Files without a newline end detected. See ^"
-    exit 1;
-fi
--- contranomy/tests/tests.hs
+++
@@ -1,5 +0,0 @@
-import Prelude
-import qualified Tests.Contranomy.Core
-
-main :: IO ()
-main = Tests.Contranomy.Core.main
