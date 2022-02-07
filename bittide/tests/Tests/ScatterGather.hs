c385bb0864acf159a18824c7baf0e9248652d743
lmbollen <lucas@qbaylogic.com>
2022-02-07 09:28:29 +0100

    cleanup + adding comments

--- bittide/tests/Tests/ScatterGather.hs
+++ bittide/tests/Tests/ScatterGather.hs
@@ -41,6 +41,8 @@ data SomeCalendar extra where
 instance Show (SomeCalendar extra) where
   show (SomeCalendar SNat list) = show list
 
+-- | Returns a calendar for a engine with a memory depth equal to the calendar depth,
+-- the addresses in the calendar are all unique.
 genSomeCalendar :: Gen (SomeCalendar 1)
 genSomeCalendar = do
   calendarSize <- Gen.enum 1 255
@@ -66,57 +68,82 @@ sgGroup = testGroup "Scatter Gather group"
   , testProperty "ScatterGather - Switch to PE Communication" sgSwitchToPECommunication
   , testProperty "ScatterGather - PE to Switch Communication" sgPEToSwitchCommunication]
 
-
+-- | Tests that for a calendar that contains only unique entries,
+-- all frames send to the scatter engine will appear at its output.
 sSeqNoFrameLoss :: Property
 sSeqNoFrameLoss = property $ do
   someCalendar <- forAll genSomeCalendar
+  let topEntity (unbundle -> (frameIn, calIn, newMeta)) =
+        maybeIsUndefined <$> scatterEngine newMeta frameIn calIn
   case someCalendar of
     SomeCalendar size@SNat (toList -> calendar) -> do
       inputFrames <- forAll genFrameList
-      let inputFrames' = Nothing : inputFrames P.++ P.replicate (2 * snatToNum size + 2) Nothing --Add extra cycles to collect results
-      let topEntity (unbundle -> (frameIn, calIn, newMeta)) = maybeIsUndefined <$> scatterEngine newMeta frameIn calIn
-      let topEntityInput = P.zip3 inputFrames' (cycle calendar) (cycle $ (==0) <$> [0..P.length calendar - 1])
-      let simOut = simulateN @System (P.length topEntityInput) topEntity topEntityInput
+      let
+        postFramesInput = P.replicate (2 * snatToNum size + 2) Nothing
+        inputFrames' = Nothing : inputFrames P.++ postFramesInput
+        newMetaSignal = cycle $ (==0) <$> [0..P.length calendar - 1]
+        topEntityInput = P.zip3 inputFrames' (cycle calendar) newMetaSignal
+        simOut = simulateN @System (P.length topEntityInput) topEntity topEntityInput
       footnote . fromString $ showX simOut
       footnote . fromString $ showX topEntityInput
       Set.fromList inputFrames' === Set.fromList simOut
 
+-- | Tests that for a calendar that contains only unique entries,
+-- all frames send to the gather engine will appear at its output.
 gSeqNoFrameLoss :: Property
 gSeqNoFrameLoss = property $ do
   someCalendar <- forAll genSomeCalendar
+  let topEntity (unbundle -> (frameIn, calIn, newMeta)) =
+        maybeIsUndefined <$> gatherEngine newMeta frameIn calIn
   case someCalendar of
     SomeCalendar size@SNat (toList -> calendar) -> do
       inputFrames <- forAll genFrameList
-      let inputFrames' = Nothing: inputFrames P.++ P.replicate (2 * snatToNum size + 2) Nothing --Add extra cycles to collect results
-      let topEntity (unbundle -> (frameIn, calIn, newMeta)) = maybeIsUndefined <$> gatherEngine newMeta frameIn calIn
-      let topEntityInput = P.zip3 inputFrames' (cycle calendar) (cycle $ (==0) <$> [0..P.length calendar - 1])
-      let simOut = simulateN @System (P.length topEntityInput) topEntity topEntityInput
+      let
+        postFramesInput = P.replicate (2 * snatToNum size + 2) Nothing
+        inputFrames' = Nothing : inputFrames P.++ postFramesInput
+        newMetaSignal = cycle $ (==0) <$> [0..P.length calendar - 1]
+        topEntityInput = P.zip3 inputFrames' (cycle calendar) newMetaSignal
+        simOut = simulateN @System (P.length topEntityInput) topEntity topEntityInput
       footnote . fromString $ showX simOut
       Set.fromList inputFrames' === Set.fromList simOut
 
+-- | Tests that for a calendar that contains only unique entries,
+-- all frames send from the switch side, appear at the PE side output.
 sgSwitchToPECommunication :: Property
 sgSwitchToPECommunication = property $ do
   someCalendar <- forAll genSomeCalendar
   inputData <- forAll genFrameList
   case someCalendar of
     SomeCalendar size@SNat calendar -> do
-      let inputFrames' = Nothing : inputData P.++ P.replicate (2 * snatToNum size + 2) Nothing --Add extra cycles to collect results
-      let topEntity (unbundle -> (frameIn,readAddr)) = maybeIsUndefined . snd <$> withClockResetEnable @System clockGen resetGen enableGen scatterGatherEngine calendar calendar (pure (Nothing,Nothing)) frameIn (pure Nothing) readAddr readAddr
-      let topEntityInput = P.zip inputFrames' . cycle $ toList calendar
-      let simOut = simulateN (P.length topEntityInput) topEntity topEntityInput
+      let
+        postFramesInput = P.replicate (2 * snatToNum size + 2) Nothing
+        inputData' = Nothing : inputData P.++ postFramesInput
+        topEntity (unbundle -> (frameIn,readAddr)) = maybeIsUndefined .
+          snd <$> withClockResetEnable @System clockGen resetGen enableGen
+          scatterGatherEngine calendar calendar (pure (Nothing,Nothing)) frameIn
+          (pure Nothing) readAddr readAddr
+        topEntityInput = P.zip inputData' . cycle $ toList calendar
+        simOut = simulateN (P.length topEntityInput) topEntity topEntityInput
       footnote . fromString $ showX simOut
-      Set.fromList inputFrames' === Set.fromList simOut
+      Set.fromList inputData' === Set.fromList simOut
 
+-- | Tests that for a calendar that contains only unique entries,
+-- all frames send from the PE side to a non zero address, appear at the PE side output.
 sgPEToSwitchCommunication :: Property
 sgPEToSwitchCommunication = property $ do
   someCalendar <- forAll genSomeCalendar
   inputFrames <- forAll genFrameList
   case someCalendar of
     SomeCalendar size@SNat calendar -> do
-      let inputFrames' = Nothing : inputFrames P.++ P.replicate (2 * snatToNum size + 2) Nothing --Add extra cycles to collect results
-      let topEntity (unbundle -> (frameIn,writeAddr)) = unNestMaybe . fmap maybeIsUndefined . fst <$> withClockResetEnable @System clockGen resetGen enableGen scatterGatherEngine calendar calendar (pure (Nothing,Nothing)) (pure Nothing) frameIn writeAddr writeAddr
-      let topEntityInput = P.zip inputFrames' . cycle $ toList calendar
-      let simOut = P.drop 1 $ simulateN (P.length topEntityInput) topEntity topEntityInput
-      let zeroFilteredInput = [ if addr == 0 then Nothing else frame | (frame, addr) <- topEntityInput]
+      let
+        postFramesInput = P.replicate (2 * snatToNum size + 2) Nothing
+        inputFrames' = Nothing : inputFrames P.++ postFramesInput
+        topEntity (unbundle -> (frameIn,writeAddr)) = unNestMaybe . fmap maybeIsUndefined
+          . fst <$> withClockResetEnable @System clockGen resetGen enableGen
+          scatterGatherEngine calendar calendar (pure (Nothing,Nothing)) (pure Nothing)
+          frameIn writeAddr writeAddr
+        topEntityInput = P.zip inputFrames' . cycle $ toList calendar
+        simOut = P.drop 1 $ simulateN (P.length topEntityInput) topEntity topEntityInput
+        zeroFilteredInput = [ if addr == 0 then Nothing else frame | (frame, addr) <- topEntityInput]
       footnote . fromString $ showX simOut
       Set.fromList zeroFilteredInput === Set.fromList simOut
