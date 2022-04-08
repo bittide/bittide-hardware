81a4360c628644563c2c05f4a989de0390ac1ead
lmbollen <lucas@qbaylogic.com>
2022-04-08 13:04:37 +0200

    Processed reviewer comments

--- bittide/src/Bittide/Switch.hs
+++ bittide/src/Bittide/Switch.hs
@@ -9,19 +9,27 @@ import Clash.Prelude
 
 import Bittide.Calendar
 import Bittide.ScatterGather (scatterEngine)
+import Bittide.SharedTypes
 
-type DataLink a = Maybe (BitVector a)
+-- | An index which source is selected by the crossbar, 0 selects Nothing, k selects k - 1.
 type CrossbarIndex links = Index (links+1)
+
+-- | Stores for each link, an index where the incoming frame is written to in the scatter
+-- memory and a crossbar index to select the outgoing frame.
 type CalendarEntry links memDepth = Vec links (Index memDepth, CrossbarIndex links)
+
+-- | The Calendar for the switch contains entries of CalendarEntry defined above.
+-- For more information regarding calendar datatypes see NOTE [component calendar types]
 type Calendar calDepth links memDepth = Vec calDepth (CalendarEntry links memDepth)
+
+-- | Write operation to a switch's calendar.
 type ConfigurationPort calDepth links memDepth =
   Maybe (Index calDepth, CalendarEntry links memDepth)
 
 -- | The Bittide Switch routes data from incoming to outgoing links based on a calendar.
 -- The switch consists of a crossbar, a calendar and a scatter engine for all incoming links.
--- The crossbar selects one of te scatter engine outputs for every outgoing link, index 0
--- selects a null frame (Nothing) and higher indexes select read data from the scatter engines.
--- 1, selects engine 0, 2 select engine 1, etc.
+-- The crossbar selects one of the scatter engine outputs for every outgoing link, index 0
+-- selects a null frame (Nothing) and k selects engine k - 1.
 switch ::
   forall dom links calDepth memDepth frameWidth .
   ( HiddenClockResetEnable dom
@@ -52,7 +60,8 @@ switch bootstrapCal writeCalendar streamsIn = crossBar <$> crossBarConfig <*> st
 -- therefore indexing of incoming links starts at 1 (index 1 selects incoming frame 0).
 -- Source: bittide hardware, switch logic.
 crossBar ::
-  (KnownNat links, 1 <= links) =>
+  ( KnownNat links
+  , 1 <= links) =>
   Vec links (CrossbarIndex links) ->
   -- | Source selection for each outgoing link, 0 is a null frame, links start at index 1.
   Vec links a ->
