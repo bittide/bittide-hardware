f89b20109ed4918c6ac2ca130585d72186225948
lmbollen <lucas@qbaylogic.com>
2022-05-02 12:08:54 +0200

    Run 10000 tests by default.
    https://github.com/google-research/bittide/pull/62 slipped past with just 100 tests.

--- bittide/tests/UnitTests.hs
+++ bittide/tests/UnitTests.hs
@@ -6,6 +6,7 @@ Maintainer:          devops@qbaylogic.com
 module Main where
 
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 import Tests.Calendar
 import Tests.DoubleBufferedRAM
@@ -16,5 +17,11 @@ tests :: TestTree
 tests = testGroup "Unittests"
   [calGroup, sgGroup, switchGroup, ramGroup]
 
+setDefaultHedgehogTestLimit :: HedgehogTestLimit -> HedgehogTestLimit
+setDefaultHedgehogTestLimit (HedgehogTestLimit Nothing) = HedgehogTestLimit (Just 10000)
+setDefaultHedgehogTestLimit opt = opt
+
 main :: IO ()
-main = defaultMain tests
+main = defaultMain $
+  adjustOption setDefaultHedgehogTestLimit
+  tests
