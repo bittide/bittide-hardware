de2d5aef2f0210d57650f729f3afb243c3f2d744
lmbollen <lucas@qbaylogic.com>
2022-05-03 09:03:58 +0200

    Fix sporadic CI failure (https://github.com/google-research/bittide/pull/62), Up Hedgehog test amount(https://github.com/google-research/bittide/pull/61)
    Off-by-one error in Scatter gather test
    
    https://github.com/google-research/bittide/pull/62 slipped past with just 100 tests, upped limit to 10_000.

--- bittide/tests/UnitTests.hs
+++ bittide/tests/UnitTests.hs
@@ -5,6 +5,7 @@
 module Main where
 
 import Test.Tasty
+import Test.Tasty.Hedgehog
 
 import Tests.Calendar
 import Tests.DoubleBufferedRAM
@@ -15,5 +16,11 @@ tests :: TestTree
 tests = testGroup "Unittests"
   [calGroup, sgGroup, switchGroup, ramGroup]
 
+setDefaultHedgehogTestLimit :: HedgehogTestLimit -> HedgehogTestLimit
+setDefaultHedgehogTestLimit (HedgehogTestLimit Nothing) = HedgehogTestLimit (Just 10000)
+setDefaultHedgehogTestLimit opt = opt
+
 main :: IO ()
-main = defaultMain tests
+main = defaultMain $
+  adjustOption setDefaultHedgehogTestLimit
+  tests
