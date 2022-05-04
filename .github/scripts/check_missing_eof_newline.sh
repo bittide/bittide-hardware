76f4b57197c747062810ee66ffbd3afd96433172
Lara Herzog <lara@qbaylogic.com>
2022-05-04 10:30:57 +0200

    exclude devicetree blobs from newline checks

--- .github/scripts/check_missing_eof_newline.sh
+++ .github/scripts/check_missing_eof_newline.sh
@@ -6,7 +6,7 @@
 
 set -xou pipefail
 
-pcregrep --exclude-dir=riscv-formal --exclude-dir=.git -LMr '\n\Z' .
+pcregrep --exclude-dir=riscv-formal --exclude-dir=.git --exlucde-dir=devicetree/blobs -LMr '\n\Z' .
 
 if [[ $? == 0 ]]; then
     echo "Files without a newline end detected. See ^"
