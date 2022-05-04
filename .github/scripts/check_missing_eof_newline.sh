d77a4dde5b9c405d6ed6bcb2a065fc7360e73577
Lara Herzog <lara@qbaylogic.com>
2022-05-04 10:59:29 +0200

    skip binary files in EOF-newline checks

--- .github/scripts/check_missing_eof_newline.sh
+++ .github/scripts/check_missing_eof_newline.sh
@@ -6,7 +6,7 @@
 
 set -xou pipefail
 
-pcregrep --exclude-dir=riscv-formal --exclude-dir=.git --exclude-dir=devicetree/blobs -LMr '\n\Z' .
+pcregrep --exclude-dir=riscv-formal --exclude-dir=.git --binary-files=without-match -LMr '\n\Z' .
 
 if [[ $? == 0 ]]; then
     echo "Files without a newline end detected. See ^"
