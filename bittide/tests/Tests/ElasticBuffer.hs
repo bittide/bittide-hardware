9a07ea4bc1f05e71644d52fb558d628607cce37e
Vanessa McHale <vamchale@gmail.com>
2022-07-12 11:04:31 -0400

    stability

--- bittide/tests/Tests/ElasticBuffer.hs
+++ bittide/tests/Tests/ElasticBuffer.hs
@@ -9,7 +9,7 @@ import Clash.Explicit.Prelude
 import Control.Monad (join)
 import Data.Maybe (catMaybes)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty.HUnit (testCase, (@?=), assertBool)
 import qualified Prelude as P
 
 import Bittide.ElasticBuffer
@@ -19,12 +19,13 @@ type Dom = XilinxSystem
 
 ebGroup :: TestTree
 ebGroup = testGroup "Elastic buffer"
-    [ testCase "no exceptions" $ fmap fst res @?= P.replicate 10 0xee ]
-    -- TODO: drop beginning or filter based on underflow
+    [ testCase "Contiguous once stable" $ assertBool "doesn't lose data when stable" (contiguous res) ]
 
-feedState :: [BitVector 16] -> Bool -> ([BitVector 16], BitVector 16)
-feedState (x:xs) True = (xs, x)
-feedState xs False = (xs, deepErrorX "Feed off")
+contiguous :: (Num a, Eq a) => [a] -> Bool
+contiguous xs = P.and (P.zipWith (\x y -> y == x + 1) xs (P.tail xs))
+
+feedState :: [BitVector 16] -> a -> ([BitVector 16], BitVector 16)
+feedState (x:xs) _ = (xs, x)
 
 inpSignal ::
   KnownDomain dom =>
@@ -32,18 +33,17 @@ inpSignal ::
   Reset dom ->
   Enable dom ->
   [BitVector 16] ->
-  -- | For our stall mechanism
-  Signal dom Bool ->
+  Signal dom a ->
   Signal dom (BitVector 16)
 inpSignal clk rst ena = mealy clk rst ena feedState
 
-res :: [(BitVector 16, Signed 7)]
+res :: [BitVector 16]
 res =
   catMaybes
     $ sampleN 1000
-    $ fmap (\(p0,p1,x,d) -> join (p0 `orNothing` (p1 `orNothing` (x,d))))
-    $ (\(b0, b1, ~(x,_,ou,cnt)) -> bundle (b0, b1, x, cnt))
-    $ watchEb clk rst ena (inpSignal clk rst ena [1..] (pure True)) -- TODO inpSignal doesn't respect wobble
+    $ fmap (\(p0,p1,x,_) -> join (p0 `orNothing` (p1 `orNothing` x)))
+    $ (\(b0, b1, ~(x,st,_,cnt)) -> bundle (b0 .&&. b1, st, x, cnt))
+    $ watchEb clk rst ena (inpSignal clk rst ena [1..] (pure ())) -- TODO inpSignal doesn't respect wobble
  where
   clk = clockGen
   rst = resetGen
@@ -68,11 +68,7 @@ watchEb ::
   -- OR: enables one cycle off so
 watchEb clk rst ena dat = (fromEnable ena0, fromEnable ena1,) $ elasticBuffer (SNat @7) clk rst ena0 clk rst ena1 dat
  where
-  (ena0, ena1) = onBoth toEnable $ entangledEna clk rst ena (3 :: Int) 5
-  -- FIXME: even same ratio loses data??
-  --
-  -- TODO: tbClockGen
-  -- biTbClockGen
+  (ena0, ena1) = onBoth toEnable $ entangledEna clk rst ena (83 :: Int) 89
 
 orNothing :: Bool -> a -> Maybe a
 orNothing False _ = Nothing
