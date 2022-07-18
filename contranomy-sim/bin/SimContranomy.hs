815426941a85ed014285d2290bcc33773e3b9574
Lara Herzog <lara@qbaylogic.com>
2022-07-18 16:46:17 +0200

    make device tree loading less wasteful

--- contranomy-sim/bin/SimContranomy.hs
+++ contranomy-sim/bin/SimContranomy.hs
@@ -7,6 +7,7 @@ import           Clash.Prelude
 import           Contranomy
 import           ContranomySim.Print
 import           ContranomySim.ReadElf
+import           ContranomySim.MemoryMapConsts
 
 import qualified Data.ByteString       as BS
 import qualified Data.IntMap           as I
@@ -18,16 +19,16 @@ main = do
   elfBytes <- BS.readFile "main.elf"
   let (entry, iMem, dMem) = readElfFromMemory elfBytes
 
-  -- add device tree as a memory mapped component in 0x1000_0000
+  -- add device tree as a memory mapped component
 
   -- TODO read the device tree file from command line args?
-  deviceTree <- BS.readFile "../devicetree/blobs/contranomy-sim.dtb"
+  deviceTreeRaw <- BS.readFile "../devicetree/blobs/contranomy-sim.dtb"
   -- add padding to prevent uninitialised accesses
-  let padding = L.replicate (4 - (BS.length deviceTree `mod` 4)) 0
-  let dMem' = dMem
-               `I.union` I.fromList (L.zip [0x10000000..] $ pack <$> BS.unpack deviceTree)
-               `I.union` I.fromList (L.zip [(0x10000000 + fromIntegral (BS.length deviceTree))..] padding)
+  let padding = L.replicate (4 - (BS.length deviceTreeRaw `mod` 4)) 0
+      deviceTree = fmap pack . BS.unpack $ deviceTreeRaw <> BS.pack padding
 
-  -- Hook up to print-debugging at special address 0x70000000
-  hookPrint 0x70000000 $ sample $ fmap snd $
+  let dMem' = dMem `I.union` I.fromAscList (L.zip [fdtAddr..] deviceTree)
+
+  -- Hook up to print-debugging at uts designated address
+  hookPrint characterDeviceAddr $ sample $ fmap snd $
     contranomy' hasClock hasReset entry iMem dMem' $ pure (False, False, 0b0)
---
+++ contranomy-sim/src/ContranomySim/MemoryMapConsts.hs
@@ -0,0 +1,26 @@
+-- SPDX-FileCopyrightText: 2022 Google LLC
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+{- | Address-constants for memory mapped components.
+
+     Ideally a device tree configuration would be the "source of truth"
+     for both the consuming firmware and the providing simulation.
+     However, there currently is no published package on Hackage for reading
+     FDTs and maintaining an FDT parser is outside of the scope of this project.
+
+     As a "workaround" all the address constants are kept here in a central
+     module.
+-}
+
+{-# LANGUAGE NumericUnderscores #-}
+module ContranomySim.MemoryMapConsts where
+
+import           Clash.Prelude (Unsigned)
+import           Data.IntMap   (Key)
+
+fdtAddr :: Key
+fdtAddr = 0x1000_0000
+
+characterDeviceAddr :: Unsigned 32
+characterDeviceAddr = 0x7000_0000
