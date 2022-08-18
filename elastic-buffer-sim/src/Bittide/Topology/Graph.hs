b44ce9e1cbabbb5ae0885364aa32a26662982c34
Vanessa McHale <vamchale@gmail.com>
2022-08-18 08:01:48 -0400

    use depth explicitly (ambiguous)

--- elastic-buffer-sim/src/Bittide/Topology/Graph.hs
+++ elastic-buffer-sim/src/Bittide/Topology/Graph.hs
@@ -18,13 +18,13 @@ import Data.Tuple (swap)
 diamond :: Graph
 diamond = A.listArray (0, 3) [[1,3], [0,2,3], [2,3], [0,1,2]]
 
--- | Tree of depth @d@ with @c@ children
+-- | Tree of depth @depth@ with @c@ children
 tree :: Int -> Int -> Graph
-tree d c = treeGraph
+tree depth c = treeGraph
  where
   -- | At depth @d_i@, child node @i@ is connected to the @(i-1) `div` c + 1@st
   -- node at depth @d_i - 1@
-  pairs = [ (d_i, i, (i-1) `div` c + 1) | d_i <- [0..d], i <- [1..(c^d_i)] ]
+  pairs = [ (d_i, i, (i-1) `div` c + 1) | d_i <- [0..depth], i <- [1..(c^d_i)] ]
   mkEdges (0, _, _)           = Nothing
   mkEdges (lvl, node, p_node) = Just ((lvl, node), (lvl-1, p_node))
   directedEdges = mapMaybe mkEdges pairs
