40f7b91aed061aedbc7972f12741cb882c5643a9
Vanessa McHale <vamchale@gmail.com>
2022-08-24 07:19:19 -0400

    python code style

--- elastic-buffer-sim/genplots.py
+++ elastic-buffer-sim/genplots.py
@@ -5,17 +5,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-nodes=range(0, 5)
+nodes = range(0, 5)
 
 # generate data from dumpCsv
-dfs=[pd.read_csv(f'_build/clocks{i}.csv') for i in nodes]
+dfs = [pd.read_csv(f'_build/clocks{i}.csv') for i in nodes]
 
 for df in dfs:
     df.set_index('t', inplace=True)
 
-df=pd.concat(dfs,axis=0)
+df = pd.concat(dfs, axis=0)
 
-eb_names=[f'eb{i}{j}' for i in nodes for j in nodes if j != i]
+eb_names = [f'eb{i}{j}' for i in nodes for j in nodes if j != i]
 
 df[eb_names].plot()
 plt.xlabel('Time (ps)')
@@ -23,7 +23,7 @@ plt.ylabel('Elastic buffer occupancy')
 
 plt.savefig('_build/elasticbuffers.pdf')
 
-clk_names=[f'clk{i}' for i in nodes]
+clk_names = [f'clk{i}' for i in nodes]
 df[clk_names].plot()
 plt.xlabel('Time (ps)')
 plt.ylabel('Period')
