c42640247a17f2608f9f627e4d7d73401a4827b0
lmbollen <lucas@qbaylogic.com>
2022-12-07 11:42:56 +0100

    Add copyright

--- bittide/src/Bittide/ClockControl/ClockGenConfig.hs
+++ bittide/src/Bittide/ClockControl/ClockGenConfig.hs
@@ -1,3 +1,6 @@
+-- SPDX-FileCopyrightText: 2022 Google LLC
+--
+-- SPDX-License-Identifier: Apache-2.0
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NamedFieldPuns #-}
