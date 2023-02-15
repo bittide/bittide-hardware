adc5a30177d229b2ecc1eea28a226b77dcdd1058
Lucas Bollen <lucas@qbaylogic.com>
2023-02-15 17:20:43 +0100

    Add `fromListWithResetAndEnable` (#253)

--- bittide/tests/Tests/Shared.hs
+++ bittide/tests/Tests/Shared.hs
@@ -11,20 +11,22 @@ module Tests.Shared where
 import Clash.Prelude
 
 import Clash.Hedgehog.Sized.Unsigned
+import Clash.Signal.Internal
 
 import Data.Constraint (Dict(Dict))
 import Data.Constraint.Nat.Extra (divWithRemainder)
+import Data.List.NonEmpty (NonEmpty)
 import GHC.Stack (HasCallStack)
 import Hedgehog
+import Numeric.Natural
 import Protocols (toSignals)
 import Protocols.Wishbone as Wb
 import Protocols.Wishbone.Standard.Hedgehog (validatorCircuit)
-import Numeric.Natural
 
 import Bittide.Calendar
 import Bittide.SharedTypes (Bytes)
-
 import qualified Data.List as L
+import qualified Data.List.NonEmpty as NonEmpty
 import qualified GHC.TypeNats as TypeNats
 import qualified Hedgehog.Range as Range
 
@@ -231,3 +233,37 @@ wcre = withClockResetEnable clockGen resetGen enableGen
 -- | Make any @a@ into a non-repeating `ValidEntry` without repetition bits.
 nonRepeatingEntry :: a -> ValidEntry a 0
 nonRepeatingEntry a = ValidEntry{veEntry = a, veRepeat = 0}
+
+-- | Converts a list of elements into a signal of elements. Unlike 'fromList'
+-- it also takes a clock, reset, and enable. When the reset is asserted, it
+-- will [insert a reset value / insert the first value of the given list]. When
+-- the enable is deasserted, elements are repeated. As usual, the reset takes
+-- precedence over the enable.
+--
+-- __NB__: Not translatable to HDL
+fromListWithResetAndEnable ::
+  forall dom a .
+  (HiddenClockResetEnable dom, NFDataX a, Show a) =>
+  NonEmpty a ->
+  Signal dom a
+fromListWithResetAndEnable inp =
+  go
+    (fromEnable hasEnable)
+    (unsafeToHighPolarity hasReset)
+    inpAsSignal
+
+ where
+  eolError = deepErrorX "fromListWithResetAndEnable: end of list"
+  inpAsSignal = fromList (NonEmpty.toList inp <> L.repeat eolError)
+
+  go :: Signal dom Bool -> Signal dom Bool -> Signal dom a -> Signal dom a
+  go (ena :- enables) (rst :- resets) (x :- xs) =
+    output :- go enables resets remaining
+   where
+    output
+      | rst       = NonEmpty.head inp
+      | otherwise = x
+    remaining
+      | rst       = inpAsSignal
+      | ena       = xs
+      | otherwise = x :- xs
