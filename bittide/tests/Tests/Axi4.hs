b3cae1fe6b076b672125bcec8929361f4a6a6e88
lmbollen <lucas@qbaylogic.com>
2023-02-20 10:17:05 +0100

    fix after rebase

--- bittide/tests/Tests/Axi4.hs
+++ bittide/tests/Tests/Axi4.hs
@@ -225,7 +225,7 @@ wbAxisTxBufferWriteStreams = property $ do
       footnote . fromString $ "wbOps:" <> show wbOps
       footnote . fromString $ "axisM2SList:" <> show axisM2SList
       footnote . fromString $ "axisS2MList:" <> show axisS2MList
-      L.concat packets === axis4ToPackets axisM2SList axisS2MList
+      packets === axis4ToPackets axisM2SList axisS2MList
 
 wbAxisRxBufferReadStreams :: Property
 wbAxisRxBufferReadStreams = property $ do
