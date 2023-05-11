3f16de41355946ef65fcdcf9fe2f9f809ce7f0f7
lmbollen <lucas@qbaylogic.com>
2023-05-11 13:26:38 +0200

    Add some available constraint to empty `Dict`s.
    We used to `unsafeCoerce` empty `Dict`s to provide
    constraints that we know are true.
    However, using an empty `Dict` can cause GHC to optimize it away, resulting in errors.

--- bittide/src/Data/Constraint/Nat/Extra.hs
+++ bittide/src/Data/Constraint/Nat/Extra.hs
@@ -26,7 +26,7 @@ import Unsafe.Coerce
 
 -- | b <= ceiling(b/a)*a
 timesDivRU :: forall a b . (1 <= a) => Dict (b <= (Div (b + (a - 1)) a * a))
-timesDivRU = unsafeCoerce (Dict :: Dict (a ~ a))
+timesDivRU = unsafeCoerce (Dict :: Dict (1 <= a))
 {-# NOINLINE timesDivRU #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
 
 -- | Implements logarithmic product rule. Currently hardcoded for specific
@@ -38,7 +38,7 @@ clogProductRule = unsafeCoerce Refl
 -- | Postulates that multiplying some number /a/ by some constant /b/, and
 -- subsequently dividing that result by /b/ equals /a/.
 cancelMulDiv :: forall a b . (1 <= b) => Dict (DivRU (a * b) b ~ a)
-cancelMulDiv = unsafeCoerce (Dict :: Dict (a ~ a))
+cancelMulDiv = unsafeCoerce (Dict :: Dict (1 <= b))
 {-# NOINLINE cancelMulDiv #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
 
 -- | Postulates that adding a constant less than the denominator does not
@@ -47,7 +47,7 @@ divWithRemainder ::
   forall a b c.
   (1 <= b, c <= (b - 1)) =>
   Dict (Div ((a * b) + c) b ~ a)
-divWithRemainder = unsafeCoerce (Dict :: Dict (a ~ a))
+divWithRemainder = unsafeCoerce (Dict :: Dict (1 <= b, c <= (b - 1)))
 {-# NOINLINE divWithRemainder #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
 
 -- | Postulates that a part is less than or equal to a sum parts, in context
@@ -58,7 +58,7 @@ leMaxLeft = unsafeCoerce (Dict :: Dict (a ~ a))
 
 -- | If @c <= a@ and @c <= b@, then @c <= Max a b@
 lessThanMax :: forall a b c . (c <= a, c <= b) => Dict (c <= Max a b)
-lessThanMax = unsafeCoerce (Dict :: Dict (a ~ a))
+lessThanMax = unsafeCoerce (Dict :: Dict (c <= a, c <= b))
 
 -- | Postulates that a part is less than or equal to a sum parts, in context
 -- of 'Max's right argument.
@@ -68,21 +68,21 @@ leMaxRight = unsafeCoerce (Dict :: Dict (a ~ a))
 
 -- | if (1 <= a) and (1 <= b) then (1 <= DivRU a b)
 strictlyPositiveDivRu :: forall a b . (1 <= a, 1 <= b) => Dict (1 <= DivRU a b)
-strictlyPositiveDivRu = unsafeCoerce (Dict :: Dict (a ~ a))
+strictlyPositiveDivRu = unsafeCoerce (Dict :: Dict (1 <= a, 1 <= b))
 {-# NOINLINE strictlyPositiveDivRu #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
 
 -- | Euclid's third axiom: /If equals be subtracted from equals, the remainders
 -- are equal/.
 euclid3 :: forall a b c . (a + b <= c) => Dict (a <= c - b)
-euclid3 = unsafeCoerce (Dict :: Dict (a ~ a))
+euclid3 = unsafeCoerce (Dict :: Dict (a + b <= c))
 {-# NOINLINE euclid3 #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
 
 -- | if (2 <= n) holds, then (1 <= CLog 2 n) also holds.
 oneLeCLog2n :: forall n . (2 <= n) => Dict (1 <= CLog 2 n)
-oneLeCLog2n = unsafeCoerce unsafeCoerce (Dict :: Dict (a ~ a))
+oneLeCLog2n = unsafeCoerce unsafeCoerce (Dict :: Dict (2 <= n))
 {-# NOINLINE oneLeCLog2n #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
 
 -- | If @1 <= m@ and @n + m <= u@, then @1 + n <= u@
 useLowerLimit :: forall n m u . (1 <= m, n + m <= u) => Dict (1 + n <= u)
-useLowerLimit = unsafeCoerce (Dict :: Dict (a ~ a))
+useLowerLimit = unsafeCoerce (Dict :: Dict (1 <= m, n + m <= u))
 {-# NOINLINE useLowerLimit #-} -- https://github.com/clash-lang/clash-compiler/issues/2376
