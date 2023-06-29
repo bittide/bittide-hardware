b070c2019f1ab0f823a6f6f2490d1ff9082137d3
Martijn Bastiaan <martijn@qbaylogic.com>
2023-06-29 12:20:00 +0200

    Move Vivado dependencies from runtime to docker image
    Will increase the maximum throughput of 'hoeve'

--- .github/scripts/with_vivado.sh
+++ .github/scripts/with_vivado.sh
@@ -13,14 +13,7 @@ source /opt/tools/Xilinx/VivadoEnterprise/Vivado/2022.1/settings64.sh
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1
 
 # Vivado insists on using en_US.UTF-8
-apt-get update
-apt-get install language-pack-en-base -y
-locale-gen en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-update-locale
-
-# Vivado dependencies
-apt-get install libtinfo5 libncurses5 libx11-6 -y
 
 # Run command given as argument
 $@
