cf0260fb5e838140e600102b12f508aed0f72544
Lara Herzog <lara@qbaylogic.com>
2023-10-02 10:31:10 +0200

    Squashed 'clash-vexriscv/' changes from c5a5e8b4..3cb69b4d
    3cb69b4d make BlackBox more robust, fix index error
    
    git-subtree-dir: clash-vexriscv
    git-subtree-split: 3cb69b4da5e853f8e7679b8808fa355453d396f1

--- clash-vexriscv/src/VexRiscv.hs
+++ clash-vexriscv/src/VexRiscv.hs
@@ -317,7 +317,11 @@ vexRiscv# !_sourcePath !_clk rst0
 {-# ANN vexRiscv# (
     let
       primName = 'vexRiscv#
-      ( _
+
+      
+      (
+       -- ARGs
+       _
        :> srcPath
        :> clk
        :> rst
@@ -333,10 +337,9 @@ vexRiscv# !_sourcePath !_clk rst0
        :> jtag_TMS
        :> jtag_TDI
        :> jtag_TCK
-       :> Nil
-       ) = indicesI @16
 
-      (   iBus_CYC
+       -- GENSYMs
+       :> iBus_CYC
        :> iBus_STB
        :> iBus_WE
        :> iBus_ADR
@@ -354,10 +357,10 @@ vexRiscv# !_sourcePath !_clk rst0
        :> dBus_BTE
        :> debug_resetOut
        :> jtag_TDO
-       :> Nil
-       ) = (\x -> extend @_ @18 @16 x + 1) <$> indicesI @18
 
-      cpu = extend @_ @_ @1 dBus_BTE + 1
+       :> cpu
+       :> Nil
+       ) = indicesI
     in
       InlineYamlPrimitive [Verilog] [__i|
   BlackBox:
