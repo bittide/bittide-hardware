6c35d233212e1be44ba0a6a5ee79e242ec6e4555
Martijn Bastiaan <martijn@qbaylogic.com>
2024-04-27 22:42:06 +0200

    Remove kernel bug workarounds
    Doesn't apply anymore to our current GHC version (9.4)

--- bittide-experiments/tests/doctests.hs
+++ bittide-experiments/tests/doctests.hs
@@ -5,17 +5,10 @@
 module Main (main) where
 
 import Test.DocTest (mainFromCabal)
-import System.Environment (getArgs, lookupEnv, setEnv)
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  -- Workaround for kernel bugs
-  ghcRts0 <- lookupEnv "GHCRTS"
-  let
-    ghcRts1 = "-xp -xm20000000"
-    ghcRts2 = maybe ghcRts1 (\s -> s <> " " <> ghcRts1) ghcRts0
-  setEnv "GHCRTS" ghcRts2
-
   -- We use Nix to setup tooling, not to provide GHC packages so we need to set --no-nix
   args <- getArgs
   mainFromCabal "bittide-experiments" ("--no-nix":args)
