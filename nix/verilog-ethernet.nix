1493260cb5467f5f8a2d6d70724f78f45fdeb2b4
Lucas Bollen <lucas@qbaylogic.com>
2024-07-18 11:05:57 +0200

    Add patches `verilog-ethernet` version
    This removes a non-existing parameter from an ddr primitive
    instantiation.
    See https://github.com/alexforencich/verilog-ethernet/pull/214

--- nix/verilog-ethernet.nix
+++ nix/verilog-ethernet.nix
@@ -2,11 +2,23 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "verilog-ethernet";
+  src = verilog-ethernet-patches;
 
-  src = pkgs.fetchgit {
+  verilog-ethernet-src = pkgs.fetchgit {
     url = "https://github.com/alexforencich/verilog-ethernet.git";
     rev = "baac5f8d811d43853d59d69957975ead8bbed088";
     sha256 = "sha256-rxoUHjOxxQc/JjEp06vibCJ2OIWbsbEtnkqS1gS+A7g=";
+  };
+
+  verilog-ethernet-patches = pkgs.applyPatches {
+    name = "verilog-ethernet-patched";
+    src = verilog-ethernet-src;
+    patches = [
+      (pkgs.fetchpatch {
+        url = "https://github.com/lmbollen/verilog-ethernet/commit/f762edd71dfebc129dacac64ff5cd5fbf7d67801.patch";
+        hash = "sha256-hKHsNViPInDKzOP3OwMZFlQ0RjYYTFPRu6rOJF1g0hQ=";
+      })
+    ];
   };
 
   installPhase = ''
