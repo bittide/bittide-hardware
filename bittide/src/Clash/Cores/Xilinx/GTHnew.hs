24513978a590a20a7db8113fcecfbfc272751b5c
Leon Schoorl <leon@qbaylogic.com>
2024-09-05 16:56:25 +0200

    Create duplicate GTH for now

---
+++ bittide/src/Clash/Cores/Xilinx/GTHnew.hs
@@ -0,0 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Google LLC
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+module Clash.Cores.Xilinx.GTHnew (module X) where
+
+import Clash.Cores.Xilinx.GTHnew.Internal as X
