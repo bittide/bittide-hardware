5f5fd466b0561acc8ba56745ce92c12287f68375
Martijn Bastiaan <martijn@qbaylogic.com>
2024-09-06 21:18:30 +0200

    Allow setting "only capture on schedueled"

--- bittide-instances/src/Bittide/Instances/Hitl/IlaPlot.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/IlaPlot.hs
@@ -250,6 +250,8 @@ data IlaControl dom = IlaControl
   -- ^ Synchronized pulse counter
   , skipTest :: Signal dom Bool
   -- ^ Skip this test
+  , onlyScheduledCaptures :: Bool
+  -- ^ Only capture measurements on scheduled intervals, not on data changes
   }
 
 -- | Names of the additional ILA plot probes.
@@ -273,6 +275,8 @@ ilaPlotSetup ::
   IlaControl dom
 ilaPlotSetup IlaPlotSetup{..} = IlaControl{..}
  where
+  onlyScheduledCaptures = False
+
   -- 'syncOutGenerator' is used to drive 'SYNC_OUT'.
   syncOut =
     dflipflop sysClk
@@ -604,6 +608,7 @@ callistoClockControlWithIla dynClk clk rst ccc IlaControl{..} mask ebs =
     let captureType calibrate scheduled dc dat
           | scheduled && calibrate = Just (Calibrate, dat)
           | scheduled = Just (Scheduled, dat)
+          | onlyScheduledCaptures = Nothing
           | dc || dataChange dat && not calibrate = Just (DataChange, dat)
           | otherwise = Nothing
 
