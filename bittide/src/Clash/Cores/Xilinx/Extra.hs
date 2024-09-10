ef3055efc8baa49c9ae23b429a80e3e35416a504
Leon Schoorl <leon@qbaylogic.com>
2024-09-10 14:40:53 +0200

    Add BUFG_GT primitive

--- bittide/src/Clash/Cores/Xilinx/Extra.hs
+++ bittide/src/Clash/Cores/Xilinx/Extra.hs
@@ -6,12 +6,15 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Clash.Cores.Xilinx.Extra (
+  bufgGt,
   ibufds,
   readDnaPortE2I,
   module GTH,
 
   -- * Internal
+  unsafeBufgGt,
   ibufdsTF,
+  unsafeBufgGtTF,
 ) where
 
 import Clash.Prelude
@@ -97,3 +100,65 @@ ibufdsTF = TemplateFunction used valid go
 
             pure [ibufdsOut]
   go bbCtx = error ("ibufdsTemplate:\n\n" <> ppShow bbCtx)
+
+
+{- | Clock Buffer Driven by Gigabit Transceiver. For more information see:
+
+    https://docs.xilinx.com/r/en-US/ug974-vivado-ultrascale-libraries/BUFG_GT
+
+The actual divide value is the value provide in @SNat div@ plus 1.
+So and @SNat 0@ gives you a division of
+-}
+bufgGt :: (KnownDomain domIn, KnownDomain domOut, 0 <= div, div <= 7) => SNat div -> Clock domIn -> Reset domIn -> Clock domOut
+bufgGt = unsafeBufgGt
+
+unsafeBufgGt :: (KnownDomain domOut) => SNat div -> Clock domIn -> Reset domIn -> Clock domOut
+unsafeBufgGt !_ !_ !_ = clockGen
+{-# ANN unsafeBufgGt hasBlackBox #-}
+{-# OPAQUE unsafeBufgGt #-}
+{-# ANN
+  unsafeBufgGt
+  ( InlineYamlPrimitive
+      [Verilog]
+      [__i|
+  BlackBox:
+    name: Clash.Cores.Xilinx.Extra.unsafeBufgGt
+    kind: Declaration
+    format: Haskell
+    templateFunction: Clash.Cores.Xilinx.Extra.unsafeBufgGtTF
+  |]
+  )
+  #-}
+
+{- | Template function for 'unsafeBufgGt'.
+
+TODO: Upstream to @clash-cores@
+-}
+unsafeBufgGtTF :: TemplateFunction
+unsafeBufgGtTF = TemplateFunction used valid go
+ where
+  used = [1, 2, 3]
+  valid = const True
+
+  go :: (Backend s) => BlackBoxContext -> State s Doc
+  go bbCtx
+    | [_knownDomOut, div0, clk, clr] <- P.map fst (DSL.tInputs bbCtx)
+    =
+        do
+          instLabel <- Id.makeBasic "bufgGt_inst"
+
+          DSL.declarationReturn bbCtx "bufgGt_block" $ do
+            bufgGtOut <- DSL.declare "bufgGt_out" Bit
+            -- div1 <- DSL.pureToBVResized "div" 3 div0 -- TODO resize is currently VHDL only
+            let
+              compName = "BUFG_GT"
+              compInps = [("I", Bit), ("CLR", Bit), ("DIV", BitVector 3), ("CE", Bit), ("CEMASK", Bit), ("CLRMASK", Bit)]
+              compOuts = [("O", Bit)]
+              inps = [("I", clk), ("CLR", clr), ("DIV", div0), ("CE", DSL.High), ("CEMASK", DSL.Low), ("CLRMASK", DSL.Low)]
+              outs = [("O", bufgGtOut)]
+
+            DSL.compInBlock compName compInps compOuts
+            DSL.instDecl Empty (Id.unsafeMake compName) instLabel [] inps outs
+
+            pure [bufgGtOut]
+  go bbCtx = error ("bufgGunsafeBTemplate:\n\n" <> ppShow bbCtx)
