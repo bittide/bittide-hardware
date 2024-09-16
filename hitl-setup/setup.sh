fe915faa85b9ebedec1578c5a37b5965e5df3b9a
Peter Lebbing <peter@qbaylogic.com>
2024-09-16 17:03:21 +0200

    Avoid @set -e@ for anything but simple commands

--- hitl-setup/setup.sh
+++ hitl-setup/setup.sh
@@ -4,13 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 FORCE="$1"
-set -exuo pipefail
+set -xuo pipefail
 HERE=$(realpath $(dirname "$0"))
-source "$HERE/fpganet"
+source "$HERE/fpganet" || exit $?
 
 echo Checking that all INTERFACES exist
 for INTERFACE in $INTERFACES; do
-  ip addr show $INTERFACE > /dev/null || exit 1
+  ip addr show $INTERFACE > /dev/null || exit $?
 done
 echo All INTERFACES exist
 
@@ -28,6 +28,7 @@ if [ -n "$EXISTS"  -a  "$FORCE" = "-f" ]; then
   exit 1
 fi
 
+set -e
 apt-get install -y isc-dhcp-server iproute2
 
 cp "$HERE/fpganet.service" /etc/systemd/system
