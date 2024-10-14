73ba6180840a529a36bb0029cd9508814ca1a0b4
Ryan Slawson <ryan@qbaylogic.com>
2024-10-14 18:28:48 +0200

    Making ILA plotting generic in preparation for SwCc.

--- bittide-instances/src/Bittide/Instances/Hitl/FullMeshHwCc.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/FullMeshHwCc.hs
@@ -301,6 +301,7 @@ fullMeshHwTest refClk sysClk IlaControl{syncRst = rst, ..} rxNs rxPs miso =
       sysClk
       clockControlReset
       clockControlConfig
+      callistoClockControl
       IlaControl{..}
       availableLinkMask
       (fmap (fmap resize) domainDiffs)
