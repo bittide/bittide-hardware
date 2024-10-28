ad721a0f4cc876f1b1de667ae75bfe43bf4609d4
Ryan Slawson <ryan@qbaylogic.com>
2024-10-28 11:11:15 +0100

    Handle types with type arguments.

--- bittide/src/Clash/Signal/TH/Extra.hs
+++ bittide/src/Clash/Signal/TH/Extra.hs
@@ -11,14 +11,37 @@ import GHC.Records (HasField (..))
 import Language.Haskell.TH
 
 {- | Derive instances of 'HasField' for a Signals of Records:
+
 Example:
+
+@
 data MyRecord = MyRecord { field1 :: Int, field2 :: Bool }
 deriveSignalHasFields ''MyRecord
+@
+
 This will generate the following instances:
+
+@
 instance HasField "field1" (Signal dom MyRecord) (Signal dom Int) where
   getField = fmap field1
 instance HasField "field2" (Signal dom MyRecord) (Signal dom Bool) where
   getField = fmap field2
+@
+
+The record type may also take type arguments. For instance, this type:
+
+@
+data MyRecord n = MyRecord { field1 :: BitVector n, field2 :: BitVector n (Unsigned 8) }
+@
+
+Will generate the following instances:
+
+@
+instance HasField "field1" (Signal dom (MyRecord n)) (Signal dom (BitVector n)) where
+  getField = fmap field1
+instance HasField "field2" (Signal dom (MyRecord n)) (Signal dom (Vec n (Unsigned 8))) where
+  getField = fmap field2
+@
 -}
 deriveSignalHasFields :: Name -> Q [Dec]
 deriveSignalHasFields recordName = do
@@ -26,13 +49,22 @@ deriveSignalHasFields recordName = do
   info <- reify recordName
   case info of
     -- If the name refers to a record type, generate instances for each field
-    TyConI (DataD _ _ _ _ [RecC _ fields] _) -> do
+    TyConI (DataD _ _ tvb _ [RecC _ fields] _) -> do
+      runIO $ mapM_ print tvb
       let
         recordType = conT recordName
+        recordTyInputs = varT . go <$> tvb
+         where
+          go (PlainTV n _) = n
+          go (KindedTV n _ _) = n
+        recordAppT = go recordType recordTyInputs
+         where
+          go ty [] = ty
+          go ty (h : t) = go (appT ty h) t
         mkInstance :: (Name, Bang, Type) -> Q [Dec]
         mkInstance (fieldName, _fieldBang, fieldType) =
           [d|
-            instance HasField $fieldStr (Signal dom $recordType) (Signal dom $(pure fieldType)) where
+            instance HasField $fieldStr (Signal dom $recordAppT) (Signal dom $(pure fieldType)) where
               getField = fmap $(varE fieldName)
             |]
          where
