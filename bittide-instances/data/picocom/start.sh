ddd94ff98914d89d45269747bd2af6a3657ade67
Ryan Slawson <ryan@qbaylogic.com>
2024-11-19 14:56:14 +0100

    Tee Picocom stdout and stderr to files and archive them.

--- bittide-instances/data/picocom/start.sh
+++ bittide-instances/data/picocom/start.sh
@@ -8,4 +8,18 @@
 #
 #    /dev/serial/by-id/usb-FTDI_Dual_RS232-if01-port0
 #
-picocom  --baud 921600 --imap lfcrlf --omap lfcrlf $@
+set -e
+
+# Default stdout to /dev/null
+PICOCOM_STDOUT_LOG="${PICOCOM_STDOUT_LOG:-/dev/null}"
+stdout_dir=$(dirname "${PICOCOM_STDOUT_LOG}")
+mkdir -p "${stdout_dir}"
+
+# Default stderr to /dev/null
+PICOCOM_STDERR_LOG="${PICOCOM_STDERR_LOG:-/dev/null}"
+stderr_dir=$(dirname "${PICOCOM_STDERR_LOG}")
+mkdir -p "${stderr_dir}"
+
+picocom  --baud 921600 --imap lfcrlf --omap lfcrlf $@ \
+  > >(tee "${PICOCOM_STDOUT_LOG}") \
+  2> >(tee "${PICOCOM_STDERR_LOG}" >&2)
