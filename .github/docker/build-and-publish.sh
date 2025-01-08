dcdc0e36d660061d0034ac8752c683d5cc2aa7c1
Lucas Bollen <lucas@qbaylogic.com>
2025-01-08 11:33:07 +0100

    docker `build-and-publish.sh` clean up copied files

--- .github/docker/build-and-publish.sh
+++ .github/docker/build-and-publish.sh
@@ -13,12 +13,17 @@ TODAY="$(date +%F)"
 
 cd "${HERE}"
 
+# Copy files for caching purposes
 cp "${ROOT}/default.nix" .
 cp "${ROOT}/shell.nix" .
 cp "${ROOT}/rust-toolchain.toml" .
 cp -ap "${ROOT}/nix" .
 
 docker build -t "${REPO}/${NAME}:$TODAY" -t "${REPO}/${NAME}:latest" .
+
+# Clean up copied files
+rm default.nix shell.nix rust-toolchain.toml
+rm -rf nix
 
 read -p "Push to GitHub? (y/N) " push
 
