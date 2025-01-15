3710074f3efd62c65dd17904765aa4248752a981
Hidde Moll <hidde@qbaylogic.com>
2025-01-15 16:33:43 +0100

    Extend scatter gather test to echo second array

--- firmware-binaries/test-cases/scatter_gather_test/src/main.rs
+++ firmware-binaries/test-cases/scatter_gather_test/src/main.rs
@@ -26,30 +26,39 @@ fn main() -> ! {
     let scatter_unit: ScatterUnit<MEM_SIZE> = unsafe { ScatterUnit::new(SCATTER_ADDR) };
     let gather_unit: GatherUnit<MEM_SIZE> = unsafe { GatherUnit::new(GATHER_ADDR) };
 
-    // For this test an array of incrementing values is written to the full gather
-    // memory. The calendars for the scatter and gather units are expected to write the
+    // The calendars for the scatter and gather units are expected to write the
     // data sent over the link to the same memory location in the scatter unit. This makes
     // this test behave like an 'echo'. The scatter and gather memories are double
     // buffered, and their buffers are swapped at the end of each metacycle, which is the
-    // duration of a calendar.
-    // The test takes 3 metacycles:
-    //   1: CPU writes to the gather unit memory
-    //   2: Gather memory is written over the link to the scatter memory
-    //   3: Read from scatter memory
+    // duration of a calendar. For this test two halves of an array of incrementing values
+    // are written to the gather memory. These two halves should be read back from the
+    // scatter memory.
+    // The test takes 4 metacycles:
+    //   1: Write first halve of the array to the gather memory.
+    //   2: Write second halve of the array to the gather memory, while first halve is
+    //      send over the link.
+    //   3: Read first halve of array from the scatter memory, while second halve is
+    //      send over the link.
+    //   3: Read second halve of array from scatter memory.
 
-    let source: [u64; MEM_SIZE] = core::array::from_fn(|i| i as u64);
-    let mut destination: [u64; MEM_SIZE] = [0; MEM_SIZE];
+    let source: [u64; MEM_SIZE * 2] = core::array::from_fn(|i| i as u64);
+    let mut destination: [u64; MEM_SIZE * 2] = [99; MEM_SIZE * 2];
 
-    // First metacycle
+    // First metacycle: write to gather A, link sends gather B
     gather_unit.wait_for_new_metacycle();
-    gather_unit.write_slice(&source, 0);
+    gather_unit.write_slice(&source[..MEM_SIZE], 0);
 
-    // Second metacycle
+    // Second metacycle, write to gather B, link sends gather A
     gather_unit.wait_for_new_metacycle();
+    gather_unit.write_slice(&source[MEM_SIZE..], 0);
 
-    // Third metacycle
-    gather_unit.wait_for_new_metacycle();
-    scatter_unit.read_slice(destination.as_mut(), 0);
+    // Third metacycle: read from scatter A
+    scatter_unit.wait_for_new_metacycle();
+    scatter_unit.read_slice(destination[..MEM_SIZE].as_mut(), 0);
+
+    // Fourth metacycle: read from scatter B
+    scatter_unit.wait_for_new_metacycle();
+    scatter_unit.read_slice(destination[MEM_SIZE..].as_mut(), 0);
 
     // Check if slices are equal
     if source == destination {
