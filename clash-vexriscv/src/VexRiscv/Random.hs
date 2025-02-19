1fe63fc9daf22a298f171a4fd251577bd5b0b140
Lucas Bollen <lucas@qbaylogic.com>
2025-02-19 09:24:35 +0100

    Squashed 'clash-vexriscv/' changes from 2eced423..da3b01a2
    da3b01a2 Set cache path to Cabal store set in `shell.nix` (#65)
    3f9758a1 Check whether Nix shell works on CI (#61)
    9363f8d4 Merge pull request #63 from clash-lang/improve-expect-functions
    79c5e34a Add timeout to expect functions
    ed39928d Add call stack to `waitForLine`
    f1d76ead Use `tail` instead of `streaming` to follow files (#64)
    e86580bf Remove enable from `cpu` (#58)
    c69899c2 Drop register on wishbone bus (#59)
    1715fc73 Print `JTAG bridge ready` and wait for it to prevent race conditions (#57)
    1ca5a69d Merge pull request #53 from clash-lang/add-fourmolu
    5e11a6f6 Add `.git-blame-ignore-revs`
    2bd141e6 Add fourmolu formatting
    96ad7244 Use `HasCallStack` instead of manual source line tracking (#52)
    3900cb07 Bump OpenOCD (#54)
    3a26b8bd Correcetly simulate passed time before first clock edge in verilator (#50)
    51239127 Add post fetch step that removes `.git` (#48)
    ca418fcb Merge pull request #47 from clash-lang/lucas/move-stoptime-ndmreset
    27c82690 Move `ndmreset` and `stoptime` from `JtagOut` to `CpuOut`
    68886c88 Merge pull request #46 from clash-lang/run-clang-format
    a32dbc98 Add `clang-format-check` to ci
    68ca4d0e Format with `clang-format`
    9a676aa6 Add `clang` to nix shell
    0d4be0e0 Merge pull request #45 from clash-lang/lucas/bump-jtag-interface
    61c7719d Bump GHC versions on Docker images and rebuild
    5f9a53c9 Add `.scala-build` to `.gitignore`
    2c1ec3a3 Remove `openocd-vexriscv` from `nix` and `docker`
    deabc085 Switch to `openocd-vexriscv` and use `remote-bitbang`
    d987e396 Replace `DebugPlugin` with `EmbeddedRiscvJtag`
    59298429 Add `nix` derivation for `openocv-riscv`
    2dfda13f Add `.metals` to `.gitignore`
    554f4af1 Bump scala and spinal version
    720e5d44 Add execute permissions to `update-vexriscv.pu`
    ad6edb75 Merge pull request #44 from clash-lang/lucas/add-docker-readme
    636ac968 Extend readme with CI info
    ac10433d Bump GHA actions (#43)
    59215117 Merge pull request #42 from clash-lang/add-all-check
    e697bfdd Fix typo: ouput -> output
    e7e8e29c Add all check to CI
    
    git-subtree-dir: clash-vexriscv
    git-subtree-split: da3b01a2fc91f55cec7fa33c1f07f88631d02ecf

--- clash-vexriscv/src/VexRiscv/Random.hs
+++ clash-vexriscv/src/VexRiscv/Random.hs
@@ -17,14 +17,16 @@ import Data.Bifunctor (bimap)
 import GHC.IO (unsafePerformIO)
 import System.Random
 
--- | Unsafe version of 'makeDefinedRandom' with a NOINLINE pragma to avoid
--- compiler optimizations.
-unsafeMakeDefinedRandom :: DefinedRandom a => a -> a
+{- | Unsafe version of 'makeDefinedRandom' with a NOINLINE pragma to avoid
+compiler optimizations.
+-}
+unsafeMakeDefinedRandom :: (DefinedRandom a) => a -> a
 unsafeMakeDefinedRandom = unsafePerformIO . makeDefinedRandom
 {-# NOINLINE unsafeMakeDefinedRandom #-}
 
--- | A class for types that can be (partially) undefined whose undefined values
--- can be replaced with defined random values.
+{- | A class for types that can be (partially) undefined whose undefined values
+can be replaced with defined random values.
+-}
 class DefinedRandom a where
   makeDefinedRandom :: a -> IO a
 
@@ -38,14 +40,14 @@ instance DefinedRandom Bit where
     | hasUndefined b = Bit 0 <$> randomRIO (0, 1)
     | otherwise = pure b
 
-instance KnownNat n => DefinedRandom (BitVector n) where
-  makeDefinedRandom :: KnownNat n => BitVector n -> IO (BitVector n)
+instance (KnownNat n) => DefinedRandom (BitVector n) where
+  makeDefinedRandom :: (KnownNat n) => BitVector n -> IO (BitVector n)
   makeDefinedRandom bv@(ensureSpine -> BV mask dat)
     | mask == 0 = pure bv
-    | otherwise =  do
-    let maxVal = natToNum @(2^n - 1)
-    randomInt <- genNatural (0, fromIntegral maxVal)
-    pure $ BV 0 (((maxVal `xor` mask) .&. dat) .|. (mask .&. randomInt))
+    | otherwise = do
+        let maxVal = natToNum @(2 ^ n - 1)
+        randomInt <- genNatural (0, fromIntegral maxVal)
+        pure $ BV 0 (((maxVal `xor` mask) .&. dat) .|. (mask .&. randomInt))
 
 -- | Generate a random natural number in the given inclusive range.
 genNatural :: (Natural, Natural) -> IO Natural
