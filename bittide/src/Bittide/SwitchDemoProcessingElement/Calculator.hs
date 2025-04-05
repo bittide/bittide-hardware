ea5e7c2fa8787e71110848094a715b7b4f8cb02b
Ryan Slawson <ryan@qbaylogic.com>
2025-04-05 22:45:04 +0200

    Add pretty-printing function for CI usage, and add in off-by-one fix to calculator.
    Co-authored-by: Martijn Bastiaan <martijn@qbaylogic.com>

--- bittide/src/Bittide/SwitchDemoProcessingElement/Calculator.hs
+++ bittide/src/Bittide/SwitchDemoProcessingElement/Calculator.hs
@@ -3,16 +3,21 @@
 -- SPDX-License-Identifier: Apache-2.0
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 module Bittide.SwitchDemoProcessingElement.Calculator where
 
 import Clash.Prelude
 
+import Clash.Functor.Extra ((<<$>>))
+import Control.Monad (forM_)
 import Data.Bifunctor (Bifunctor (bimap))
 import Data.Map (Map)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromJust)
+import Data.String.Interpolate (i)
 import GHC.Stack (HasCallStack)
 
+import qualified Data.List as L
 import qualified Data.Map as Map
 
 type FpgaId = String
@@ -20,6 +25,10 @@ type FpgaId = String
 -- TODO: find a way to derive this 4 from the code instead of using a magic number.
 iNTERNAL_SWITCH_DELAY :: (Num a) => a
 iNTERNAL_SWITCH_DELAY = 4
+
+-- TODO: find out why this is necessary
+dELAY_FROM_EB_TO_CROSSBAR :: (Num a) => a
+dELAY_FROM_EB_TO_CROSSBAR = 1
 
 {- | Convert a table (fpga_nr x link_nr) to a vector of maps mapping an fpga index
 (instead of a link index) to the value.
@@ -172,8 +181,7 @@ chainConfigurationWorker ::
   Integer ->
   Vec chainLength (PeConfig Integer Integer)
 chainConfigurationWorker fpgaConfig ugnParts writeOffset cyclesPerWrite =
-  id
-    $ leToPlus @1 @nNodes
+  leToPlus @1 @nNodes
     $ leToPlus @1 @chainLength
     $ leToPlusKN @chainLength @nNodes
     $ let
@@ -218,7 +226,7 @@ chainConfigurationWorker fpgaConfig ugnParts writeOffset cyclesPerWrite =
     linkSelectOffset = offsetsByFpga !! dst Map.! src
     fullCalendarsSeen = dstCycle `quot` calendarLength
     prevCalendarStart = fullCalendarsSeen * calendarLength
-    linkStart0 = prevCalendarStart + linkSelectOffset
+    linkStart0 = prevCalendarStart + linkSelectOffset + dELAY_FROM_EB_TO_CROSSBAR
     linkStart1 = linkStart0 + calendarLength
 
   calendarEntryLength = cyclesPerWrite * natToNum @nNodes
@@ -229,3 +237,100 @@ chainConfigurationWorker fpgaConfig ugnParts writeOffset cyclesPerWrite =
 
   offsetsByFpga :: (KnownNat nNodes, 1 <= nNodes) => Vec nNodes (Map (Index nNodes) Integer)
   offsetsByFpga = toFpgaIndexed fpgaConfig offsets
+
+{- | Prints the measured IGNs in a matrix-like format.
+
+For example:
+
+@
+xxxx,   67, 1299,   68,   69,   69,   69,   70
+  67, xxxx,   80,   69,   69,   69,   70,   70
+1299,   80, xxxx,   69,   69,   69,   69,   69
+  68,   69,   69, xxxx,   70,   70,   69,   69
+  69,   69,   69,   70, xxxx,   69,   69,   69
+  69,   69,   69,   70,   69, xxxx,   68,   70
+  69,   70,   69,   69,   69,   68, xxxx,   69
+  70,   70,   69,   69,   69,   70,   69, xxxx
+@
+-}
+printAllIgns ::
+  forall n m l.
+  ( HasCallStack
+  , KnownNat m
+  , KnownNat n
+  , KnownNat l
+  , l + 1 ~ (n + 1) * m
+  ) =>
+  Vec (n + 1) (Vec m (Unsigned 64, Unsigned 64)) ->
+  Vec (n + 1) (FpgaId, Vec m (Index (n + 1))) ->
+  IO ()
+printAllIgns ugnPairsTable fpgasTable = do
+  let
+    ugnsTable :: Vec (n + 1) (Vec m (Unsigned 64))
+    ugnsTable = uncurry (-) <<$>> ugnPairsTable
+    fpgasMap :: Vec (n + 1) (Vec m (Index (n + 1)))
+    fpgasMap = snd <$> fpgasTable
+    indicesM :: Vec m (Index m)
+    indicesM = indicesI
+    indicesN :: Vec (n + 1) (Index (n + 1))
+    indicesN = indicesI
+    printIgns ::
+      (HasCallStack) =>
+      Index (n + 1) ->
+      (FpgaId, Vec m (Index (n + 1))) ->
+      IO (Vec m (Unsigned 64))
+    printIgns rowI (myId, row) = do
+      putStrLn [i|Finding IGNs for #{myId} (#{rowI})|]
+      putStrLn [i|  My connections: #{row}|]
+      let
+        printIgn :: (HasCallStack) => Index m -> Index (n + 1) -> IO (Unsigned 64)
+        printIgn colI otherRowI = do
+          putStrLn [i|  Looking at column #{colI} => #{otherRowI}|]
+          let
+            otherRow :: Vec m (Index (n + 1))
+            otherRow = fpgasMap !! otherRowI
+            otherRowName :: FpgaId
+            otherRowName = fst $ fpgasTable !! otherRowI
+          putStrLn [i|    Row name: #{otherRowName}|]
+          putStrLn [i|    Row contents: #{otherRow}|]
+          let
+            myIndex :: Index m
+            myIndex = fromJust $ elemIndex rowI otherRow
+          putStrLn [i|    Other row has me at index: #{myIndex}|]
+          let
+            myUgn = ugnsTable !! rowI !! colI
+            otherUgn = ugnsTable !! otherRowI !! myIndex
+            ign = myUgn + otherUgn
+          putStrLn [i|  The IGN for this link is: #{myUgn} + #{otherUgn} = #{ign}|]
+          return ign
+      igns <- sequence $ zipWith printIgn indicesM row
+      putStrLn [i|IGNs for FPGA #{myId} (#{rowI}): #{igns}|]
+      return igns
+
+  igns <- sequence $ zipWith printIgns indicesN fpgasTable
+  let
+    allList :: Vec (l + 1) (Unsigned 64)
+    allList = concat igns
+    largest :: Unsigned 64
+    largest = maximum allList
+    largestLen :: Int
+    largestLen = L.length $ show largest
+    coords :: Vec (n + 1) (Vec (n + 1) (Index (n + 1), Index (n + 1)))
+    coords = (\ind -> (,) ind <$> indicesN) <$> indicesN
+    printIgn :: Index (n + 1) -> Index (n + 1) -> IO ()
+    printIgn row fpgaNum = do
+      let
+        fpgaNumI = fromIntegral fpgaNum :: Integer
+        col = fromJust $ findIndex ((==) fpgaNumI . fromIntegral) (fpgasMap !! row)
+        ign = igns !! row !! col
+        str =
+          if (fromIntegral row :: Integer) == fpgaNumI
+            then L.replicate largestLen 'x'
+            else show ign
+        pad = L.replicate (largestLen - L.length str) ' '
+      if fpgaNum == maxBound
+        then putStrLn [i|#{pad}#{str}|]
+        else putStr [i|#{pad}#{str}, |]
+
+  putStrLn "Final IGNs table:"
+  forM_ coords $ mapM_ (uncurry printIgn)
