341d579bda9411fdc4caf85f0e48a06882c8bd64
Hidde Moll <hidde@qbaylogic.com>
2025-05-21 11:03:20 +0200

    Improve tabular format of domains

--- bittide-instances/src/Bittide/Instances/Domains.hs
+++ bittide-instances/src/Bittide/Instances/Domains.hs
@@ -12,23 +12,23 @@ import Bittide.ClockControl
 import Data.Proxy
 
 {- ORMOLU_DISABLE -}
-createDomain vXilinxSystem{vName="Basic50", vPeriod= hzToPeriod 50e6}
-createDomain vXilinxSystem{vName="Basic125", vPeriod= hzToPeriod 125e6}
-createDomain vXilinxSystem{vName="Basic125A", vPeriod= hzToPeriod 125e6}
-createDomain vXilinxSystem{vName="Basic125B", vPeriod= hzToPeriod 125e6}
-createDomain vXilinxSystem{vName="Basic199", vPeriod=hzToPeriod 199e6}
-createDomain vXilinxSystem{vName="Basic200", vPeriod=hzToPeriod 200e6}
-createDomain vXilinxSystem{vName="Basic625", vPeriod=hzToPeriod 625e6, vResetKind=Asynchronous}
+createDomain vXilinxSystem{vName="Basic50",   vPeriod=hzToPeriod  50e6}
+createDomain vXilinxSystem{vName="Basic125",  vPeriod=hzToPeriod 125e6}
+createDomain vXilinxSystem{vName="Basic125A", vPeriod=hzToPeriod 125e6}
+createDomain vXilinxSystem{vName="Basic125B", vPeriod=hzToPeriod 125e6}
+createDomain vXilinxSystem{vName="Basic199",  vPeriod=hzToPeriod 199e6}
+createDomain vXilinxSystem{vName="Basic200",  vPeriod=hzToPeriod 200e6}
+createDomain vXilinxSystem{vName="Basic625",  vPeriod=hzToPeriod 625e6, vResetKind=Asynchronous}
 
-createDomain vXilinxSystem{vName="Ext125", vPeriod= hzToPeriod 125e6, vResetKind=Asynchronous}
-createDomain vXilinxSystem{vName="Ext200", vPeriod=hzToPeriod 200e6, vResetKind=Asynchronous}
+createDomain vXilinxSystem{vName="Ext125",    vPeriod=hzToPeriod 125e6, vResetKind=Asynchronous}
+createDomain vXilinxSystem{vName="Ext200",    vPeriod=hzToPeriod 200e6, vResetKind=Asynchronous}
 
-createDomain vXilinxSystem{vName="GthRx", vPeriod=hzToPeriod 125e6}
-createDomain vXilinxSystem{vName="GthTx", vPeriod= hzToPeriod 125e6}
-createDomain vXilinxSystem{vName="GthRx1", vPeriod= hzToPeriod 250e6}
-createDomain vXilinxSystem{vName="GthTx1", vPeriod= hzToPeriod 250e6}
-createDomain vXilinxSystem{vName="GthRxS", vPeriod=hzToPeriod 10e9}
-createDomain vXilinxSystem{vName="GthTxS", vPeriod= hzToPeriod 10e9}
+createDomain vXilinxSystem{vName="GthRx",     vPeriod=hzToPeriod 125e6}
+createDomain vXilinxSystem{vName="GthTx",     vPeriod=hzToPeriod 125e6}
+createDomain vXilinxSystem{vName="GthRx1",    vPeriod=hzToPeriod 250e6}
+createDomain vXilinxSystem{vName="GthTx1",    vPeriod=hzToPeriod 250e6}
+createDomain vXilinxSystem{vName="GthRxS",    vPeriod=hzToPeriod  10e9}
+createDomain vXilinxSystem{vName="GthTxS",    vPeriod=hzToPeriod  10e9}
 {- ORMOLU_ENABLE -}
 
 type CccBufferSize = 25 :: Nat
