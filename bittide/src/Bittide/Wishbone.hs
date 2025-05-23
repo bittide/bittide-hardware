df3fdceea66b424b91e65e65dfbe96f1909b7d6e
Martijn Bastiaan <martijn@qbaylogic.com>
2025-05-23 16:25:13 +0200

    Lessen space leaks in interconnects
    Doesn't fully solve the space leaks, but that's for another day..

--- bittide/src/Bittide/Wishbone.hs
+++ bittide/src/Bittide/Wishbone.hs
@@ -132,7 +132,10 @@ singleMasterInterconnect (fmap pack -> config) =
   go (masterS, slavesS) =
     fmap unbundle . unbundle $ route <$> masterS <*> bundle slavesS
 
-  route master@(WishboneM2S{..}) slaves = (toMaster, toSlaves)
+  route master@(WishboneM2S{..}) slaves =
+    ( strictV slaves `seqX` strictV toSlaves `seqX` toMaster
+    , toSlaves
+    )
    where
     oneHotOrZeroSelected = fmap (== addrIndex) config
     (addrIndex, newAddr) =
@@ -146,6 +149,11 @@ singleMasterInterconnect (fmap pack -> config) =
             emptyWishboneS2M{err = True} -- master tries to access unmapped memory
             (maskToMaybes slaves oneHotOrZeroSelected)
       | otherwise = emptyWishboneS2M
+
+  strictV :: Vec m b -> Vec m b
+  strictV v
+    | clashSimulation = foldl (\b a -> a `seqX` b) () v `seqX` v
+    | otherwise = v
 
 dupWb ::
   forall dom aw.
