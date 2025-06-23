ad68f25529a3feaf7a1a3db10b973cad3c2fd1ad
Hidde Moll <hidde@qbaylogic.com>
2025-06-23 13:44:06 +0200

    Use specialized domains for ddr4 primitive

--- bittide/src/Clash/Cores/Xilinx/Ddr4.hs
+++ bittide/src/Clash/Cores/Xilinx/Ddr4.hs
@@ -3,6 +3,8 @@
 -- SPDX-License-Identifier: Apache-2.0
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# OPTIONS_GHC -Wno-ambiguous-fields #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Clash.Cores.Xilinx.Ddr4 where
 
@@ -53,56 +55,60 @@ data Ddr4MemorySignals = Ddr4MemorySignals
   , c0_ddr4_reset_n :: Bit
   }
 
+-- Since these domains are _generated_ by the DDR4 primitive it makes sense to declare
+-- them here.
+createDomain vXilinxSystem{vName = "Ddr800", vPeriod = 1250}
+createDomain vXilinxSystem{vName = "Ddr200", vPeriod = hzToPeriod 200e6}
+
 {- BiSignals don't work in record types, which is why we have to write out all inputs and
 outputs instead.
 The input clock _must_ be `sys_clock_300`. We cannot constrain this exactly due to the
 approximation in `DomainToHz`.
 -}
 ddr4Axi ::
-  forall uiDom sysDom.
-  ( KnownDomain uiDom
-  , KnownDomain sysDom
-  , 290_000_000 <= DomainToHz sysDom
-  , DomainToHz sysDom <= 310_000_000
+  forall refDom.
+  ( KnownDomain refDom
+  , 299_000_000 <= DomainToHz refDom
+  , DomainToHz refDom <= 301_000_000
   ) =>
   -- | Must be sysclk_300
-  "sys_clock_300" ::: DiffClock sysDom ->
+  "sys_clock_300" ::: DiffClock refDom ->
   -- | Resets the entire memory design.
-  "sys_rst" ::: Reset sysDom ->
+  "sys_rst" ::: Reset Ddr800 ->
   -- Routed from our own designs
-  "axi_shim_reset" ::: Reset uiDom ->
-  "axi4_m2s_write_address" ::: Signal uiDom (M2S_WriteAddress ConfAW ()) ->
-  "axi4_m2s_write_data" ::: Signal uiDom (M2S_WriteData ConfW ()) ->
-  "axi4_m2s_write_response" ::: Signal uiDom M2S_WriteResponse ->
-  "axi4_m2s_read_address" ::: Signal uiDom (M2S_ReadAddress ConfAR ()) ->
-  "axi4_m2s_read_data" ::: Signal uiDom M2S_ReadData ->
+  "axi_shim_reset" ::: Reset Ddr200 ->
+  "axi4_m2s_write_address" ::: Signal Ddr200 (M2S_WriteAddress ConfAW ()) ->
+  "axi4_m2s_write_data" ::: Signal Ddr200 (M2S_WriteData ConfW ()) ->
+  "axi4_m2s_write_response" ::: Signal Ddr200 M2S_WriteResponse ->
+  "axi4_m2s_read_address" ::: Signal Ddr200 (M2S_ReadAddress ConfAR ()) ->
+  "axi4_m2s_read_data" ::: Signal Ddr200 M2S_ReadData ->
   -- Routed directly from the memory
-  "c0_ddr4_dm_dbi_n" ::: BiSignalIn 'Floating sysDom 1 ->
-  "c0_ddr4_dq" ::: BiSignalIn 'Floating sysDom 8 ->
-  "c0_ddr4_dqs_t" ::: BiSignalIn 'Floating sysDom 1 ->
-  "c0_ddr4_dqs_c" ::: BiSignalIn 'Floating sysDom 1 ->
+  "c0_ddr4_dm_dbi_n" ::: BiSignalIn 'Floating Ddr800 1 ->
+  "c0_ddr4_dq" ::: BiSignalIn 'Floating Ddr800 8 ->
+  "c0_ddr4_dqs_t" ::: BiSignalIn 'Floating Ddr800 1 ->
+  "c0_ddr4_dqs_c" ::: BiSignalIn 'Floating Ddr800 1 ->
   ( -- Routed to our own designs
-    "c0_ddr4_ui_clk" ::: Clock uiDom
+    "c0_ddr4_ui_clk" ::: Clock Ddr200
   , -- \^ Output clock from the user interface. Must be a quarter of the frequency of
     -- the clock going out to the external SDRAM.
-    "c0_ddr4_ui_clk_sync_rst" ::: Reset uiDom
+    "c0_ddr4_ui_clk_sync_rst" ::: Reset Ddr200
   , -- \^ @sys_rst@ is internally synchronized to @ui_clk@
-    "c0_init_calib_complete" ::: Signal uiDom Bool
-  , "axi4_s2m_write_address" ::: Signal uiDom S2M_WriteAddress
-  , "axi4_s2m_write_data" ::: Signal uiDom S2M_WriteData
-  , "axi4_s2m_write_response" ::: Signal uiDom (S2M_WriteResponse ConfB ())
-  , "axi4_s2m_read_address" ::: Signal uiDom S2M_ReadAddress
+    "c0_init_calib_complete" ::: Signal Ddr200 Bool
+  , "axi4_s2m_write_address" ::: Signal Ddr200 S2M_WriteAddress
+  , "axi4_s2m_write_data" ::: Signal Ddr200 S2M_WriteData
+  , "axi4_s2m_write_response" ::: Signal Ddr200 (S2M_WriteResponse ConfB ())
+  , "axi4_s2m_read_address" ::: Signal Ddr200 S2M_ReadAddress
   , "axi4_s2m_read_data"
-      ::: Signal uiDom (S2M_ReadData ConfR () (BitVector C_S_AXI_DATA_WIDTH))
+      ::: Signal Ddr200 (S2M_ReadData ConfR () (BitVector C_S_AXI_DATA_WIDTH))
   , -- Routed directly to the memory
-    "ddr4_to_memory" ::: Signal sysDom Ddr4MemorySignals
-  , "c0_ddr4_dm_dbi_n_o" ::: BiSignalOut 'Floating sysDom 1
-  , "c0_ddr4_dq_o" ::: BiSignalOut 'Floating sysDom 8
-  , "c0_ddr4_dqs_t_o" ::: BiSignalOut 'Floating sysDom 1
-  , "c0_ddr4_dqs_c_o" ::: BiSignalOut 'Floating sysDom 1
+    "ddr4_to_memory" ::: Signal Ddr800 Ddr4MemorySignals
+  , "c0_ddr4_dm_dbi_n_o" ::: BiSignalOut 'Floating Ddr800 1
+  , "c0_ddr4_dq_o" ::: BiSignalOut 'Floating Ddr800 8
+  , "c0_ddr4_dqs_t_o" ::: BiSignalOut 'Floating Ddr800 1
+  , "c0_ddr4_dqs_c_o" ::: BiSignalOut 'Floating Ddr800 1
   )
 ddr4Axi
-  sysClkDiff
+  refClkDiff
   sysRst
   axi_rst
   m2s_wa
@@ -137,7 +143,7 @@ ddr4Axi
       rst200 = resetGen
       calibrationComplete = pure False
 
-      ddr4_sigs :: Signal sysDom Ddr4MemorySignals
+      ddr4_sigs :: Signal Ddr800 Ddr4MemorySignals
       ddr4_sigs = pure (Ddr4MemorySignals 0 0 0 0 0 0 0 0 0 0)
 
       c0_ddr4_dm_dbi_n_o = writeToBiSignal @Bit c0_ddr4_dm_dbi_n (pure Nothing)
@@ -234,56 +240,56 @@ ddr4Axi
 {- ORMOLU_DISABLE -}
     go ::
       ( -- DDR4
-        Port "c0_ddr4_adr"     sysDom (BitVector 17)
-      , Port "c0_ddr4_ba"      sysDom (BitVector 2)
-      , Port "c0_ddr4_bg"      sysDom Bit
-      , Port "c0_ddr4_cke"     sysDom Bit
-      , Port "c0_ddr4_odt"     sysDom Bit
-      , Port "c0_ddr4_cs_n"    sysDom Bit
-      , Port "c0_ddr4_ck_t"    sysDom Bit
-      , Port "c0_ddr4_ck_c"    sysDom Bit
-      , Port "c0_ddr4_act_n"   sysDom Bit
-      , Port "c0_ddr4_reset_n" sysDom Bit
+        Port "c0_ddr4_adr"     Ddr800 (BitVector 17)
+      , Port "c0_ddr4_ba"      Ddr800 (BitVector 2)
+      , Port "c0_ddr4_bg"      Ddr800 Bit
+      , Port "c0_ddr4_cke"     Ddr800 Bit
+      , Port "c0_ddr4_odt"     Ddr800 Bit
+      , Port "c0_ddr4_cs_n"    Ddr800 Bit
+      , Port "c0_ddr4_ck_t"    Ddr800 Bit
+      , Port "c0_ddr4_ck_c"    Ddr800 Bit
+      , Port "c0_ddr4_act_n"   Ddr800 Bit
+      , Port "c0_ddr4_reset_n" Ddr800 Bit
 
         -- DDR4 BiSignal parts.
         -- These are currently not used since BiSignalOutPort causes a mismatch
         -- in number of output ports. See:
         -- https://github.com/clash-lang/clash-cores/issues/35
-        -- , BiSignalOutPort "c0_ddr4_dm_dbi_n_o" 'Floating sysDom 1
-        -- , BiSignalOutPort "c0_ddr4_dq_o"       'Floating sysDom 8
-        -- , BiSignalOutPort "c0_ddr4_dqs_t_o"    'Floating sysDom 1
-        -- , BiSignalOutPort "c0_ddr4_dqs_c_o"    'Floating sysDom 1
+        -- , BiSignalOutPort "c0_ddr4_dm_dbi_n_o" 'Floating Ddr800 1
+        -- , BiSignalOutPort "c0_ddr4_dq_o"       'Floating Ddr800 8
+        -- , BiSignalOutPort "c0_ddr4_dqs_t_o"    'Floating Ddr800 1
+        -- , BiSignalOutPort "c0_ddr4_dqs_c_o"    'Floating Ddr800 1
 
         -- General ports
-      , ClockPort "c0_ddr4_ui_clk"    uiDom
-      , ResetPort "c0_ddr4_ui_clk_sync_rst" ActiveHigh uiDom
+      , ClockPort "c0_ddr4_ui_clk"    Ddr200
+      , ResetPort "c0_ddr4_ui_clk_sync_rst" ActiveHigh Ddr200
       , -- | Indicates that initialization and calibration are complete (active-high)
-        Port "c0_init_calib_complete" uiDom Bool
+        Port "c0_init_calib_complete" Ddr200 Bool
 
         -- AXI Write Address ports
-      , Port "c0_ddr4_s_axi_awready"  uiDom Bool
+      , Port "c0_ddr4_s_axi_awready"  Ddr200 Bool
         -- AXI Write Data ports
-      , Port "c0_ddr4_s_axi_wready"   uiDom Bool
+      , Port "c0_ddr4_s_axi_wready"   Ddr200 Bool
         -- AXI Write Response ports
-      , Port "c0_ddr4_s_axi_bid"      uiDom (BitVector C_S_AXI_ID_WIDTH)
-      , Port "c0_ddr4_s_axi_bresp"    uiDom (BitVector 2)
-      , Port "c0_ddr4_s_axi_bvalid"   uiDom Bool
+      , Port "c0_ddr4_s_axi_bid"      Ddr200 (BitVector C_S_AXI_ID_WIDTH)
+      , Port "c0_ddr4_s_axi_bresp"    Ddr200 (BitVector 2)
+      , Port "c0_ddr4_s_axi_bvalid"   Ddr200 Bool
         -- AXI Read Address ports
-      , Port "c0_ddr4_s_axi_arready"  uiDom Bool
+      , Port "c0_ddr4_s_axi_arready"  Ddr200 Bool
         -- AXI Read Data ports
-      , Port "c0_ddr4_s_axi_rid"      uiDom (BitVector C_S_AXI_ID_WIDTH)
-      , Port "c0_ddr4_s_axi_rdata"    uiDom (BitVector C_S_AXI_DATA_WIDTH)
-      , Port "c0_ddr4_s_axi_rresp"    uiDom (BitVector 2)
-      , Port "c0_ddr4_s_axi_rlast"    uiDom Bool
-      , Port "c0_ddr4_s_axi_rvalid"   uiDom Bool
+      , Port "c0_ddr4_s_axi_rid"      Ddr200 (BitVector C_S_AXI_ID_WIDTH)
+      , Port "c0_ddr4_s_axi_rdata"    Ddr200 (BitVector C_S_AXI_DATA_WIDTH)
+      , Port "c0_ddr4_s_axi_rresp"    Ddr200 (BitVector 2)
+      , Port "c0_ddr4_s_axi_rlast"    Ddr200 Bool
+      , Port "c0_ddr4_s_axi_rvalid"   Ddr200 Bool
 
         -- Debug ports
       , -- | Debug Clock. Do not connect any signals to dbg_clk and keep the port
         -- open during instantiation. Automatically connects to the debug hub logic.
-        ClockPort "dbg_clk"           uiDom
+        ClockPort "dbg_clk"           Ddr200
       , -- | Reserved. Do not connect any signals to dbg_bus and keep the port
         -- open during instantiation.
-        Port "dbg_bus"                uiDom (BitVector 512)
+        Port "dbg_bus"                Ddr200 (BitVector 512)
       )
     go =
       instWithXilinxWizard
@@ -295,19 +301,20 @@ ddr4Axi
           , wiz_version = "2.2"
           , wiz_options =
                  ("CONFIG.C0_CLOCK_BOARD_INTERFACE", StrOpt "default_sysclk_300")
-              :> ("CONFIG.C0.DDR4_TimePeriod",       IntegerOpt 1000)
-              :> ("CONFIG.C0.DDR4_InputClockPeriod", IntegerOpt 8000)
-              :> ("CONFIG.C0.DDR4_CLKOUT0_DIVIDE",   IntegerOpt 5)
+              :> ("CONFIG.C0.DDR4_TimePeriod",       IntegerOpt 1250)
+              :> ("CONFIG.C0.DDR4_InputClockPeriod", IntegerOpt 3333)
+              :> ("CONFIG.C0.DDR4_CLKOUT0_DIVIDE",   IntegerOpt 7)
               :> ("CONFIG.C0.DDR4_MemoryPart",       StrOpt "MT40A256M16GE-083E")
               :> ("CONFIG.C0.DDR4_AxiSelection",     BoolOpt True)
-              :> ("CONFIG.C0.DDR4_CasLatency",       IntegerOpt 15)
+              :> ("CONFIG.C0.DDR4_CasLatency",       IntegerOpt 11)
               :> ("CONFIG.C0.DDR4_CasWriteLatency",  IntegerOpt 11)
               :> ("CONFIG.C0.DDR4_AxiDataWidth",     IntegerOpt (natToNum @C_S_AXI_DATA_WIDTH))
               :> ("CONFIG.C0.DDR4_AxiAddressWidth",  IntegerOpt (natToNum @C_S_AXI_ADDR_WIDTH))
               :> ("CONFIG.C0.BANK_GROUP_WIDTH",      IntegerOpt 1)
+              :> ("CONFIG.System_Clock",             StrOpt "Differential")
               :> Nil
           }
-        (DiffClockPort @"c0_sys_clk" sysClkDiff)
+        (DiffClockPort @"c0_sys_clk" refClkDiff)
         (ResetPort @"sys_rst" @ActiveHigh sysRst)
 
         -- DDR4 ports
@@ -320,13 +327,13 @@ ddr4Axi
         (ResetPort @"c0_ddr4_aresetn" @ActiveLow axi_rst)
         (Port @"c0_ddr4_s_axi_awid"    ((._awid) <$> m2s_wa))
         (Port @"c0_ddr4_s_axi_awaddr"  ((._awaddr) <$> m2s_wa))
-        (Port @"c0_ddr4_s_axi_awlen"   (pure 0 :: Signal uiDom (BitVector 8)))
-        (Port @"c0_ddr4_s_axi_awsize"  (pure (pack Bs1) :: Signal uiDom (BitVector 3)))
-        (Port @"c0_ddr4_s_axi_awburst" (pure (pack BmFixed) :: Signal uiDom (BitVector 2)))
+        (Port @"c0_ddr4_s_axi_awlen"   (pure 0 :: Signal Ddr200 (BitVector 8)))
+        (Port @"c0_ddr4_s_axi_awsize"  (pure (pack Bs1) :: Signal Ddr200 (BitVector 3)))
+        (Port @"c0_ddr4_s_axi_awburst" (pure (pack BmFixed) :: Signal Ddr200 (BitVector 2)))
         (Port @"c0_ddr4_s_axi_awlock"  (pack . (._awlock) <$> m2s_wa)) -- Not used in the current implementation according to pg150.
         (Port @"c0_ddr4_s_axi_awcache" (pack . (._awcache) <$> m2s_wa)) -- Not used in the current implementation according to pg150.
         (Port @"c0_ddr4_s_axi_awprot"  (pack . (._awprot) <$> m2s_wa)) -- Not used in the current implementation according to pg150.
-        (Port @"c0_ddr4_s_axi_awqos"   (pure 0 :: Signal uiDom (BitVector 4))) -- Not used in the current implementation according to pg150.
+        (Port @"c0_ddr4_s_axi_awqos"   (pure 0 :: Signal Ddr200 (BitVector 4))) -- Not used in the current implementation according to pg150.
         (Port @"c0_ddr4_s_axi_awvalid" (isWriteAddress <$> m2s_wa))
 
         -- AXI Write Data Ports
@@ -341,13 +348,13 @@ ddr4Axi
         -- AXI Read Address Ports
         (Port @"c0_ddr4_s_axi_arid"    ((._arid) <$> m2s_ra))
         (Port @"c0_ddr4_s_axi_araddr"  ((._araddr) <$> m2s_ra))
-        (Port @"c0_ddr4_s_axi_arlen"   (pure 0 :: Signal uiDom (BitVector 8)))
-        (Port @"c0_ddr4_s_axi_arsize"  (pure (pack Bs1) :: Signal uiDom (BitVector 3)))
-        (Port @"c0_ddr4_s_axi_arburst" (pure (pack BmFixed) :: Signal uiDom (BitVector 2)))
+        (Port @"c0_ddr4_s_axi_arlen"   (pure 0 :: Signal Ddr200 (BitVector 8)))
+        (Port @"c0_ddr4_s_axi_arsize"  (pure (pack Bs1) :: Signal Ddr200 (BitVector 3)))
+        (Port @"c0_ddr4_s_axi_arburst" (pure (pack BmFixed) :: Signal Ddr200 (BitVector 2)))
         (Port @"c0_ddr4_s_axi_arlock"  (pack . (._arlock) <$> m2s_ra)) -- Not used in the current implementation according to pg150.
         (Port @"c0_ddr4_s_axi_arcache" (pack . (._arcache) <$> m2s_ra)) -- Not used in the current implementation according to pg150.
         (Port @"c0_ddr4_s_axi_arprot"  (pack . (._arprot) <$> m2s_ra)) -- Not used in the current implementation according to pg150.
-        (Port @"c0_ddr4_s_axi_arqos"   (pure 0 :: Signal uiDom (BitVector 4))) -- Not used in the current implementation according to pg150.
+        (Port @"c0_ddr4_s_axi_arqos"   (pure 0 :: Signal Ddr200 (BitVector 4))) -- Not used in the current implementation according to pg150.
         (Port @"c0_ddr4_s_axi_arvalid" (isReadAddress <$> m2s_ra))
 
         -- AXI Read Data Ports
