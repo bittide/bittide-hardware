45d0d5937fae5a5b72b6c7fc3b2b6c4abd03b15e
hydrolarus <lara@qbaylogic.com>
2025-07-08 10:21:31 +0200

    add metaPeConfig device

---
+++ bittide/src/Bittide/SwitchDemoProcessingElement/MetaPeConfig.hs
@@ -0,0 +1,92 @@
+-- SPDX-FileCopyrightText: 2025 Google LLC
+--
+-- SPDX-License-Identifier: Apache-2.0
+{-# LANGUAGE ImplicitParams #-}
+
+-- | Device to store configuration data for the Switch Demo
+module Bittide.SwitchDemoProcessingElement.MetaPeConfig where
+
+import Clash.Prelude
+
+import Clash.Class.BitPackC (ByteOrder, Bytes)
+import GHC.Stack (HasCallStack)
+import Protocols
+import Protocols.MemoryMap (Access (..), ConstBwd, MM)
+import Protocols.MemoryMap.Registers.WishboneStandard (
+  RegisterConfig (access, description),
+  deviceWbC,
+  registerConfig,
+  registerWbCI_,
+ )
+import Protocols.Wishbone
+
+metaPeConfig ::
+  forall bufferSize aw dom.
+  ( HasCallStack
+  , HiddenClockResetEnable dom
+  , KnownNat aw
+  , 4 <= aw
+  , KnownNat bufferSize
+  , 1 <= bufferSize
+  , ?busByteOrder :: ByteOrder
+  , ?regByteOrder :: ByteOrder
+  ) =>
+  SNat bufferSize ->
+  Circuit
+    ( ConstBwd MM
+    , Wishbone dom 'Standard aw (Bytes 4)
+    )
+    ()
+metaPeConfig SNat = circuit $ \(mm, wb) -> do
+  [wMC, wO, wN, rMC, rO, rN, buffer] <- deviceWbC "MetaPeConfig" -< (mm, wb)
+
+  registerWbCI_ wMcConfig (0 :: Unsigned 32) -< (wMC, Fwd noWrite)
+  registerWbCI_ wOConfig (0 :: Unsigned 32) -< (wO, Fwd noWrite)
+  registerWbCI_ wNConfig (0 :: Unsigned 32) -< (wN, Fwd noWrite)
+  registerWbCI_ rMcConfig (0 :: Unsigned 32) -< (rMC, Fwd noWrite)
+  registerWbCI_ rOConfig (0 :: Unsigned 32) -< (rO, Fwd noWrite)
+  registerWbCI_ rNConfig (0 :: Unsigned 32) -< (rN, Fwd noWrite)
+
+  registerWbCI_ bufferConfig (repeat 0 :: Vec (bufferSize * 3) (BitVector 64))
+    -< (buffer, Fwd noWrite)
+
+  idC -< ()
+ where
+  wMcConfig =
+    (registerConfig "write_metacycle")
+      { access = ReadWrite
+      , description = "The metacycle in which the PE should write in"
+      }
+  wOConfig =
+    (registerConfig "write_offset")
+      { access = ReadWrite
+      , description = "Offset in gather memory to start writing from"
+      }
+  wNConfig =
+    (registerConfig "write_n_addresses")
+      { access = ReadWrite
+      , description = "How many addresses the PE should write"
+      }
+  rMcConfig =
+    (registerConfig "read_metacycle")
+      { access = ReadWrite
+      , description = "The metacycle in which the PE should read"
+      }
+  rOConfig =
+    (registerConfig "read_offset")
+      { access = ReadWrite
+      , description = "Offset in scatter memory to start reading from"
+      }
+  rNConfig =
+    (registerConfig "read_n_addresses")
+      { access = ReadWrite
+      , description = "How many addresses the PE should read"
+      }
+
+  bufferConfig =
+    (registerConfig "buffer")
+      { access = ReadWrite
+      , description = "Buffer to hold bittide data"
+      }
+
+  noWrite = pure Nothing
