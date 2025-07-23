a3868605ac6bd0fca4272c3196392e0643a9fb97
Martijn Bastiaan <martijn@qbaylogic.com>
2025-07-23 14:30:35 +0200

    Build Rust crates before running `bittide-instances`'s `unittests`

--- bittide-instances/tests/unittests.hs
+++ bittide-instances/tests/unittests.hs
@@ -1,12 +1,19 @@
 -- SPDX-FileCopyrightText: 2024 Google LLC
 --
 -- SPDX-License-Identifier: Apache-2.0
+{-# LANGUAGE PackageImports #-}
 
 module Main where
 
 import Prelude
 
+import Data.String.Interpolate (i)
+import System.Exit (ExitCode (ExitFailure, ExitSuccess))
+import System.IO.Unsafe (unsafePerformIO)
+import System.Process (cwd, proc, readCreateProcessWithExitCode, readProcess)
 import Test.Tasty
+import Test.Tasty.HUnit (assertFailure, testCase)
+import "extra" Data.List.Extra (trim)
 
 import qualified Tests.ClockControlWb as ClockControlWb
 import qualified Tests.OverflowResistantDiff as Ord
@@ -19,21 +26,57 @@ import qualified Wishbone.SwitchDemoProcessingElement as SwitchDemoProcessingEle
 import qualified Wishbone.Time as Time
 import qualified Wishbone.Watchdog as Watchdog
 
+getGitRoot :: IO FilePath
+getGitRoot = trim <$> readProcess "git" ["rev-parse", "--show-toplevel"] ""
+
+run :: FilePath -> [String] -> Maybe FilePath -> IO ()
+run cmd0 args cwd = do
+  let cmd1 = (proc cmd0 args){cwd = cwd}
+  (exitCode, stdout, stdErr) <- readCreateProcessWithExitCode cmd1 ""
+  case exitCode of
+    ExitSuccess -> pure ()
+    ExitFailure _ ->
+      assertFailure
+        [i|
+      Command failed with exit code #{exitCode}:
+
+        #{cmd0} #{unwords args}
+
+      stdout:
+
+        #{stdout}
+
+      stderr:
+
+        #{stdErr}
+    |]
+
 tests :: TestTree
 tests =
-  testGroup
-    "Unittests"
-    [ Axi.tests
-    , CaptureUgn.tests
-    , ClockControlWb.tests
-    , DnaPortE2.tests
-    , Ord.tests
-    , ScatterGather.tests
-    , SwitchDemoProcessingElement.tests
-    , Time.tests
-    , RegisterWbC.tests
-    , Watchdog.tests
+  sequentialTestGroup
+    "bittide-instances"
+    AllSucceed
+    [ testGroup
+        "Build Rust crates"
+        [ testCase "release" (run "./cargo.sh" ["build", "--release"] (Just gitRoot))
+        , testCase "debug" (run "./cargo.sh" ["build"] (Just gitRoot))
+        ]
+    , testGroup
+        "Unittests"
+        [ Axi.tests
+        , CaptureUgn.tests
+        , ClockControlWb.tests
+        , DnaPortE2.tests
+        , Ord.tests
+        , ScatterGather.tests
+        , SwitchDemoProcessingElement.tests
+        , Time.tests
+        , RegisterWbC.tests
+        , Watchdog.tests
+        ]
     ]
+ where
+  gitRoot = unsafePerformIO getGitRoot
 
 main :: IO ()
 main = defaultMain tests
