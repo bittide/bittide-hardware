aeb7acba074c37f7986ace005d9f654a80113a11
Lucas Bollen <lucas@qbaylogic.com>
2025-08-11 16:37:33 +0200

    Add `pre-commit` config to nix
    This introduces the infra for adding more pre-commit hooks
    and the first hooks

---
+++ nix/git-hooks.nix
@@ -0,0 +1,42 @@
+# See https://github.com/cachix/git-hooks.nix/tree/master
+{ pkgs, ... }:
+
+{
+  # This is the configuration passed to the pre-commit-hooks module.
+  hooks = {
+
+    reuse.enable = true; # check for REUSE compliance
+    trim-trailing-whitespace.enable = true;
+    end-of-file-fixer.enable = true;
+    check-added-large-files.enable = true;
+
+    # Enable a formatter for Haskell
+    cabal-gild.enable = true;
+
+
+    # Fourmolu and header fix, Fourmolu sometimes messes up the header comments, so
+    # we need to fix them manually using the fix_spdx_header.py script.
+    format-hs = {
+      enable = true;
+      name = "Format Haskell files and fix SPDX headers";
+      entry = ".github/scripts/fourmolu.sh";
+      language = "system";
+      files = "\\.(hs|hs-boot)$";
+      excludes = ["^clash-vexriscv/.*"];
+    };
+
+    # Excludes, I dont know a different way to do this
+    reuse.excludes = ["^clash-vexriscv/.*"];
+    trim-trailing-whitespace.excludes = ["^clash-vexriscv/.*"];
+    end-of-file-fixer.excludes = ["^clash-vexriscv/.*"];
+    check-added-large-files.excludes = ["^clash-vexriscv/.*"];
+    cabal-gild.excludes = ["^clash-vexriscv/.*"];
+
+
+    # We can not use rust hooks yet because they assume the repository root contains a `Cargo.toml` file.
+    # TODO: Create workaround for this issue.
+    # rustfmt.enable = true;
+    # cargo-check.enable = true;
+    # clippy.enable = true;
+  };
+}
