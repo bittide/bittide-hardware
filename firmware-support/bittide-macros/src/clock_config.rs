4ec1ec9f52a0baa251a289cd89896a5238ed6472
Hidde Moll <hidde@qbaylogic.com>
2025-09-17 16:27:55 +0200

    Add macro for parsing clock configurations

---
+++ firmware-support/bittide-macros/src/clock_config.rs
@@ -0,0 +1,134 @@
+// SPDX-FileCopyrightText: 2025 Google LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum ParserState {
+    PreambleHeader,
+    Preamble,
+    PostPreambleDelay,
+    Config,
+    PostambleHeader,
+    Postamble,
+    Done,
+}
+
+#[derive(Debug)]
+pub enum ClockConfigParseError {
+    AddressMissing,
+    DataMissing,
+    AddressOutOfRange,
+    DataOutOfRange,
+    EndOfConfig,
+    InvalidHexNumber,
+}
+
+pub struct ConfigEntry {
+    pub page: u8,
+    pub address: u8,
+    pub data: u8,
+}
+
+/// Create a class that implements the statemachine for parsing the clock configuration file.
+pub struct ClockConfigParser {
+    pub state: ParserState,
+}
+
+impl ClockConfigParser {
+    /// Create a new parser.
+    pub fn new() -> Self {
+        Self {
+            state: ParserState::PreambleHeader,
+        }
+    }
+
+    /// Parse a line of the clock configuration file.
+    pub fn parse_line(&mut self, line: &str) -> Result<Option<ConfigEntry>, ClockConfigParseError> {
+        let (next_state, result) = parse_line(self.state, line);
+        self.state = next_state;
+        result
+    }
+
+    /// Check if the parser is done.
+    pub fn is_done(&self) -> bool {
+        self.state == ParserState::Done
+    }
+}
+
+impl Default for ClockConfigParser {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn parse_line(
+    state: ParserState,
+    line: &str,
+) -> (
+    ParserState,
+    Result<Option<ConfigEntry>, ClockConfigParseError>,
+) {
+    let next_state = match (state, line) {
+        (ParserState::PreambleHeader, _) if line.contains("Start configuration preamble") => {
+            ParserState::Preamble
+        }
+        (ParserState::Preamble, _) if line.contains("End configuration preamble") => {
+            ParserState::PostPreambleDelay
+        }
+        (ParserState::PostPreambleDelay, _) if line.contains("Start configuration registers") => {
+            ParserState::Config
+        }
+        (ParserState::Config, _) if line.contains("End configuration registers") => {
+            ParserState::PostambleHeader
+        }
+        (ParserState::PostambleHeader, _) if line.contains("Start configuration postamble") => {
+            ParserState::Postamble
+        }
+        (ParserState::Postamble, _) if line.contains("End configuration postamble") => {
+            ParserState::Done
+        }
+        _ => state,
+    };
+    if next_state != state {
+        return (next_state, Ok(None));
+    }
+    let result = match state {
+        ParserState::Preamble | ParserState::Config | ParserState::Postamble => {
+            parse_address_data(line).map(Some)
+        }
+        ParserState::Done => Err(ClockConfigParseError::EndOfConfig),
+        _ => Ok(None),
+    };
+    (next_state, result)
+}
+
+fn parse_address_data(line: &str) -> Result<ConfigEntry, ClockConfigParseError> {
+    let mut parts = line.split(',');
+
+    let address_str = parts.next().ok_or(ClockConfigParseError::AddressMissing)?;
+    let full_address: u16 = parse_hex(address_str)?
+        .try_into()
+        .map_err(|_| ClockConfigParseError::AddressOutOfRange)?;
+    let page = (full_address >> 8) as u8;
+    let address = (full_address & 0xff) as u8;
+
+    let data_str = parts.next().ok_or(ClockConfigParseError::DataMissing)?;
+    let data: u8 = parse_hex(data_str)?
+        .try_into()
+        .map_err(|_| ClockConfigParseError::DataOutOfRange)?;
+
+    let config_entry = ConfigEntry {
+        page,
+        address,
+        data,
+    };
+    Ok(config_entry)
+}
+
+fn parse_hex(hex_string: &str) -> Result<u32, ClockConfigParseError> {
+    if let Some(hex_stripped) = hex_string.strip_prefix("0x") {
+        u32::from_str_radix(hex_stripped, 16).map_err(|_| ClockConfigParseError::InvalidHexNumber)
+    } else {
+        Err(ClockConfigParseError::InvalidHexNumber)
+    }
+}
--- firmware-support/bittide-macros/src/lib.rs
+++ firmware-support/bittide-macros/src/lib.rs
@@ -2,8 +2,12 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+mod clock_config;
+
+use clock_config::{ClockConfigParser, ConfigEntry, ParserState};
 use quote::quote;
-use syn::{LitInt, Token, parse::Parse, parse_macro_input};
+use std::fs;
+use syn::{LitInt, LitStr, Token, parse::Parse, parse_macro_input};
 
 /// Macro to conveniently create an indexed type
 ///
@@ -145,4 +149,73 @@ impl Parse for IndexValArgs {
             n_val,
         })
     }
+}
+
+// /// Macro to read a CSV file with a clock configuration into a usable format.
+#[proc_macro]
+// #[allow(non_snake_case)]
+pub fn load_clock_config_csv(toks: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let path_lit = parse_macro_input!(toks as LitStr);
+    let path_str = path_lit.value();
+
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let file_path = std::path::Path::new(&manifest_dir).join(&path_str);
+
+    let file_content = match fs::read_to_string(&file_path) {
+        Ok(content) => content,
+        Err(e) => {
+            let error_msg = format!("Failed to read file '{}': {}", file_path.display(), e);
+            return syn::Error::new(path_lit.span(), error_msg)
+                .to_compile_error()
+                .into();
+        }
+    };
+
+    let mut preamble_initializers = Vec::new();
+    let mut config_initializers = Vec::new();
+    let mut postamble_initializers = Vec::new();
+    let mut parser = ClockConfigParser::new();
+    for line in file_content.lines() {
+        if !parser.is_done() {
+            let old_state = parser.state;
+            match parser.parse_line(line) {
+                Ok(Some(ConfigEntry {
+                    page,
+                    address,
+                    data,
+                })) => {
+                    let init = quote! {
+                        ::bittide_hal::manual_additions::si539x_spi::ConfigEntry {
+                            page: #page,
+                            address: #address,
+                            data: #data,
+                        }
+                    };
+                    match old_state {
+                        ParserState::Preamble => preamble_initializers.push(init),
+                        ParserState::Config => config_initializers.push(init),
+                        ParserState::Postamble => postamble_initializers.push(init),
+                        _ => {}
+                    }
+                }
+                Ok(None) => {}
+                Err(e) => {
+                    let error_msg = format!("Error: {e:?}");
+                    return syn::Error::new(path_lit.span(), error_msg)
+                        .to_compile_error()
+                        .into();
+                }
+            }
+        }
+    }
+
+    let expanded = quote! {
+        Config {
+            preamble: [#(#preamble_initializers),*],
+            config: [#(#config_initializers),*],
+            postamble: [#(#postamble_initializers),*],
+        }
+    };
+
+    expanded.into()
 }
