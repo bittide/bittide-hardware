b8c05f2e995176b645a9b176cb008ebf97612dd0
Hidde Moll <hidde@qbaylogic.com>
2025-09-23 17:01:37 +0200

    Extend picocom timeout duration from 10 to 120 seconds

--- bittide-instances/src/Bittide/Instances/Hitl/Driver/ClockBoardConfiguration.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/Driver/ClockBoardConfiguration.hs
@@ -158,7 +158,7 @@ driverFunc _name targets = do
           liftIO $ mapConcurrently_ Gdb.continue gdbs
 
           liftIO
-            $ T.tryWithTimeout T.PrintActionTime "Waiting for test success" 10_000_000
+            $ T.tryWithTimeout T.PrintActionTime "Waiting for test success" 120_000_000
             $ forConcurrently_ picocoms
             $ \pico ->
               waitForLine pico.stdoutHandle "Test passed"
