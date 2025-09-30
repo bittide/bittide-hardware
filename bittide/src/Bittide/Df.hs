b8175c81a33db6b26f6f89580295df47c4072b1e
Lucas Bollen <lucas@qbaylogic.com>
2025-09-30 21:52:53 +0200

    Add `wbToDf` (#979)
    A component to write arbitrary data to a `Df` stream over wishbone

--- bittide/src/Bittide/Df.hs
+++ bittide/src/Bittide/Df.hs
@@ -2,17 +2,26 @@
 --
 -- SPDX-License-Identifier: Apache-2.0
 
-module Bittide.Df (asciiDebugMux) where
+module Bittide.Df (asciiDebugMux, wbToDf) where
 
 import Clash.Prelude
 import Protocols
 
 import Bittide.PacketStream (timeout)
-import Bittide.SharedTypes (Byte)
+import Bittide.SharedTypes (Byte, Bytes)
+import Clash.Class.BitPackC (BitPackC, ByteOrder)
+import Data.Bifunctor
 import Data.Char (ord)
 import Data.Coerce (coerce)
 import Data.Functor ((<&>))
+import GHC.Stack (HasCallStack)
 import Protocols.Df (CollectMode (Skip))
+import Protocols.MemoryMap.FieldType (ToFieldType)
+import Protocols.MemoryMap.Registers.WishboneStandard (
+  BusActivity,
+  registerWb,
+  registerWbDf,
+ )
 import Protocols.PacketStream (
   PacketStream,
   PacketStreamM2S (..),
@@ -22,6 +31,10 @@ import Protocols.PacketStream (
 import Protocols.PacketStream.Base (truncateAbortedPackets)
 import Protocols.PacketStream.Routing (packetArbiterC)
 import Protocols.Vec (vecCircuits)
+import Protocols.Wishbone
+
+import qualified Protocols.MemoryMap as MM
+import qualified Protocols.MemoryMap.Registers.WishboneStandard as MM
 
 {- | Muxes multiple 'Df' streams of bytes into a single 'Df' stream of bytes,
 interleaving the streams from the different sources on newline characters. As
@@ -126,3 +139,59 @@ unsafeBytesFromPacketStream f =
       | Just PacketStreamM2S{_abort = True} <- m2s = (coerce True, Nothing)
       | Just PacketStreamM2S{_last = Just 0} <- m2s = (coerce True, Nothing)
       | otherwise = (coerce ack, fmap (pack . (._data)) m2s)
+
+{- | Component to create a `Df` stream from a wishbone interface. This component
+features two registers, one to accumulate the data and one to create the `Df` transaction.
+-}
+wbToDf ::
+  forall dom a addrW nBytes.
+  ( HiddenClockResetEnable dom
+  , KnownNat nBytes
+  , 1 <= nBytes
+  , KnownNat addrW
+  , Show a
+  , ShowX a
+  , ToFieldType a
+  , NFDataX a
+  , BitPack a
+  , BitPackC a
+  , ?busByteOrder :: ByteOrder
+  , ?regByteOrder :: ByteOrder
+  , HasCallStack
+  ) =>
+  String ->
+  Circuit
+    (MM.ConstBwd MM.MM, Wishbone dom 'Standard addrW (Bytes nBytes))
+    (Df dom a)
+wbToDf name = circuit $ \(mm, wb) -> do
+  [wbData, wbCommit] <- MM.deviceWb name -< (mm, wb)
+
+  (dat, _0) <- registerWb hasClock hasReset cfgData (unpack 0) -< (wbData, Fwd noWrite)
+  (_1, df) <- registerWbDf hasClock hasReset cfgCommit () -< (wbCommit, Fwd noWrite)
+
+  replaceDfData -< (dat, df)
+ where
+  noWrite = pure Nothing
+
+  -- Given a Df stream and a raw signal, replace the contents of the Df with the
+  -- contents of the raw signal. Usage of this function is only valid as long as
+  -- the raw signal stays stable whenever the Df stream is active. In our case
+  -- this is true, as the circuit doesn't write to either register and the wishbone
+  -- bus can only do one access at a time.
+  replaceDfData :: Circuit (CSignal dom a, Df dom (BusActivity ())) (Df dom a)
+  replaceDfData = Circuit (first unbundle . unbundle . fmap go . bundle . first bundle)
+   where
+    go :: ((a, Maybe (BusActivity ())), Ack) -> (((), Ack), Maybe a)
+    go ((a, busActivity), ack) = (((), ack), fmap (const a) busActivity)
+
+  cfgData =
+    (MM.registerConfig "data")
+      { MM.access = MM.WriteOnly
+      , MM.description = "Data register for " <> name
+      }
+
+  cfgCommit =
+    (MM.registerConfig "commit")
+      { MM.access = MM.WriteOnly
+      , MM.description = "Commit register for " <> name
+      }
