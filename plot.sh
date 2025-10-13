0938984810e06cd35001a312cc72a28c647f6299
Martijn Bastiaan <martijn@qbaylogic.com>
2025-10-13 14:20:43 +0200

    Bump plot scripts

--- plot.sh
+++ plot.sh
@@ -6,15 +6,19 @@ export JOB_ID="$1"
 export CUTOFF_MS="$2"
 export TOPOLOGY="$3"
 export FIBER="$4"
-export BITTIDE_PAPERS_DIR="$5"
-export BITTIDE_PAPERS_RESULTS_DIR="${BITTIDE_PAPERS_DIR}"/asplos/results
-export TARGET_DIR="${FIBER}"_10ppb_60ms_k2e9
+export K_P="$5"
+export STEP_SIZE="$6"
+export SAMPLE_SPEED="$7"
+export BITTIDE_PAPERS_DIR="$8"
+export BITTIDE_PAPERS_RESULTS_DIR="${BITTIDE_PAPERS_DIR}"/extras/results
+export TARGET_DIR="${FIBER}_${STEP_SIZE}_${SAMPLE_SPEED}_${K_P}"
 
 export RUNREF="https://github.com/bittide/bittide-hardware/actions/runs/${JOB_ID}"
 
 export PLOT_FINCFDECS="no"
 cabal run bittide-tools:cc-plot "${JOB_ID}":hwCcTopologyTest _build
 
+mkdir -p "${BITTIDE_PAPERS_RESULTS_DIR}"/"${TARGET_DIR}"/
 rm -rf "${BITTIDE_PAPERS_RESULTS_DIR}"/"${TARGET_DIR}"/"${TOPOLOGY}"
 mv "${TOPOLOGY}" "${BITTIDE_PAPERS_RESULTS_DIR}"/"${TARGET_DIR}"/
 
--- plots.sh
+++ plots.sh
@@ -2,16 +2,20 @@
 set -e
 set -u
 
-JOB_ID_2KM=XXXXXXXX
-JOB_ID_2M=10943369955
+JOB_ID_2KM_k2e9=18092705175
+JOB_ID_2M_k2e9=XXXXXXXXXX
 
-BITTIDE_PAPERS_DIR=/home/martijn/code/bittide-papers
+BITTIDE_PAPERS_DIR=/home/martijn/code/paper_qbay_hw
 
 
-# ./plot.sh "${JOB_ID_2KM}" 50000 hourglass 2km "${BITTIDE_PAPERS_DIR}"
-# ./plot.sh "${JOB_ID_2KM}" 5000  complete  2km "${BITTIDE_PAPERS_DIR}"
-# ./plot.sh "${JOB_ID_2KM}" 10000 hypercube 2km "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2KM_k2e9}" 50000  hourglass 2km k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2KM_k2e9}" 5000   complete  2km k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2KM_k2e9}" 15000  hypercube 2km k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2KM_k2e9}" 80000  line      2km k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2KM_k2e9}" 25000  torus2d   2km k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
 
-./plot.sh "${JOB_ID_2M}"  50000 hourglass 2m "${BITTIDE_PAPERS_DIR}"
-./plot.sh "${JOB_ID_2M}"  5000  complete  2m "${BITTIDE_PAPERS_DIR}"
-./plot.sh "${JOB_ID_2M}"  10000 hypercube 2m "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2M_k2e9}" 50000  hourglass 2m k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2M_k2e9}" 5000   complete  2m k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2M_k2e9}" 15000  hypercube 2m k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2M_k2e9}" 80000  line      2m k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
+./plot.sh "${JOB_ID_2M_k2e9}" 25000  torus2d   2m k2e9 10ppb 60ms "${BITTIDE_PAPERS_DIR}"
