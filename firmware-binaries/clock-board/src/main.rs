03054a7a93b35147706a8991fab35d089c81d5bd
Hidde Moll <hidde@qbaylogic.com>
2025-10-15 17:22:04 +0200

    Write config for 200 MHz instead of 100 MHz

--- firmware-binaries/clock-board/src/main.rs
+++ firmware-binaries/clock-board/src/main.rs
@@ -28,8 +28,8 @@ enum FrequencyCheckError {
 }
 
 const INSTANCES: DeviceInstances = unsafe { DeviceInstances::new() };
-const CONFIG_100: Config<3, 590, 5> =
-    load_clock_config_csv!("../../bittide/data/clock_configs/Si5395J-100MHz-1ppb-Registers.csv");
+const CONFIG_200: Config<3, 590, 5> =
+    load_clock_config_csv!("../../bittide/data/clock_configs/Si5395J-200MHz-1ppb-Registers.csv");
 
 const TOLERANCE: i32 = 1_000_000;
 
@@ -99,16 +99,16 @@ fn main() -> ! {
     let mut uart = INSTANCES.uart;
     let domain_diff_counters = INSTANCES.domain_diff_counters;
 
-    // Write a clock configuration with a frequency of 100 MHz.
-    uwriteln!(uart, "Writing configuration with f = 100 MHz...").unwrap();
+    // Write a clock configuration with a frequency of 200 MHz.
+    uwriteln!(uart, "Writing configuration with f = 200 MHz...").unwrap();
     let start = timer.now();
-    si539x_spi.write_configuration(&timer, &CONFIG_100, &mut uart);
+    si539x_spi.write_configuration(&timer, &CONFIG_200, &mut uart);
     let end = timer.now();
     uwriteln!(uart, "Writing configuration took {}", end - start).unwrap();
 
     // Verify that the configuration is now on the board.
     uwriteln!(uart, "Verifying configuration...").unwrap();
-    match si539x_spi.verify_configuration(&CONFIG_100) {
+    match si539x_spi.verify_configuration(&CONFIG_200) {
         Ok(()) => uwriteln!(uart, "Verified configuration").unwrap(),
         Err(Si539xError::NotExpectedConfig { design_id }) => {
             match core::str::from_utf8(&design_id) {
@@ -129,9 +129,9 @@ fn main() -> ! {
         Err(_) => (),
     }
 
-    // Compare the programmed clock (100 MHz) against the system clock (125 MHz)
+    // Compare the programmed clock (200 MHz) against the system clock (125 MHz)
     // using a domain difference counter. After 1 second the domain difference
-    // counter is expected to be around -25M.
+    // counter is expected to be around 75M.
     for i in 0..10_u8 {
         if unsafe { domain_diff_counters.counters_active_unchecked(0) } {
             uwriteln!(uart, "Counter 0 active after {} retries", i).unwrap();
@@ -146,7 +146,7 @@ fn main() -> ! {
         uwriteln!(uart, "Retry {}: Counter 0 is inactive", i).unwrap();
         timer.wait(Duration::from_millis(100));
     }
-    let result_a = check_frequency(&timer, &mut uart, &domain_diff_counters, -25_000_000);
+    let result_a = check_frequency(&timer, &mut uart, &domain_diff_counters, 75_000_000);
 
     match result_a {
         Ok(()) => uwriteln!(uart, "Test passed").unwrap(),
