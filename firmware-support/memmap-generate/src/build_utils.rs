467e84040844a913eb4eedd713c4a12cbe9eeca0
Ryan Slawson <ryan@qbaylogic.com>
2025-10-20 14:31:19 +0200

    Use `git2` instead of `gix`, turns out the latter has a lot of deps

--- firmware-support/memmap-generate/src/build_utils.rs
+++ firmware-support/memmap-generate/src/build_utils.rs
@@ -27,9 +27,9 @@ fn setup_riscv_linker(out_dir: &str) {
 pub fn memmap_dir() -> PathBuf {
     let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
-    gix::ThreadSafeRepository::discover(&manifest_dir)
+    git2::Repository::discover(&manifest_dir)
         .expect("Failed to find `.git`.")
-        .work_dir()
+        .workdir()
         .expect("Failed to find repository working directory")
         .join("_build")
         .join("memory_maps")
