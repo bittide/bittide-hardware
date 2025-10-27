636b41fb06854edb6cca4cc4d5ee3076473d2f90
Lucas Bollen <lucas@qbaylogic.com>
2025-10-27 16:26:48 +0100

    Entirely remove `resettableXilinxElasticBuffer` in favor of `xilinxElasticBufferWb`

--- bittide-instances/src/Bittide/Instances/Pnr/ElasticBuffer.hs
+++ bittide-instances/src/Bittide/Instances/Pnr/ElasticBuffer.hs
@@ -8,23 +8,35 @@ module Bittide.Instances.Pnr.ElasticBuffer where
 import Clash.Annotations.TH
 import Clash.Prelude
 
+import Protocols
+import Protocols.Wishbone
+
 import Bittide.ClockControl (RelDataCount)
 import Bittide.ElasticBuffer
+import Bittide.SharedTypes (Bytes, withBittideByteOrder)
 
 createDomain vXilinxSystem{vPeriod = hzToPeriod 201e6, vName = "Fast"}
 createDomain vXilinxSystem{vPeriod = hzToPeriod 199e6, vName = "Slow"}
 
-elasticBuffer5 ::
-  "clkReadFast" ::: Clock Fast ->
-  "clkWriteSlow" ::: Clock Slow ->
+elasticBufferWb ::
+  "clkRead" ::: Clock Fast ->
   "resetRead" ::: Reset Fast ->
-  "writeData" ::: Signal Slow (Unsigned 8) ->
-  ( "dataCount" ::: Signal Fast (RelDataCount 5)
+  "clkWrite" ::: Clock Slow ->
+  "wbIn" ::: Signal Fast (WishboneM2S 30 4 (Bytes 4)) ->
+  "writeData" ::: Signal Slow (Unsigned 64) ->
+  ( "wbOut" ::: Signal Fast (WishboneS2M (Bytes 4))
+  , "dataCount" ::: Signal Fast (RelDataCount 5)
   , "underflow" ::: Signal Fast Underflow
   , "overflow" ::: Signal Fast Overflow
   , "stable" ::: Signal Fast Stable
-  , "readData" ::: Signal Fast (Unsigned 8)
+  , "readData" ::: Signal Fast (Unsigned 64)
   )
-elasticBuffer5 = resettableXilinxElasticBuffer
+elasticBufferWb clkRead rstRead clkWrite wbIn wdata = (wbOut, dataCount, underflow, overflow, stable, readData)
+ where
+  ((SimOnly _mm, wbOut), (dataCount, underflow, overflow, stable, readData)) =
+    withBittideByteOrder
+      $ toSignals
+        (xilinxElasticBufferWb clkRead rstRead d5 clkWrite wdata)
+        (((), wbIn), (pure (), pure (), pure (), pure (), pure ()))
 
-makeTopEntity 'elasticBuffer5
+makeTopEntity 'elasticBufferWb
