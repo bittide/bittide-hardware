154539a5f9ca99c47080ff1b89f51250b6076184
hydrolarus <lara@qbaylogic.com>
2025-11-07 13:40:22 +0100

    add licenses

--- firmware-support/bittide-hal/src/primitives/mod.rs
+++ firmware-support/bittide-hal/src/primitives/mod.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Google LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
 // pub use bitvector::*;
 // pub mod bitvector;
 // pub mod index;
--- firmware-support/memmap-generate/src/deprecated/mod.rs
+++ firmware-support/memmap-generate/src/deprecated/mod.rs
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2025 Google LLC
+//
+// SPDX-License-Identifier: Apache-2.0
 pub mod generators;
 pub mod hal_set;
-
