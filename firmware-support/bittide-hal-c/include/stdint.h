5ce2ebb2bd5b570b61fee44e0a6d0998adc79b9f
Lucas Bollen <lucas@qbaylogic.com>
2025-12-08 08:40:27 +0100

    fix `INT*_MIN` bug. Previous definition was wrong, the definitions were not literals, but the `-` operator applied to the positive value which, given the asymmetic range of positive and negative values in signed numbers did not fit.
    This caused a `clang` linter warning mentiond the overflow

--- firmware-support/bittide-hal-c/include/stdint.h
+++ firmware-support/bittide-hal-c/include/stdint.h
@@ -55,19 +55,19 @@ typedef int64_t intmax_t;
 typedef uint64_t uintmax_t;
 
 /* Limits of exact-width integer types */
-#define INT8_MIN (-128)
+#define INT8_MIN (-127 - 1)
 #define INT8_MAX 127
 #define UINT8_MAX 255
 
-#define INT16_MIN (-32768)
+#define INT16_MIN (-32767 - 1)
 #define INT16_MAX 32767
 #define UINT16_MAX 65535
 
-#define INT32_MIN (-2147483648)
+#define INT32_MIN (-2147483647 - 1)
 #define INT32_MAX 2147483647
 #define UINT32_MAX 4294967295U
 
-#define INT64_MIN (-9223372036854775808LL)
+#define INT64_MIN (-9223372036854775807LL - 1)
 #define INT64_MAX 9223372036854775807LL
 #define UINT64_MAX 18446744073709551615ULL
 
