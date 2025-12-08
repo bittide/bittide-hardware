b7b47211d5909af1f20d30ddcfa8e3b2fcc5aad7
Lucas Bollen <lucas@qbaylogic.com>
2025-12-08 14:58:22 +0100

    Refactor ugn discovery to use calculated buffer alignments

--- firmware-binaries/demos/soft-ugn-gppe/src/main.c
+++ firmware-binaries/demos/soft-ugn-gppe/src/main.c
@@ -28,13 +28,13 @@ ScatterUnit su;
 #define BUFFER_SIZE 4000
 
 // SEND timing parameters
-#define SEND_SPACING 8000 // Spacing between SEND events
+#define SEND_SPACING BUFFER_SIZE // Spacing between SEND events
 #define SEND_PERIOD (SEND_SPACING * NUM_PORTS)
 #define FINAL_SEND_DELAY (2 * SEND_PERIOD + BUFFER_SIZE)
 
 // RECEIVE timing parameters
-#define RECEIVE_SPACING 8000 // Spacing between RECEIVE events
-#define RECEIVE_PERIOD (RECEIVE_SPACING * NUM_PORTS + 1)
+#define RECEIVE_SPACING BUFFER_SIZE // Spacing between RECEIVE events
+#define RECEIVE_PERIOD (RECEIVE_SPACING * NUM_PORTS)
 
 // INVALIDATE timing parameters
 #define INVALIDATE_DELAY BUFFER_SIZE
@@ -192,7 +192,8 @@ int c_main(void) {
 
   // Schedule SEND and RECEIVE events
   for (uint32_t port = 0; port < NUM_PORTS; port++) {
-    uint64_t send_time = start_cycles_write + port * SEND_SPACING;
+    uint64_t send_time =
+        start_cycles_write + port * SEND_SPACING - outgoing_offsets[port];
     uint32_t send_offset = send_time % BUFFER_SIZE;
 
     uint64_t receive_time = start_cycles_read + port * RECEIVE_SPACING;
@@ -229,25 +230,25 @@ int c_main(void) {
 
     // Process the event
     if (current_event.type == EVENT_TYPE_SEND) {
-      send_ugn_to_port(&ugn_ctx, &timer, &current_event);
       UgnMessageType msg_type;
-      // Schedule ACKNOWLEDGE instead of ANNOUNCE If we captured the incoming
-      // ugn
-      if (ugn_ctx.incoming_link_ugn_list[current_event.port].is_valid) {
+      if (ugn_ctx.outgoing_link_ugn_list[current_event.port].is_valid) {
         msg_type = MSG_TYPE_ACKNOWLEDGE;
       } else {
+        // Reschedule if we dont have the outgoing ugn yet
         msg_type = MSG_TYPE_ANNOUNCE;
+        uint64_t next_event_time = event_time + SEND_PERIOD;
+        Event next_send_event =
+            make_send_event(msg_type, next_event_time, current_event.port);
+        pq_insert(&event_queue, encode_event(next_send_event), next_event_time);
       }
-      uint64_t next_event_time = event_time + SEND_PERIOD;
-      Event next_send_event =
-          make_send_event(msg_type, next_event_time, current_event.port);
-      pq_insert(&event_queue, encode_event(next_send_event), next_event_time);
+      send_ugn_to_port(&ugn_ctx, &timer, &current_event);
 
       // Schedule invalidate event
       uint64_t invalidate_time = event_time + INVALIDATE_DELAY;
       Event invalidate_event =
           make_invalidate_event(invalidate_time, current_event.port);
       pq_insert(&event_queue, encode_event(invalidate_event), invalidate_time);
+
     } else if (current_event.type == EVENT_TYPE_INVALIDATE) {
 
       // Invalidate the specific port and offset encoded in the event
