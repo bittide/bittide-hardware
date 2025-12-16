391e0c42022bbf06187561d62d518994c8e119c5
Lucas Bollen <lucas@qbaylogic.com>
2025-12-16 14:34:29 +0100

    Add stages to ila

--- bittide-instances/src/Bittide/Instances/Hitl/SoftUgnDemo/Core.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/SoftUgnDemo/Core.hs
@@ -282,6 +282,7 @@ core (refClk, refRst) (bitClk, bitRst, bitEna) rxClocks rxResets =
               )
                 { depth = D1024
                 , advancedTriggers = True
+                , stages = 2
                 }
               refClk
               ilaBufStable
