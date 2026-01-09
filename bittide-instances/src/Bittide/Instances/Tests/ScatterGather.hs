6a921bc03bd81828854ccdc8c5aafb7506dba04c
Lucas Bollen <lucas@qbaylogic.com>
2026-01-09 11:55:24 +0100

    Add timing peripheral to `ScatterGather` test architecture Used in smoltcp test

--- bittide-instances/src/Bittide/Instances/Tests/ScatterGather.hs
+++ bittide-instances/src/Bittide/Instances/Tests/ScatterGather.hs
@@ -58,7 +58,7 @@ dutMM =
 
 -- | Parameterized DUT that loads a specific firmware binary.
 dutWithBinary ::
-  (HasCallStack, HiddenClockResetEnable dom) =>
+  (HasCallStack, HiddenClockResetEnable dom, 1 <= DomainPeriod dom) =>
   String ->
   Circuit (ToConstBwd Mm) (Df dom (BitVector 8))
 dutWithBinary binaryName = withBittideByteOrder $ circuit $ \mm -> do
@@ -68,11 +68,13 @@ dutWithBinary binaryName = withBittideByteOrder $ circuit $ \mm -> do
     , wbGu
     , wbSuCal
     , wbGuCal
+    , timeBus
     ] <-
     processingElement NoDumpVcd (peConfig binaryName) -< (mm, jtagIdle)
   (uartTx, _uartStatus) <- uartInterfaceWb d16 d2 uartBytes -< (uartBus, uartRx)
   Fwd link <- gatherUnitWbC gatherConfig -< (wbGu, wbGuCal)
   scatterUnitWbC scatterConfig link -< (wbSu, wbSuCal)
+  _cnt <- timeWb -< timeBus
   idC -< uartTx
  where
   peConfig binary = unsafePerformIO $ do
