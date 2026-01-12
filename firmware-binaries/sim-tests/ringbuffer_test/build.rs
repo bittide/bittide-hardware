5d38995274cb7e03fe90e9be9fdc3d4988663fe0
Lucas Bollen <lucas@qbaylogic.com>
2026-01-12 11:43:18 +0100

    Add initial ringbuffer components

---
+++ firmware-binaries/sim-tests/ringbuffer_test/build.rs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 Google LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use memmap_generate::build_utils::standard_memmap_build;
+
+fn main() {
+    standard_memmap_build("RingbufferTest.json", "DataMemory", "InstructionMemory");
+    println!("cargo:rerun-if-changed=build.rs");
+}
