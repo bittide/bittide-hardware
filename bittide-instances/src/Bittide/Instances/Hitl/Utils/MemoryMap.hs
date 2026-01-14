4191530f9b59831e713c249a93449b59527efb5d
Ryan Slawson <ryan@qbaylogic.com>
2026-01-14 11:45:35 +0100

    Separate lookup and canonicalization, fix lookup for canonicalized names

--- bittide-instances/src/Bittide/Instances/Hitl/Utils/MemoryMap.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/Utils/MemoryMap.hs
@@ -7,7 +7,7 @@ import Prelude
 
 import Data.Bifunctor (Bifunctor (bimap))
 import Data.List (find)
-import Data.Maybe (mapMaybe)
+import Data.Maybe (fromMaybe, mapMaybe)
 import Data.String.Interpolate (i)
 import GHC.Stack (HasCallStack)
 import Protocols.MemoryMap (
@@ -20,7 +20,10 @@ import Protocols.MemoryMap (
   convert,
   normalizeRelTree,
  )
-import Protocols.MemoryMap.Check (MemoryMapTreeAnn (..))
+import Protocols.MemoryMap.Check (
+  MemoryMapTreeAnn (..),
+  Normalized (Normalized),
+ )
 import Protocols.MemoryMap.Check.AbsAddress (
   AbsNormData (absoluteAddr, path),
   MemoryMapTreeAbsNorm,
@@ -29,6 +32,8 @@ import Protocols.MemoryMap.Check.AbsAddress (
 import Text.Show.Pretty (ppShow)
 
 import qualified Data.Map.Strict as M
+
+type CanonicalTreeAbsNorm = MemoryMapTreeAnn (Maybe String, AbsNormData) 'Normalized
 
 {- | Finds the address for a given path.
 
@@ -70,17 +75,60 @@ getPathAddress ::
   -- | The path to search for in the memory map
   [String] ->
   a
-getPathAddress mm = traverseTree annAbsTree1
+getPathAddress mm = traverseTree canonicalTree
+ where
+  canonicalTree = canonicalizeMemoryMapTree mm
+
+  showPathComponent :: PathComp -> String
+  showPathComponent (PathName _ s) = s
+  showPathComponent (PathUnnamed n) = show n
+
+  getTreeName :: (HasCallStack) => CanonicalTreeAbsNorm -> String
+  getTreeName (AnnInterconnect (_, absData) _ _) = showPathComponent (last absData.path)
+  getTreeName (AnnDeviceInstance _ _ name) = name
+
+  traverseTree :: (HasCallStack, Num a) => CanonicalTreeAbsNorm -> [String] -> a
+  traverseTree (AnnInterconnect _ _ _) [] = error "Empty path given!"
+  traverseTree (AnnInterconnect (_, absData) _ _) [name1] =
+    let name0 = showPathComponent (last absData.path)
+     in if name0 /= name1
+          then error [i|Mismatch on interconnect name! Expected #{name0}, found #{name1}.|]
+          else fromIntegral absData.absoluteAddr
+  traverseTree
+    (AnnInterconnect (_, absData) _ (fmap snd -> components))
+    (name1 : next : t) =
+      let name0 = showPathComponent (last absData.path)
+       in if name0 == name1
+            then case find (\tree -> next == getTreeName tree) components of
+              Just comp -> traverseTree comp t
+              Nothing -> error [i|Failed to find device #{next} in interconnect.|]
+            else error [i|Mismatch on interconnect name! Expected #{name0}, found #{name1}.|]
+  traverseTree (AnnDeviceInstance (_, absData) _ _) [] = fromIntegral absData.absoluteAddr
+  traverseTree (AnnDeviceInstance _ _ name) (a : b : c) =
+    error
+      [i|Cannot index into #{name} farther than #{a}, but path continues: #{ppShow $ b : c}|]
+  traverseTree (AnnDeviceInstance (oldName, absData) _ devName) [regName] =
+    case find (\regNL -> regName == regNL.name.name) devDef.registers of
+      Just regNL -> fromIntegral (absData.absoluteAddr + regNL.value.address)
+      Nothing -> error [i|Failed to find register #{regName} in device #{devName}|]
+   where
+    devDef :: DeviceDefinition
+    devDef =
+      case mm.deviceDefs M.!? fromMaybe devName oldName of
+        Just d -> d
+        Nothing -> error [i|Device definition for #{devName} not found in memory map.|]
+
+canonicalizeMemoryMapTree :: (HasCallStack) => MemoryMap -> CanonicalTreeAbsNorm
+canonicalizeMemoryMapTree mm = canonicalizeDevNames annAbsTree0
  where
   annRelTree = convert mm.tree
   annRelNormTree = normalizeRelTree annRelTree
   (annAbsTree0, _) = runMakeAbsolute mm.deviceDefs (0x0000_0000, 0xFFFF_FFFF) annRelNormTree
-  annAbsTree1 = canonicalizeDevNames annAbsTree0
 
-  canonicalizeDevNames :: (HasCallStack) => MemoryMapTreeAbsNorm -> MemoryMapTreeAbsNorm
-  canonicalizeDevNames self@(AnnDeviceInstance _ _ _) = self
+  canonicalizeDevNames :: (HasCallStack) => MemoryMapTreeAbsNorm -> CanonicalTreeAbsNorm
+  canonicalizeDevNames (AnnDeviceInstance a b c) = AnnDeviceInstance (Nothing, a) b c
   canonicalizeDevNames (AnnInterconnect ann srcLoc relsAndMMs) =
-    AnnInterconnect ann srcLoc $
+    AnnInterconnect (Nothing, ann) srcLoc $
       doCanonicalization (M.fromList $ zip deviceNames (repeat 0)) relsAndMMs
    where
     components :: [MemoryMapTreeAbsNorm]
@@ -98,54 +146,31 @@ getPathAddress mm = traverseTree annAbsTree1
       (HasCallStack) =>
       M.Map String Integer ->
       [(Integer, MemoryMapTreeAbsNorm)] ->
-      [(Integer, MemoryMapTreeAbsNorm)]
+      [(Integer, CanonicalTreeAbsNorm)]
     doCanonicalization _ [] = []
     doCanonicalization counts ((addr, AnnInterconnect ann1 srcLoc1 subtree) : t) =
-      (addr, AnnInterconnect ann1 srcLoc1 (bimap id canonicalizeDevNames <$> subtree))
+      ( addr
+      , AnnInterconnect (Nothing, ann1) srcLoc1 (bimap id canonicalizeDevNames <$> subtree)
+      )
         : doCanonicalization counts t
     doCanonicalization counts0 ((addr, AnnDeviceInstance ann1 srcLoc1 devName0) : t) =
-      (addr, AnnDeviceInstance ann1 srcLoc1 devName1) : doCanonicalization counts1 t
+      (addr, AnnDeviceInstance (extraAnn, ann1) srcLoc1 devName1) : doCanonicalization counts1 t
      where
-      needsSuffix = deviceNamesMult M.! devName0
-      curCount = counts0 M.! devName0
+      needsSuffix =
+        case deviceNamesMult M.!? devName0 of
+          Nothing ->
+            error [i|Device name #{devName0} not found in device names map during canonicalization.|]
+          Just b -> b
+
+      extraAnn = if needsSuffix then Just devName0 else Nothing
+
+      curCount =
+        case counts0 M.!? devName0 of
+          Nothing -> error [i|Device name #{devName0} not found in counts map during canonicalization.|]
+          Just c -> c
+
       devName1 = if needsSuffix then devName0 <> show curCount else devName0
       counts1 =
         if needsSuffix
           then M.adjust succ devName0 counts0
           else counts0
-
-  showPathComponent :: PathComp -> String
-  showPathComponent (PathName _ s) = s
-  showPathComponent (PathUnnamed n) = show n
-
-  getTreeName :: (HasCallStack) => MemoryMapTreeAbsNorm -> String
-  getTreeName (AnnInterconnect absData _ _) = showPathComponent (last absData.path)
-  getTreeName (AnnDeviceInstance _ _ name) = name
-
-  traverseTree :: (HasCallStack, Num a) => MemoryMapTreeAbsNorm -> [String] -> a
-  traverseTree (AnnInterconnect _ _ _) [] = error "Empty path given!"
-  traverseTree (AnnInterconnect absData _ _) [name1] =
-    let name0 = showPathComponent (last absData.path)
-     in if name0 /= name1
-          then error [i|Mismatch on interconnect name! Expected #{name0}, found #{name1}.|]
-          else fromIntegral absData.absoluteAddr
-  traverseTree
-    (AnnInterconnect absData _ (fmap snd -> components))
-    (name1 : next : t) =
-      let name0 = showPathComponent (last absData.path)
-       in if name0 == name1
-            then case find (\tree -> next == getTreeName tree) components of
-              Just comp -> traverseTree comp t
-              Nothing -> error [i|Failed to find device #{next} in interconnect.|]
-            else error [i|Mismatch on interconnect name! Expected #{name0}, found #{name1}.|]
-  traverseTree (AnnDeviceInstance absData _ _) [] = fromIntegral absData.absoluteAddr
-  traverseTree (AnnDeviceInstance _ _ name) (a : b : c) =
-    error
-      [i|Cannot index into #{name} farther than #{a}, but path continues: #{ppShow $ b : c}|]
-  traverseTree (AnnDeviceInstance absData _ devName) [regName] =
-    case find (\regNL -> regName == regNL.name.name) devDef.registers of
-      Just regNL -> fromIntegral (absData.absoluteAddr + regNL.value.address)
-      Nothing -> error [i|Failed to find register #{regName} in device #{devName}|]
-   where
-    devDef :: DeviceDefinition
-    devDef = mm.deviceDefs M.! devName
