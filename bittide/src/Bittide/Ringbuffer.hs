eeb606f326fb165ea31deb0429790d6046d3866d
Lucas Bollen <lucas@qbaylogic.com>
2026-02-24 14:05:55 +0100

    Add `Ringbuffer` components and test

--- bittide/src/Bittide/Ringbuffer.hs
+++ bittide/src/Bittide/Ringbuffer.hs
@@ -12,20 +12,31 @@ import Clash.Class.BitPackC
 import Data.Maybe
 import Protocols
 import Protocols.Extra
+import Protocols.Idle
+import Protocols.MemoryMap (Access (..))
 
 import GHC.Stack (HasCallStack)
 import qualified Protocols.Df as Df
 import qualified Protocols.Df.Extra as Df
-import Protocols.Idle
 import qualified Protocols.ReqResp as ReqResp
 import qualified Protocols.Wishbone.Extra as Wb
 
 import Protocols.MemoryMap.Registers.WishboneStandard (
+  RegisterConfig (..),
   addressableBytesWb,
   deviceWb,
   matchEndianness,
   registerConfig,
  )
+
+partitionRamOp ::
+  (NFDataX mask, NFDataX bv) =>
+  RamOp n (mask, bv) ->
+  Either (Index n) (Index n, mask, bv)
+partitionRamOp = \case
+  RamRead addr -> Left addr
+  RamWrite addr (mask, bv) -> Right (addr, mask, bv)
+  RamNoOp -> deepErrorX "RamNoOp should not be in a ReqResp stream"
 
 {-# OPAQUE transmitRingbufferWb #-}
 transmitRingbufferWb ::
@@ -53,15 +64,20 @@ transmitRingbufferWb ::
 transmitRingbufferWb primitive SNat = circuit $ \wb -> do
   [wb0] <- deviceWb "TransmitRingbuffer" <| fmapC (matchEndianness <| Wb.increaseBuswidth d1) -< wb
   reqresp <- addressableBytesWb @memDepth regConfig -< wb0
-  (reads, writes0) <- ReqResp.partitionEithers -< reqresp
+  (reads, writes0) <- ReqResp.partition partitionRamOp -< reqresp
   writes1 <- ReqResp.toDfs -< (writes0, writeAcks)
   writeAcks <- Df.pure 0
   idleSink -< reads
   readAddress <- Df.iterate (satSucc SatWrap) 0
   applyC (fmap $ fromMaybe 0) id <| Df.toMaybe <| ram -< (readAddress, writes1)
  where
-  regConfig = registerConfig "data"
+  regConfig =
+    (registerConfig "data")
+      { access = WriteOnly
+      , description = "Buffer that continuously transmits frames to the network"
+      }
   ram = withClockResetEnable hasClock hasReset enableGen (Df.fromBlockRamWithMask primitive)
+
 {-# OPAQUE receiveRingbufferWb #-}
 receiveRingbufferWb ::
   forall dom aw memDepth.
@@ -96,11 +112,15 @@ receiveRingbufferWb primitive SNat = circuit $ \(wb, Fwd frames) -> do
         (matchEndianness <| Wb.increaseBuswidth d1)
       -< wb
   reqresp <- addressableBytesWb @memDepth regConfig -< wb0
-  (reads, cpuWrites) <- ReqResp.partitionEithers -< reqresp
+  (reads, cpuWrites) <- ReqResp.partition partitionRamOp -< reqresp
   readAddress <- ReqResp.toDfs -< (reads, readData)
   idleSink -< cpuWrites
   readData <- ram -< (readAddress, Fwd writes)
   idC -< ()
  where
-  regConfig = registerConfig "data"
+  regConfig =
+    (registerConfig "data")
+      { access = ReadOnly
+      , description = "Buffer that continuously receives frames from the network"
+      }
   ram = withClockResetEnable hasClock hasReset enableGen (Df.fromBlockRam primitive)
