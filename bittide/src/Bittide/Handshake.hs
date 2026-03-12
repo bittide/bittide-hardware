26cc33af0bc4538234cd0545a11da63b85d8a277
Nathan <nathan@qbaylogic.com>
2026-03-12 13:40:59 +0100

    Addresses PR feedback ; adds hh test for ugn capture

--- bittide/src/Bittide/Handshake.hs
+++ bittide/src/Bittide/Handshake.hs
@@ -3,60 +3,61 @@
 -- SPDX-License-Identifier: Apache-2.0
 module Bittide.Handshake where
 
-import Clash.Annotations.BitRepresentation
-import Clash.Annotations.BitRepresentation.Deriving
 import Clash.Prelude
 
-type BittideWord = Vec 8 (BitVector 8)
-
-magicConstant :: BitVector (7 * 8)
+magicConstant :: (KnownNat n) => BitVector (n * 8)
 magicConstant = 0xdeadbeef
 
-{- | Meta information send along with the PRBS and alignment symbols. See module
-documentation for more information.
+{- | Meta information send along with the PRBS and alignment symbols. See
+"Bittide.Transceiver" documentation for more information.
 -}
 data Meta = Meta
   { readyToReceive :: Bool
   -- ^ Ready to receive user data
   , readyToTransmit :: Bool
   -- ^ Ready to transmit user data
-  , lastTransmit :: Bool
+  , lastPrbsWord :: Bool
   -- ^ Next word will be user data
+  , padding :: Unsigned 5
+  -- ^ Padding up to 1 byte
   }
-  deriving (Generic, NFDataX, Eq, Show)
+  deriving (Generic, NFDataX, BitPack, Eq, Show)
 
-{-# ANN
-  module
-  ( DataReprAnn
-      $(liftQ [t|Meta|])
-      8
-      -- Test comment
-      [ ConstrRepr
-          'Meta
-          0b0001_1111 -- constructorMask
-          0b0000_0000 -- constructorValue
-          [ 0b1000_0000 -- field1Mask
-          , 0b0100_0000 -- field2Mask
-          , 0b0010_0000 -- field3Mask
-          ]
-      ]
-  )
-  #-}
-deriveBitPack [t|Meta|]
-
-metadataFromWord ::
-  BittideWord ->
+{- | Given a @BitVector n@, where @n@ is a positive multiple of @8@,
+convert it to a @Just metadata@ (or @Nothing@ if the BitVector does
+not fit the metadata format).
+-}
+wordToMetadata ::
+  (KnownNat n) =>
+  BitVector ((n + 1) * 8) ->
   Maybe Meta
-metadataFromWord word =
-  -- Can also use splitAt
-  if (tail (reverse word) == reverse (unpack magicConstant))
-    then Just $ unpack $ last word
-    else Nothing
+wordToMetadata word =
+  let (header, meta) = split word
+   in if (header == magicConstant)
+        then Just $ unpack meta
+        else Nothing
 
+{-
+wordToMetadata' ::
+  forall n.
+  (KnownNat n, 2 <= n) =>
+  BitVector (n*8) ->
+  Maybe Meta
+wordToMetadata' word =
+  let (header, meta) = (leToPlus @1 @n split) word in
+  if (header == magicConstant)
+    then Just $ unpack meta
+    else Nothing
+-}
+
+{- | Given a @Meta@, convert it to a @BitVector n@, where @n@ is a positive
+multiple of @8@.
+-}
 metadataToWord ::
+  (KnownNat n) =>
   Meta ->
-  BittideWord
-metadataToWord meta = unpack $ magicConstant ++# (pack meta)
+  BitVector ((n + 1) * 8)
+metadataToWord meta = magicConstant ++# (pack meta)
 
 stickyTrue ::
   (HiddenClockResetEnable dom) =>
@@ -67,44 +68,43 @@ stickyTrue s = s'
   s' = (register False s') .||. s
 
 handshake ::
-  (KnownDomain dom, HiddenClock dom) =>
-  Reset dom ->
-  Enable dom ->
+  (KnownNat n) =>
+  (HiddenClockResetEnable dom) =>
   -- | From transceiver
-  Signal dom BittideWord ->
+  Signal dom (BitVector ((n + 1) * 8)) ->
   -- | From ugn capture
-  Signal dom BittideWord ->
+  Signal dom (BitVector ((n + 1) * 8)) ->
   -- | Read, Write enable regs
   Signal dom (Bool, Bool) ->
   -- | To UGN capture
-  ( Signal dom BittideWord
+  ( Signal dom (BitVector ((n + 1) * 8))
   , -- \| To transceiver
-    Signal dom BittideWord
-  , -- \| To UGN send/receive
+    Signal dom (BitVector ((n + 1) * 8))
+  , -- \| Tuple of (txLast, rxLast), which indicates to ugnCapture when to send/receive the UGN.
     Signal dom (Bool, Bool)
   )
-handshake rst en rxWordIn txWordIn regs = (rxWordOut, txWordOut, bundle (txLast, rxLast))
+handshake rxWordIn txWordIn regs = (rxWordOut, txWordOut, bundle (txLast, rxLast))
  where
-  neighborMetadata = metadataFromWord <$> rxWordIn
+  neighborMetadata = wordToMetadata <$> rxWordIn
 
-  (metadata, rxLastS) = unbundle $ withReset rst $ withEnable en $ handshakeStateMachine neighborMetadata regs
+  (metadata, rxLastS) = unbundle $ handshakeStateMachine neighborMetadata regs
 
-  newTxLast = withReset rst $ withEnable en $ stickyTrue $ isRising False metadata.lastTransmit
-  newRxLast = withReset rst $ withEnable en $ stickyTrue $ isRising False rxLastS
+  txLast = isRising False $ stickyTrue $ metadata.lastPrbsWord
+  rxLast = isRising False $ stickyTrue $ rxLastS
 
-  (txLast, rxLast) =
-    ( withReset rst $ withEnable en $ isRising False newTxLast
-    , withReset rst $ withEnable en $ isRising False newRxLast
-    )
+  handshakeFinished = not <$> (stickyTrue txLast)
 
   -- Words out
   txWordOut =
     mux
-      (withReset rst $ withEnable en $ register False $ not <$> newTxLast)
+      (register False handshakeFinished)
       (metadataToWord <$> metadata)
       txWordIn
   rxWordOut = rxWordIn
 
+{- | The following properties should be observed by the state machine
+1) A txReady should only be sent after both rxReady and rxNeighborReady
+-}
 handshakeStateMachine ::
   (HiddenClockResetEnable dom) =>
   -- | Neighbor state
@@ -113,21 +113,20 @@ handshakeStateMachine ::
   Signal dom (Bool, Bool) ->
   -- | New state of node
   Signal dom (Meta, Bool)
-handshakeStateMachine neighborState regs = state
+handshakeStateMachine neighborState regs = moore updateState id initState $ bundle (neighborState, regs)
  where
-  state = register initState $ updateState <$> state <*> neighborState <*> regs
+  initState = (Meta False False False 0, False)
 
-  initState = (Meta False False False, False)
-
-  updateState hState _neighborState@Nothing _regs = hState
+  updateState hState (Nothing, _) = hState
   updateState
     -- \| Current state
-    (Meta txReady rxReady txLast, rxLast)
+    (Meta txReady rxReady txLast pad, rxLast)
     -- \| Neighbor state
-    (Just (Meta neighborTxReady neighborRxReady neighborTxLast))
-    -- \| Local registers
-    (txEn, rxEn) =
-      ((Meta newTxReady newRxReady newTxLast), newRxLast)
+    ( (Just (Meta neighborTxReady neighborRxReady neighborTxLast _))
+      , -- \| Local registers
+        (txEn, rxEn)
+      ) =
+      ((Meta newTxReady newRxReady newTxLast pad), newRxLast)
      where
       newTxReady = txReady || txEn
       newRxReady = rxReady || ((rxEn && txEn) && neighborTxReady)
--- bittide/tests/Tests/Handshake.hs
+++ bittide/tests/Tests/Handshake.hs
@@ -15,33 +15,45 @@ import Test.Tasty.HUnit (Assertion, assertEqual, testCase)
 import Test.Tasty.Hedgehog (testProperty)
 import Tests.Shared (SomeSNat (..), someSNat)
 
--- Generate a finite list that starts False, switches to True after a random
--- number of cycles in range `[minK, maxK]`, and stays True.
---
--- The list is finite so that hedgehog does not try to print/shrink an infinite
--- data structure.
+{- | Number of bytes in a BittideWord. Meant to be adjustable based on expected
+workload size
+-}
+type BittideWordSize = 8
+
+{- | To work around Clash's constraint solver, we declare all functions to work on
+@n+1@, so we must declare the size to be the actual word size - 1.
+-}
+type BittideWordSizeAdj = BittideWordSize - 1
+
+{- Generate a finite list that starts False, switches to True after a random
+number of cycles in range @[minK, maxK]@, and stays True.
+
+The list is finite so that hedgehog does not try to print/shrink an infinite
+data structure.
+-}
 genStickyTrue :: Int -> Int -> Int -> Gen [Bool]
 genStickyTrue minK maxK len = do
   k <- Gen.int (Range.linear minK maxK)
   let l = List.replicate k False List.++ List.replicate (len - k) True
   pure l
 
--- Generate a finite list that starts False, switches to True for once cycle after
--- a random number of cycles in range `[minK, maxK]`, and then stays False.
---
--- The list is finite so that hedgehog does not try to print/shrink an infinite
--- data structure.
+{- Generate a finite list that starts False, switches to True for once cycle after
+a random number of cycles in range @[minK, maxK]@, and then stays False.
+
+The list is finite so that hedgehog does not try to print/shrink an infinite
+data structure.
+-}
 genOneshotTrue :: Int -> Int -> Int -> Gen [Bool]
 genOneshotTrue minK maxK len = do
   k <- Gen.int (Range.linear minK maxK)
   let l = List.replicate k False List.++ List.replicate 1 True List.++ List.replicate (len - k - 1) False
   pure l
 
--- Output a list that is always true as soon as the input list has a True valie.
+-- | Output a list that is always true as soon as the input list has a True value.
 stickyTrue :: [Bool] -> [Bool]
 stickyTrue = List.drop 1 . List.scanl (||) False
 
--- Create n registers in a row, each with initial value `a`.
+-- | Create @n@ registers in a row, each with initial value @a@.
 registerN ::
   forall n a dom.
   (KnownNat n) =>
@@ -53,33 +65,55 @@ registerN ::
   Signal dom a
 registerN _ initVal s = register initVal $ last $ iterateI @(n + 1) (register initVal) s
 
--- Connect two handshake state machines together, with a variable number of cycle delay
--- between the two state machines. The cycle delays need not be symmetrical.
+{- Connect two handshake state machines together, with a variable number of cycle delay
+between the two state machines. The cycle delays need not be symmetrical.
+-}
 handshakesWithDelays ::
-  forall dom m n.
+  forall dom n delayAB delayBA.
   (HiddenClockResetEnable dom) =>
-  (KnownNat m, KnownNat n) =>
-  SNat m ->
-  SNat n ->
+  (KnownNat delayAB, KnownNat delayBA) =>
+  (KnownNat n) =>
+  SNat delayAB ->
+  SNat delayBA ->
   Signal dom (Bool, Bool) ->
-  Signal dom BittideWord ->
+  Signal dom (BitVector ((n + 1) * 8)) ->
   Signal dom (Bool, Bool) ->
-  Signal dom BittideWord ->
-  ( Signal dom BittideWord
+  Signal dom (BitVector ((n + 1) * 8)) ->
+  ( Signal dom (BitVector ((n + 1) * 8))
   , Signal dom (Bool, Bool)
-  , Signal dom BittideWord
+  , Signal dom (BitVector ((n + 1) * 8))
   , Signal dom (Bool, Bool)
   )
 handshakesWithDelays delayAtoB delayBtoA regsA wordA regsB wordB = (wordA', regsA', wordB', regsB')
  where
-  rst = resetGen @dom
-  en = enableGen @dom
-  (wordA', wordToB, regsA') = handshake rst en wordToADelayed wordA regsA
-  (wordB', wordToA, regsB') = handshake rst en wordToBDelayed wordB regsB
+  (wordA', wordToB, regsA') = handshake @n wordToADelayed wordA regsA
+  (wordB', wordToA, regsB') = handshake @n wordToBDelayed wordB regsB
 
   wordToADelayed = registerN delayBtoA initValue wordToA
   wordToBDelayed = registerN delayAtoB initValue wordToB
   initValue = unpack 0
+
+ugnSender ::
+  forall dom n.
+  (KnownNat n) =>
+  (HiddenClockResetEnable dom) =>
+  Signal dom Bool ->
+  Signal dom (BitVector ((n + 1) * 8))
+ugnSender = moore updateState outputUserData initState
+ where
+  dummyWord = 0x00 :: BitVector ((n + 1) * 8)
+  ugnWord = 0xaa :: BitVector ((n + 1) * 8)
+  userDataWord = 0xff :: BitVector ((n + 1) * 8)
+
+  initState = (False, False)
+
+  updateState (True, False) _ = (True, True)
+  updateState (False, False) isLast = (isLast, False)
+  updateState state _ = state
+
+  outputUserData (False, _) = dummyWord -- Output dummy data since it'll be overwritten by metadata words
+  outputUserData (True, False) = ugnWord -- Output UGN
+  outputUserData (True, True) = userDataWord -- Output user data
 
 -- Test the property that no handshake can transition to user data before all enables from both sides are asserted
 testEnableSafety :: Property
@@ -95,15 +129,21 @@ testEnableSafety = property $ do
 
   let regsA = bundle (fromList txEnA, fromList rxEnA)
       regsB = bundle (fromList txEnB, fromList rxEnB)
-      wordA = pure (unpack 0)
-      wordB = pure (unpack 0)
+      wordA = pure 0
+      wordB = pure 0
 
       -- Simulate the handshake
       (_outA, lastTxRxA, _outB, lastTxRxB) =
         case (someSNat delayAtoB, someSNat delayBtoA) of
           (SomeSNat (_a@SNat :: SNat delayAtoB), SomeSNat (_b@SNat :: SNat delayBtoA)) ->
             withClockResetEnable clockGen resetGen enableGen
-              $ handshakesWithDelays @System (SNat @delayAtoB) (SNat @delayBtoA) regsA wordA regsB wordB
+              $ handshakesWithDelays @System @BittideWordSizeAdj
+                (SNat @delayAtoB)
+                (SNat @delayBtoA)
+                regsA
+                wordA
+                regsB
+                wordB
 
       (lastTxA, lastRxA) = unbundle lastTxRxA
       (lastTxB, lastRxB) = unbundle lastTxRxB
@@ -144,15 +184,21 @@ testHandshakeLiveness = property $ do
 
   let regsA = bundle (fromList txEnA, fromList rxEnA)
       regsB = bundle (fromList txEnB, fromList rxEnB)
-      wordA = pure (unpack 0)
-      wordB = pure (unpack 0)
+      wordA = pure 0
+      wordB = pure 0
 
       -- Simulate the handshake
       (_outA, lastTxRxA, _outB, lastTxRxB) =
         case (someSNat delayAtoB, someSNat delayBtoA) of
           (SomeSNat (_a@SNat :: SNat delayAtoB), SomeSNat (_b@SNat :: SNat delayBtoA)) ->
             withClockResetEnable clockGen resetGen enableGen
-              $ handshakesWithDelays @System (SNat @delayAtoB) (SNat @delayBtoA) regsA wordA regsB wordB
+              $ handshakesWithDelays @System @BittideWordSizeAdj
+                (SNat @delayAtoB)
+                (SNat @delayBtoA)
+                regsA
+                wordA
+                regsB
+                wordB
 
       (lastTxA, lastRxA) = unbundle lastTxRxA
       (lastTxB, lastRxB) = unbundle lastTxRxB
@@ -170,15 +216,78 @@ testHandshakeLiveness = property $ do
   -- Assertions
   numAssertTrue === (1, 1, 1, 1)
 
+-- Test that when @lastRx@ is asserted, the next cycle will be the UGN, therefore ensuring there
+-- are not timing mismatches in the handshake signals.
+testHandshakeUgnCapture :: Property
+testHandshakeUgnCapture = property $ do
+  let testLength = 100
+  let enableBy = 20
+  txEnA <- forAll $ genStickyTrue 0 enableBy testLength
+  rxEnA <- forAll $ genStickyTrue 0 enableBy testLength
+  txEnB <- forAll $ genStickyTrue 0 enableBy testLength
+  rxEnB <- forAll $ genStickyTrue 0 enableBy testLength
+
+  delayAtoB <- forAll $ Gen.integral (Range.linear 1 19)
+  delayBtoA <- forAll $ Gen.integral (Range.linear 1 19)
+
+  let regsA = bundle (fromList txEnA, fromList rxEnA)
+      regsB = bundle (fromList txEnB, fromList rxEnB)
+      wordA = (withClockResetEnable clockGen resetGen enableGen $ register 0 $ ugnSender lastTxA)
+      wordB = (withClockResetEnable clockGen resetGen enableGen $ register 0 $ ugnSender lastTxB)
+
+      -- Simulate the handshake
+      (outA, lastTxRxA, outB, lastTxRxB) =
+        case (someSNat delayAtoB, someSNat delayBtoA) of
+          (SomeSNat (_a@SNat :: SNat delayAtoB), SomeSNat (_b@SNat :: SNat delayBtoA)) ->
+            withClockResetEnable clockGen resetGen enableGen
+              $ handshakesWithDelays @System @BittideWordSizeAdj
+                (SNat @delayAtoB)
+                (SNat @delayBtoA)
+                regsA
+                wordA
+                regsB
+                wordB
+
+      (lastTxA, lastRxA) = unbundle lastTxRxA
+      (lastTxB, lastRxB) = unbundle lastTxRxB
+
+      -- Capture how many times the UGN and lastRx are the true on 1) BOTH cycles 2) EITHER cycles
+      capturedUgnA =
+        (outA .==. pure 0xaa)
+          .&&. (withClockResetEnable clockGen resetGen enableGen $ register False lastRxA)
+      capturedUgnA' =
+        (outA .==. pure 0xaa)
+          .||. (withClockResetEnable clockGen resetGen enableGen $ register False lastRxA)
+      capturedUgnB =
+        (outB .==. pure 0xaa)
+          .&&. (withClockResetEnable clockGen resetGen enableGen $ register False lastRxB)
+      capturedUgnB' =
+        (outB .==. pure 0xaa)
+          .||. (withClockResetEnable clockGen resetGen enableGen $ register False lastRxB)
+
+      finiteTest = List.take (testLength - 10)
+      countNumAssertTrue = List.sum . List.map fromEnum
+      ugnCapturedA = countNumAssertTrue $ finiteTest $ sample capturedUgnA
+      ugnCapturedA' = countNumAssertTrue $ finiteTest $ sample capturedUgnA'
+      ugnCapturedB = countNumAssertTrue $ finiteTest $ sample capturedUgnB
+      ugnCapturedB' = countNumAssertTrue $ finiteTest $ sample capturedUgnB'
+
+  -- Assertions
+  ugnCapturedA === 1
+  ugnCapturedA' === 1
+  ugnCapturedB === 1
+  ugnCapturedB' === 1
+
 testMetadataParsing :: Assertion
 testMetadataParsing = do
-  let regularWord = unpack 0 :: BittideWord
-  let metadataWord = unpack $ magicConstant ++# 0b1110_0000
-  let metadata = Meta True True True
+  let regularWord = unpack 0 :: (BitVector 64)
+  let metadataWord = unpack $ magicConstant @BittideWordSizeAdj ++# (0b1110_0000 :: BitVector 8)
+  let metadata = Meta True True True 0
 
-  assertEqual "Parsing regular word returns Nothing" Nothing (metadataFromWord regularWord)
-  assertEqual "Parsing metadata word returns Just xxx" (Just metadata) (metadataFromWord metadataWord)
-  assertEqual "id ~ fromWord . toWord" (Just metadata) (metadataFromWord $ metadataToWord metadata)
+  assertEqual "Parsing regular word returns Nothing" Nothing (wordToMetadata regularWord)
+  assertEqual "Parsing metadata word returns Just xxx" (Just metadata) (wordToMetadata metadataWord)
+
+-- assertEqual "id ~ fromWord . toWord" (Just metadata) (wordToMetadata $ metadataToWord metadata)
 
 tests :: TestTree
 tests =
@@ -186,5 +295,6 @@ tests =
     "Bittide.Handshake"
     [ testCase "testMetadataParsing" testMetadataParsing
     , testProperty "testEnableSafety" testEnableSafety
+    , testProperty "testHandshakeUgnCapture" testHandshakeUgnCapture
     , testProperty "testHandshakeLiveness" testHandshakeLiveness
     ]
