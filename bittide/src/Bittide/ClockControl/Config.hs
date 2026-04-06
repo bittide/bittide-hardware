97a052f25b45fb4fd717fc6140433fa5093a7d2e
Martijn Bastiaan <martijn@qbaylogic.com>
2026-04-06 11:54:30 +0200

    gain: 9e-8

--- bittide/src/Bittide/ClockControl/Config.hs
+++ bittide/src/Bittide/ClockControl/Config.hs
@@ -58,7 +58,7 @@ defCcConf :: Int -> CcConf Topology
 defCcConf nLinks =
   CcConf
     { topology = Topology.complete nLinks
-    , callisto = CallistoConfig{waitTime = Nothing, gain = 8e-8}
+    , callisto = CallistoConfig{waitTime = Nothing, gain = 9e-8}
     }
 
 -- | Default name of the clock control JSON configuration file.
