7b05452b132b584e27db68038f82cbdf06d30512
Lucas Bollen <lucas@qbaylogic.com>
2026-04-09 10:20:01 +0200

    Add wbStorage compliant memory model

--- bittide/tests/Tests/Clash/Protocols/Wishbone/Extra.hs
+++ bittide/tests/Tests/Clash/Protocols/Wishbone/Extra.hs
@@ -13,9 +13,8 @@ import Bittide.DoubleBufferedRam (
  )
 import Clash.Prelude (withClockResetEnable)
 import Data.Map (Map)
-import Data.Maybe (fromMaybe)
-import Hedgehog (Gen, Property)
-import Hedgehog.Internal.Property (property)
+import Data.String.Interpolate (i)
+import Hedgehog (Gen, Property, property)
 import Protocols
 import Protocols.Hedgehog (ExpectOptions (eoResetCycles), defExpectOptions, eoSampleMax)
 import Protocols.MemoryMap (unMemmap)
@@ -25,28 +24,61 @@ import Protocols.Wishbone.Standard.Hedgehog (
   WishboneMasterRequest (Read, Write),
   wishbonePropWithModel,
  )
-import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.Hedgehog (testPropertyNamed)
+import Test.Tasty (TestTree)
+import Test.Tasty.Hedgehog (testProperty)
+import Test.Tasty.TH (testGroupGenerator)
 
+import qualified Data.List as L
 import qualified Data.Map as Map
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
 type AddressWidth = 4
-
-tests :: TestTree
-tests =
-  testGroup
-    "Tests.Clash.Protocols.Wishbone.Extra"
-    [ testPropertyNamed
-        "xpmCdcHandshakeWb preserves wishbone transactions"
-        "xpmCdcHandshakeWb"
-        prop_xpmCdcHandshakeWb
-    ]
-
-mergeWithMask :: BitVector 32 -> BitVector 32 -> BitVector 4 -> BitVector 32
+mergeWithMask ::
+  forall n m.
+  (KnownNat n, KnownNat m) =>
+  BitVector (n * m) ->
+  BitVector (n * m) ->
+  BitVector n ->
+  BitVector (n * m)
 mergeWithMask (unpack -> old) (unpack -> new) (unpack -> mask) =
-  pack (mux @(Vec 4) @(BitVector 8) mask new old)
+  pack (mux @(Vec n) @(BitVector m) mask new old)
+
+-- | Generic memory model for a Wishbone slave.
+memoryModel ::
+  forall aw dw.
+  (KnownNat aw, KnownNat dw) =>
+  Int ->
+  WishboneMasterRequest aw dw ->
+  WishboneS2M dw ->
+  Map (BitVector aw) (BitVector (dw * 8)) ->
+  Either String (Map (BitVector aw) (BitVector (dw * 8)))
+memoryModel maxAddr req@(Write addr mask dat) resp mem
+  | addrInt <= maxAddr && resp.acknowledge = Right newMem
+  | resp.acknowledge = Left [i|Received acknowledge for out-of-range address #{(req, resp)}|]
+  | maxAddr > addrRange = Left [i|Supplied depth #{maxAddr} exceeds addressable range #{addrRange}|]
+  | resp.err = Right mem
+  | otherwise = Left [i|Expected acknowledge for valid write #{(req, resp)}|]
+ where
+  addrInt = fromIntegral addr
+  addrRange :: Int
+  addrRange = fromIntegral (maxBound :: BitVector aw)
+  oldData = mem Map.! addr
+  newData = mergeWithMask oldData dat mask
+  newMem = Map.insert addr newData mem
+memoryModel maxAddr req@(Read addr mask) resp mem
+  | addrInt <= maxAddr && resp.acknowledge && match = Right mem
+  | addrInt <= maxAddr && resp.acknowledge = Left "Data mismatch"
+  | resp.acknowledge = Left [i|Received acknowledge for out-of-range address #{(req, resp)}|]
+  | maxAddr > addrRange = Left [i|Supplied depth #{maxAddr} exceeds addressable range #{addrRange}|]
+  | resp.err = Right mem
+  | otherwise = Left [i|Expected acknowledge for valid read #{(req, resp)}|]
+ where
+  addrInt = fromIntegral addr
+  addrRange :: Int
+  addrRange = fromIntegral (maxBound :: BitVector aw)
+  match = mergeWithMask 0 (resp.readData) mask == mergeWithMask 0 memData mask
+  memData = mem Map.! addr
 
 {- | Test that xpmCdcHandshakeWb correctly transfers Wishbone transactions
 between clock domains without data corruption. The test connects the CDC
@@ -55,41 +87,29 @@ work correctly.
 -}
 prop_xpmCdcHandshakeWb :: Property
 prop_xpmCdcHandshakeWb = property $ do
+  let
+    dut :: Circuit (Wishbone System 'Standard AddressWidth 4) ()
+    dut = xpmCdcHandshakeWb clk rst clk rst |> dutMem
+
+    dutMem :: Circuit (Wishbone System 'Standard AddressWidth 4) ()
+    dutMem =
+      withClockResetEnable clk rst ena
+        $ unMemmap
+        $ wbStorage "test" (SNat @(2 ^ (AddressWidth - 1))) (Just (Vec (repeat 0)))
+
   withClockResetEnable clk rst ena
     $ wishbonePropWithModel
       @System
       defExpectOptions{eoSampleMax = 10_000, eoResetCycles = 0}
-      model
+      (memoryModel 7)
       dut
       genInputs
-      Map.empty
+      (Map.fromList (L.zip [0 .. lastAddress] (L.repeat 0)))
  where
+  lastAddress = natToNum @(2 ^ (AddressWidth - 1)) -- Less elements to also test error propagation.
   clk = clockGen
   rst = noReset
   ena = enableGen
-
-  -- Behavioral model: track memory state
-  model ::
-    WishboneMasterRequest AddressWidth 4 ->
-    WishboneS2M 4 ->
-    Map (BitVector AddressWidth) (BitVector 32) ->
-    Either String (Map (BitVector AddressWidth) (BitVector 32))
-  model _ resp _mem | resp.err = Left "Unexpected bus error"
-  --
-  model (Read addr _mask) resp mem
-    | resp.acknowledge =
-        let expected = fromMaybe 0 (Map.lookup addr mem)
-         in if resp.readData == expected then Right mem else Left "Data mismatch"
-  --
-  model (Write addr mask dat) resp mem
-    | resp.acknowledge =
-        let
-          old = fromMaybe 0 (Map.lookup addr mem)
-          new = mergeWithMask old dat mask
-         in
-          Right (Map.insert addr new mem)
-  --
-  model _ _ mem = Right mem
 
   -- Generate wishbone requests
   genInputs :: Gen [WishboneMasterRequest AddressWidth 4]
@@ -102,11 +122,6 @@ prop_xpmCdcHandshakeWb = property $ do
   genBoundedIntegral :: forall a. (Integral a, Bounded a) => Gen a
   genBoundedIntegral = Gen.integral Range.constantBounded
 
-  dut :: Circuit (Wishbone System 'Standard AddressWidth 4) ()
-  dut = xpmCdcHandshakeWb clk rst clk rst |> dutMem
 
-  dutMem :: Circuit (Wishbone System 'Standard AddressWidth 4) ()
-  dutMem =
-    withClockResetEnable clk rst ena
-      $ unMemmap
-      $ wbStorage "test" (SNat @(2 ^ AddressWidth)) (Just (Vec (repeat 0)))
+tests :: TestTree
+tests = $(testGroupGenerator)
