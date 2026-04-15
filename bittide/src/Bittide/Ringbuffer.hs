757097dfdccde61c17bf297b04ce0aa65c352a61
Lucas Bollen <lucas@qbaylogic.com>
2026-04-15 11:40:20 +0200

    Add `receiveRingbuffer` and `transmitRingbuffer`

--- bittide/src/Bittide/Ringbuffer.hs
+++ bittide/src/Bittide/Ringbuffer.hs
@@ -12,23 +12,30 @@ import Clash.Class.BitPackC
 import Data.Maybe
 import Protocols
 import Protocols.Extra
+import Protocols.Idle
+import Protocols.MemoryMap (Access (..))
 
 import GHC.Stack (HasCallStack)
 import qualified Protocols.Df as Df
 import qualified Protocols.Df.Extra as Df
-import Protocols.Idle
 import qualified Protocols.ReqResp as ReqResp
 import qualified Protocols.Wishbone.Extra as Wb
 
 import Protocols.MemoryMap.Registers.WishboneStandard (
+  BusActivity,
+  DeviceConfig (registered),
+  RegisterConfig (..),
   addressableBytesWb,
-  deviceWb,
-  matchEndianness,
+  busActivityWrite,
+  deviceConfig,
+  deviceWbI,
   registerConfig,
+  registerWbDfI,
+  registerWbI,
  )
 
-{-# OPAQUE transmitRingbufferWb #-}
-transmitRingbufferWb ::
+{-# OPAQUE transmitRingbuffer #-}
+transmitRingbuffer ::
   forall dom aw memDepth.
   ( HasCallStack
   , HiddenClockResetEnable dom
@@ -38,34 +45,53 @@ transmitRingbufferWb ::
   , ?byteOrder :: ByteOrder
   ) =>
   -- | Primitive to use for the underlying memory.
-  ( Enable dom ->
-    Signal dom (Index memDepth) ->
-    Signal dom (Maybe (Index memDepth, Bytes 8)) ->
-    Signal dom (BitVector 8) ->
-    Signal dom (Bytes 8)
+  ( Signal dom (RamOp memDepth (BitVector 8, BitVector 64)) ->
+    Signal dom (RamOp memDepth (BitVector 8, BitVector 64)) ->
+    (Signal dom (BitVector 64), Signal dom (BitVector 64))
   ) ->
   -- | Configuration for the ringbuffer.
   SNat memDepth ->
   Circuit
     (BitboneMm dom aw)
     (CSignal dom (BitVector 64))
-transmitRingbufferWb primitive SNat = circuit $ \wb -> do
-  [wb0] <-
-    deviceWb (deviceConfig "TransmitRingbuffer")
-      <| fmapC (matchEndianness <| Wb.increaseBuswidth d1)
+transmitRingbuffer primitive SNat = circuit $ \wb -> do
+  [wb0, wb1] <-
+    deviceWbI (deviceConfig "TransmitRingbuffer"){registered = False}
+      <| fmapC (Wb.increaseBuswidth d1)
       -< wb
   reqresp <- addressableBytesWb @memDepth regConfig -< wb0
-  (reads, writes0) <- ReqResp.partitionEithers -< reqresp
-  writes1 <- ReqResp.toDfs -< (writes0, writeAcks)
+  (Fwd (transmitEnable, _)) <-
+    registerWbI @_ @_ @8 transmitEnableConfig False -< (wb1, Fwd (pure Nothing))
+  (reads0, writes0) <- ReqResp.partitionEithers -< reqresp
+
+  reads1 <- applyC (fmap (fmap RamRead)) id <| ReqResp.toDfs -< (reads0, rightDat)
+  writes1 <- applyC (fmap (fmap writeToRamOp)) id <| ReqResp.toDfs -< (writes0, writeAcks)
   writeAcks <- Df.pure 0
-  idleSink -< reads
-  readAddress <- Df.iterate (satSucc SatWrap) 0
-  applyC (fmap $ fromMaybe 0) id <| Df.toMaybe <| ram -< (readAddress, writes1)
+  cpuRamOp <- Df.roundrobinCollect Df.Parallel -< [reads1, writes1]
+
+  readRamOp <-
+    Df.filterS (fmap const transmitEnable)
+      <| applyC (fmap (fmap RamRead)) id
+      <| Df.iterate (satSucc SatWrap) 0
+  (leftDat, rightDat) <- ram -< (readRamOp, cpuRamOp)
+  applyC (fmap $ fromMaybe 0) id <| Df.toMaybe -< leftDat
  where
-  regConfig = registerConfig "data"
-  ram = withClockResetEnable hasClock hasReset enableGen (Df.fromBlockRamWithMask primitive)
-{-# OPAQUE receiveRingbufferWb #-}
-receiveRingbufferWb ::
+  transmitEnableConfig =
+    (registerConfig "enable")
+      { access = WriteOnly
+      , description = "Enable signal that controls transmission of frames to the network"
+      }
+  regConfig =
+    (registerConfig "data")
+      { access = ReadWrite
+      , description = "Buffer that continuously transmits frames to the network"
+      }
+  -- ram = withClockResetEnable hasClock hasReset enableGen (Df.fromBlockRamWithMask primitive)
+  ram = Df.fromDualPortedBramWithMask primitive hasClock hasClock
+  writeToRamOp (addr, mask, bv) = RamWrite addr (mask, bv)
+
+{-# OPAQUE receiveRingbuffer #-}
+receiveRingbuffer ::
   forall dom aw memDepth.
   ( HasCallStack
   , HiddenClockResetEnable dom
@@ -87,15 +113,20 @@ receiveRingbufferWb ::
     , CSignal dom (BitVector 64)
     )
     ()
-receiveRingbufferWb primitive SNat = circuit $ \(wb, Fwd frames) -> do
-  let
-    writeAddress = register (0 :: Index memDepth) $ fmap (satSucc SatWrap) writeAddress
-    writes = fmap Just $ bundle (writeAddress, frames)
-  [wb0] <-
-    deviceWb (deviceConfig "ReceiveRingbuffer")
+receiveRingbuffer primitive SNat = circuit $ \(wb, Fwd frames) -> do
+  Fwd writeAddress <- writeCounter -< clearAtCount
+  let writes = mux receiveEnable (fmap Just $ bundle (writeAddress, frames)) (pure Nothing)
+  [wb0, wb1, wb2] <-
+    deviceWbI (deviceConfig "ReceiveRingbuffer"){registered = False}
       <| fmapC
-        (matchEndianness <| Wb.increaseBuswidth d1)
+        (Wb.increaseBuswidth d1)
       -< wb
+  (Fwd (receiveEnable, _)) <-
+    registerWbI @_ @_ @8 receiveEnableConfig False -< (wb1, Fwd (pure Nothing))
+
+  ((_clearAtCount, clearAtCount)) <-
+    registerWbDfI @_ @_ @8 (registerConfig "clearAtCount") 0 -< (wb2, Fwd (pure Nothing))
+
   reqresp <- addressableBytesWb @memDepth regConfig -< wb0
   (reads, cpuWrites) <- ReqResp.partitionEithers -< reqresp
   readAddress <- ReqResp.toDfs -< (reads, readData)
@@ -103,5 +134,23 @@ receiveRingbufferWb primitive SNat = circuit $ \(wb, Fwd frames) -> do
   readData <- ram -< (readAddress, Fwd writes)
   idC -< ()
  where
-  regConfig = registerConfig "data"
+  writeCounter :: Circuit (Df dom (BusActivity (Index memDepth))) (CSignal dom (Index memDepth))
+  writeCounter = Circuit (unbundle . mealy goWriteAddress 0 . fst)
+  goWriteAddress current0 clearAt = (next, (Ack clearAck, current0))
+   where
+    next = satSucc SatWrap current1
+    (current1, clearAck)
+      | Just clear <- busActivityWrite clearAt, clear == current0 = (0, True)
+      | otherwise = (current0, False)
+
+  receiveEnableConfig =
+    (registerConfig "enable")
+      { access = WriteOnly
+      , description = "Enable signal for receiving frames from the network"
+      }
+  regConfig =
+    (registerConfig "data")
+      { access = ReadOnly
+      , description = "Buffer that continuously receives frames from the network"
+      }
   ram = withClockResetEnable hasClock hasReset enableGen (Df.fromBlockRam primitive)
