d806753ef5b5410b36da0cd33f7d51d8ce1642db
Lucas Bollen <lucas@qbaylogic.com>
2026-04-16 11:50:12 +0200

    Rename `driver2` to `driver` in SoftUgnDemo

--- bittide-instances/src/Bittide/Instances/Hitl/SoftUgnDemo/Driver.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/SoftUgnDemo/Driver.hs
@@ -18,10 +18,11 @@ import Bittide.Instances.Hitl.SwitchDemo.Driver (
   parseTapInfo,
  )
 import Bittide.Instances.Hitl.Utils.Driver
-import Bittide.Instances.Hitl.Utils.Ugn
-import Control.Concurrent.Async (forConcurrently_, mapConcurrently, mapConcurrently_)
+
+-- import Bittide.Instances.Hitl.Utils.Ugn
+import Control.Concurrent.Async (forConcurrently_, mapConcurrently_)
 import Control.Concurrent.Async.Extra (zipWithConcurrently3_)
-import Control.Monad (forM_, unless, when)
+import Control.Monad (forM_)
 import Control.Monad.IO.Class
 import Data.Vector.Internal.Check (HasCallStack)
 import Project.Chan
@@ -37,6 +38,180 @@ import qualified Bittide.Instances.Hitl.Utils.OpenOcd as Ocd
 import qualified Data.List as L
 import qualified Gdb
 import qualified System.Timeout.Extra as T
+
+-- driver ::
+--   (HasCallStack) =>
+--   String ->
+--   [(HwTarget, DeviceInfo)] ->
+--   VivadoM ExitCode
+-- driver testName targets = do
+--   liftIO
+--     . putStrLn
+--     $ "Running driver function for targets "
+--     <> show ((\(_, info) -> info.deviceId) <$> targets)
+
+--   projectDir <- liftIO $ findParentContaining "cabal.project"
+--   let hitlDir = projectDir </> "_build/hitl" </> testName
+
+--   forM_ targets (assertProbe "probe_test_start")
+
+--   let
+--     -- BOOT / MU / CC IDs
+--     expectedJtagIds = [0x0514C001, 0x1514C001, 0x2514C001]
+--     toInitArgs (_, deviceInfo) targetIndex =
+--       Ocd.InitOpenOcdArgs{deviceInfo, expectedJtagIds, hitlDir, targetIndex}
+--     initArgs = L.zipWith toInitArgs targets [0 ..]
+--     optionalBootInitArgs = L.repeat def{Ocd.logPrefix = "boot-", Ocd.initTcl = "vexriscv_boot_init.tcl"}
+--     openOcdBootStarts = liftIO <$> L.zipWith Ocd.initOpenOcd initArgs optionalBootInitArgs
+
+--   let picocomStarts = liftIO <$> L.zipWith (initPicocom hitlDir) targets [0 ..]
+--   brackets picocomStarts (liftIO . snd) $ \(L.map fst -> picocoms) -> do
+--     -- Start OpenOCD that will program the boot CPU
+--     brackets openOcdBootStarts (liftIO . (.cleanup)) $ \initOcdsData -> do
+--       let bootTapInfos = parseBootTapInfo <$> initOcdsData
+
+--       Gdb.withGdbs (L.length targets) $ \bootGdbs -> do
+--         liftIO
+--           $ zipWithConcurrently3_ (initGdb hitlDir "switch-demo1-boot") bootGdbs bootTapInfos targets
+--         liftIO $ mapConcurrently_ ((assertEither =<<) . Gdb.loadBinary) bootGdbs
+--         liftIO $ mapConcurrently_ Gdb.continue bootGdbs
+--         liftIO
+--           $ T.tryWithTimeout T.PrintActionTime "Waiting for done" 60_000_000
+--           $ forConcurrently_ picocoms
+--           $ \pico ->
+--             waitForLine pico "[BT] Going into infinite loop.."
+
+--   let
+--     optionalInitArgs = L.repeat def
+--     openOcdStarts = liftIO <$> L.zipWith Ocd.initOpenOcd initArgs optionalInitArgs
+
+--   -- Start OpenOCD instances for all CPUs
+--   brackets openOcdStarts (liftIO . (.cleanup)) $ \initOcdsData -> do
+--     let
+--       allTapInfos = parseTapInfo expectedJtagIds <$> initOcdsData
+
+--       _bootTapInfos, muTapInfos, ccTapInfos :: [Ocd.TapInfo]
+--       (_bootTapInfos, muTapInfos, ccTapInfos)
+--         | all (== L.length expectedJtagIds) (L.length <$> allTapInfos)
+--         , [boots, mus, ccs] <- L.transpose allTapInfos =
+--             (boots, mus, ccs)
+--         | otherwise =
+--             error
+--               $ "Unexpected number of OpenOCD taps initialized. Expected: "
+--               <> show (L.length expectedJtagIds)
+--               <> ", but got: "
+--               <> show (L.length <$> allTapInfos)
+
+--     Gdb.withGdbs (L.length targets) $ \ccGdbs -> do
+--       liftIO $ zipWithConcurrently3_ (initGdb hitlDir "clock-control") ccGdbs ccTapInfos targets
+--       liftIO $ mapConcurrently_ ((assertEither =<<) . Gdb.loadBinary) ccGdbs
+
+--       Gdb.withGdbs (L.length targets) $ \muGdbs -> do
+--         liftIO $ zipWithConcurrently3_ (initGdb hitlDir "soft-ugn-mu") muGdbs muTapInfos targets
+--         liftIO $ mapConcurrently_ ((assertEither =<<) . Gdb.loadBinary) muGdbs
+
+--         brackets picocomStarts (liftIO . snd) $ \(L.map fst -> picocoms) -> do
+--           let goDumpCcSamples = dumpCcSamples hitlDir (defCcConf (natToNum @FpgaCount)) ccGdbs
+--           liftIO $ mapConcurrently_ Gdb.continue ccGdbs
+--           liftIO $ mapConcurrently_ Gdb.continue muGdbs
+
+--           hardwareCaptureCounters <-
+--             liftIO
+--               $ T.tryWithTimeoutOn
+--                 T.PrintActionTime
+--                 "Waiting for hardware UGNs"
+--                 (3 * 60_000_000)
+--                 goDumpCcSamples
+--               $ mapConcurrently parseCaptureCounters picocoms
+--           let
+--             hardwareUgns =
+--               L.zipWith
+--                 (\i ccs -> (timingOracleToUgnEdge . counterCaptureToTimingOracle i) <$> ccs)
+--                 [0 ..]
+--                 hardwareCaptureCounters
+--             hardwareRoundtrips = calculateRoundtripLatencies $ L.concat hardwareUgns
+--           _ <- liftIO $ do
+--             putStrLn "\n=== Hardware UGN Roundtrip Latencies ==="
+--           softwareUgnsPerNode <-
+--             liftIO
+--               $ T.tryWithTimeoutOn
+--                 T.PrintActionTime
+--                 "Waiting for software UGNs"
+--                 (60_000_000)
+--                 goDumpCcSamples
+--               $ mapConcurrently parseSoftwareUgns picocoms
+
+--           liftIO $ do
+--             putStrLn "\n=== Hardware UGNs ==="
+--             forM_ (L.zip hardwareUgns [0 :: Int ..]) $ \(hw, idx) ->
+--               putStrLn $ "Node " <> show idx <> ": " <> show (L.length hw) <> " edges"
+
+--             putStrLn "\n=== Software UGNs ==="
+--             forM_ (L.zip softwareUgnsPerNode [0 :: Int ..]) $ \((swIn, swOut), idx) ->
+--               putStrLn
+--                 $ "Node "
+--                 <> show idx
+--                 <> ": "
+--                 <> show (L.length swIn + L.length swOut)
+--                 <> " edges"
+
+--             -- Process UGN edges and calculate roundtrip latencies
+--             let hardwareUgnsFlat = postProcessHardwareUgns hardwareUgns
+--             softwareUgnsFlat <- postProcessSoftwareUgns softwareUgnsPerNode
+
+--             let
+--               swExtraLatency = 2 -- gather read latency + extra link registers
+--               mismatchedUgns =
+--                 findMismatchedUgnEdges
+--                   (fmap (addLatencyEdge swExtraLatency) hardwareUgnsFlat)
+--                   softwareUgnsFlat
+--               softwareRoundtrips = calculateRoundtripLatencies softwareUgnsFlat
+--             unless (L.null mismatchedUgns) $ do
+--               putStrLn "\n=== Mismatched UGN Edges ==="
+--               forM_ mismatchedUgns $ \(hw, sw) -> do
+--                 putStrLn $ "Hardware: " <> show hw
+--                 putStrLn $ "Software: " <> show sw
+
+--             putStrLn "\n=== Software Roundtrip Latencies ==="
+--             mapM_ print softwareRoundtrips
+
+--             -- Compare roundtrip latencies
+--             matched <-
+--               compareRoundtripLatencies
+--                 (fmap (adjustLatencyRoundTrip (2 * swExtraLatency)) hardwareRoundtrips)
+--                 softwareRoundtrips
+
+--             unless matched $ do
+--               putStrLn "\n=== Per-Node Details ==="
+--               forM_ (L.zip3 hardwareUgns softwareUgnsPerNode [0 :: Int ..]) $ \(hw, (swIn, swOut), idx) -> do
+--                 let sw = swIn L.++ swOut
+--                 when (L.length hw /= L.length sw) $ do
+--                   putStrLn $ "\nNode " <> show idx <> " edge count differs:"
+--                   putStrLn $ "  Hardware: " <> show (L.length hw) <> " edges"
+--                   putStrLn
+--                     $ "  Software: "
+--                     <> show (L.length sw)
+--                     <> " edges ("
+--                     <> show (L.length swIn)
+--                     <> " in + "
+--                     <> show (L.length swOut)
+--                     <> " out)"
+--               error "Roundtrip latencies did not match between hardware and software"
+--             when (not $ L.null mismatchedUgns) $ error "Some UGN edges did not match!"
+
+--             liftIO
+--               $ T.tryWithTimeoutOn
+--                 T.PrintActionTime
+--                 "Waiting for CPU test status"
+--                 (1_000_000)
+--                 goDumpCcSamples
+--               $ forConcurrently_ picocoms
+--               $ \pico ->
+--                 waitForLine pico "[MU] Test status: Success"
+
+--           liftIO goDumpCcSamples
+
+--           pure ExitSuccess
 
 driver ::
   (HasCallStack) =>
@@ -68,193 +243,6 @@ driver testName targets = do
     -- Start OpenOCD that will program the boot CPU
     brackets openOcdBootStarts (liftIO . (.cleanup)) $ \initOcdsData -> do
       let bootTapInfos = parseBootTapInfo <$> initOcdsData
-
-      Gdb.withGdbs (L.length targets) $ \bootGdbs -> do
-        liftIO
-          $ zipWithConcurrently3_ (initGdb hitlDir "switch-demo1-boot") bootGdbs bootTapInfos targets
-        liftIO $ mapConcurrently_ ((assertEither =<<) . Gdb.loadBinary) bootGdbs
-        liftIO $ mapConcurrently_ Gdb.continue bootGdbs
-        liftIO
-          $ T.tryWithTimeout T.PrintActionTime "Waiting for done" 60_000_000
-          $ forConcurrently_ picocoms
-          $ \pico ->
-            waitForLine pico "[BT] Going into infinite loop.."
-
-  let
-    optionalInitArgs = L.repeat def
-    openOcdStarts = liftIO <$> L.zipWith Ocd.initOpenOcd initArgs optionalInitArgs
-
-  -- Start OpenOCD instances for all CPUs
-  brackets openOcdStarts (liftIO . (.cleanup)) $ \initOcdsData -> do
-    let
-      allTapInfos = parseTapInfo expectedJtagIds <$> initOcdsData
-
-      _bootTapInfos, muTapInfos, ccTapInfos :: [Ocd.TapInfo]
-      (_bootTapInfos, muTapInfos, ccTapInfos)
-        | all (== L.length expectedJtagIds) (L.length <$> allTapInfos)
-        , [boots, mus, ccs] <- L.transpose allTapInfos =
-            (boots, mus, ccs)
-        | otherwise =
-            error
-              $ "Unexpected number of OpenOCD taps initialized. Expected: "
-              <> show (L.length expectedJtagIds)
-              <> ", but got: "
-              <> show (L.length <$> allTapInfos)
-
-    Gdb.withGdbs (L.length targets) $ \ccGdbs -> do
-      liftIO $ zipWithConcurrently3_ (initGdb hitlDir "clock-control") ccGdbs ccTapInfos targets
-      liftIO $ mapConcurrently_ ((assertEither =<<) . Gdb.loadBinary) ccGdbs
-
-      Gdb.withGdbs (L.length targets) $ \muGdbs -> do
-        liftIO $ zipWithConcurrently3_ (initGdb hitlDir "soft-ugn-mu") muGdbs muTapInfos targets
-        liftIO $ mapConcurrently_ ((assertEither =<<) . Gdb.loadBinary) muGdbs
-
-        brackets picocomStarts (liftIO . snd) $ \(L.map fst -> picocoms) -> do
-          let goDumpCcSamples = dumpCcSamples hitlDir (defCcConf (natToNum @FpgaCount)) ccGdbs
-          liftIO $ mapConcurrently_ Gdb.continue ccGdbs
-          liftIO $ mapConcurrently_ Gdb.continue muGdbs
-
-          hardwareCaptureCounters <-
-            liftIO
-              $ T.tryWithTimeoutOn
-                T.PrintActionTime
-                "Waiting for hardware UGNs"
-                (3 * 60_000_000)
-                goDumpCcSamples
-              $ mapConcurrently parseCaptureCounters picocoms
-          let
-            hardwareUgns =
-              L.zipWith
-                (\i ccs -> (timingOracleToUgnEdge . counterCaptureToTimingOracle i) <$> ccs)
-                [0 ..]
-                hardwareCaptureCounters
-            hardwareRoundtrips = calculateRoundtripLatencies $ L.concat hardwareUgns
-          _ <- liftIO $ do
-            putStrLn "\n=== Hardware UGN Roundtrip Latencies ==="
-          softwareUgnsPerNode <-
-            liftIO
-              $ T.tryWithTimeoutOn
-                T.PrintActionTime
-                "Waiting for software UGNs"
-                (60_000_000)
-                goDumpCcSamples
-              $ mapConcurrently parseSoftwareUgns picocoms
-
-          liftIO $ do
-            putStrLn "\n=== Hardware UGNs ==="
-            forM_ (L.zip hardwareUgns [0 :: Int ..]) $ \(hw, idx) ->
-              putStrLn $ "Node " <> show idx <> ": " <> show (L.length hw) <> " edges"
-
-            putStrLn "\n=== Software UGNs ==="
-            forM_ (L.zip softwareUgnsPerNode [0 :: Int ..]) $ \((swIn, swOut), idx) ->
-              putStrLn
-                $ "Node "
-                <> show idx
-                <> ": "
-                <> show (L.length swIn + L.length swOut)
-                <> " edges"
-
-            -- Process UGN edges and calculate roundtrip latencies
-            let hardwareUgnsFlat = postProcessHardwareUgns hardwareUgns
-            softwareUgnsFlat <- postProcessSoftwareUgns softwareUgnsPerNode
-
-            let
-              swExtraLatency = 2 -- gather read latency + extra link registers
-              mismatchedUgns =
-                findMismatchedUgnEdges
-                  (fmap (addLatencyEdge swExtraLatency) hardwareUgnsFlat)
-                  softwareUgnsFlat
-              softwareRoundtrips = calculateRoundtripLatencies softwareUgnsFlat
-            unless (L.null mismatchedUgns) $ do
-              putStrLn "\n=== Mismatched UGN Edges ==="
-              forM_ mismatchedUgns $ \(hw, sw) -> do
-                putStrLn $ "Hardware: " <> show hw
-                putStrLn $ "Software: " <> show sw
-
-            putStrLn "\n=== Software Roundtrip Latencies ==="
-            mapM_ print softwareRoundtrips
-
-            -- Compare roundtrip latencies
-            matched <-
-              compareRoundtripLatencies
-                (fmap (adjustLatencyRoundTrip (2 * swExtraLatency)) hardwareRoundtrips)
-                softwareRoundtrips
-
-            unless matched $ do
-              putStrLn "\n=== Per-Node Details ==="
-              forM_ (L.zip3 hardwareUgns softwareUgnsPerNode [0 :: Int ..]) $ \(hw, (swIn, swOut), idx) -> do
-                let sw = swIn L.++ swOut
-                when (L.length hw /= L.length sw) $ do
-                  putStrLn $ "\nNode " <> show idx <> " edge count differs:"
-                  putStrLn $ "  Hardware: " <> show (L.length hw) <> " edges"
-                  putStrLn
-                    $ "  Software: "
-                    <> show (L.length sw)
-                    <> " edges ("
-                    <> show (L.length swIn)
-                    <> " in + "
-                    <> show (L.length swOut)
-                    <> " out)"
-              error "Roundtrip latencies did not match between hardware and software"
-            when (not $ L.null mismatchedUgns) $ error "Some UGN edges did not match!"
-
-            liftIO
-              $ T.tryWithTimeoutOn
-                T.PrintActionTime
-                "Waiting for CPU test status"
-                (1_000_000)
-                goDumpCcSamples
-              $ forConcurrently_ picocoms
-              $ \pico ->
-                waitForLine pico "[MU] Test status: Success"
-
-          liftIO goDumpCcSamples
-
-          pure ExitSuccess
-
-driver2 ::
-  (HasCallStack) =>
-  String ->
-  [(HwTarget, DeviceInfo)] ->
-  VivadoM ExitCode
-driver2 testName targets = do
-  liftIO
-    . putStrLn
-    $ "Running driver function for targets "
-    <> show ((\(_, info) -> info.deviceId) <$> targets)
-
-  projectDir <- liftIO $ findParentContaining "cabal.project"
-  let hitlDir = projectDir </> "_build/hitl" </> testName
-
-  forM_ targets (assertProbe "probe_test_start")
-
-  let
-    -- BOOT / MU / CC IDs
-    expectedJtagIds = [0x0514C001, 0x1514C001, 0x2514C001]
-    toInitArgs (_, deviceInfo) targetIndex =
-      Ocd.InitOpenOcdArgs{deviceInfo, expectedJtagIds, hitlDir, targetIndex}
-    initArgs = L.zipWith toInitArgs targets [0 ..]
-    optionalBootInitArgs = L.repeat def{Ocd.logPrefix = "boot-", Ocd.initTcl = "vexriscv_boot_init.tcl"}
-    openOcdBootStarts = liftIO <$> L.zipWith Ocd.initOpenOcd initArgs optionalBootInitArgs
-
-  let picocomStarts = liftIO <$> L.zipWith (initPicocom hitlDir) targets [0 ..]
-  brackets picocomStarts (liftIO . snd) $ \(L.map fst -> picocoms) -> do
-    -- Start OpenOCD that will program the boot CPU
-    brackets openOcdBootStarts (liftIO . (.cleanup)) $ \initOcdsData -> do
-      let
-        allTapInfos = parseTapInfo expectedJtagIds <$> initOcdsData
-
-        bootTapInfos, _muTapInfos, _ccTapInfos :: [Ocd.TapInfo]
-        (bootTapInfos, _muTapInfos, _ccTapInfos)
-          | all (== L.length expectedJtagIds) (L.length <$> allTapInfos)
-          , [boots, mus, ccs] <- L.transpose allTapInfos =
-              (boots, mus, ccs)
-          | otherwise =
-              error
-                $ "Unexpected number of OpenOCD taps initialized. Expected: "
-                <> show (L.length expectedJtagIds)
-                <> ", but got: "
-                <> show (L.length <$> allTapInfos)
 
       Gdb.withGdbs (L.length targets) $ \bootGdbs -> do
         liftIO
