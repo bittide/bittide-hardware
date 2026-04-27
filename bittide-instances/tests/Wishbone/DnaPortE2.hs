48d22405934f4ac1783bf17547b4b9f4b059a485
Ryan Slawson <ryan@qbaylogic.com>
2026-04-27 09:34:34 +0200

    Remove `unsafePerformIO` from DNA test

--- bittide-instances/tests/Wishbone/DnaPortE2.hs
+++ bittide-instances/tests/Wishbone/DnaPortE2.hs
@@ -17,16 +17,13 @@ import Project.FilePath
 import Protocols
 import Protocols.Idle
 import Protocols.MemoryMap
-import System.FilePath
-import System.IO.Unsafe (unsafePerformIO)
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.TH
 import VexRiscv (DumpVcd (NoDumpVcd))
 
-import Bittide.DoubleBufferedRam
+import Bittide.Instances.Common (PeConfigElfSource (NameOnly), peConfigFromElf)
 import Bittide.ProcessingElement
-import Bittide.ProcessingElement.Util
 import Bittide.SharedTypes (withLittleEndian)
 import Bittide.Wishbone
 
@@ -34,23 +31,30 @@ import qualified Bittide.Cpus.Riscv32imc as Riscv32imc
 import qualified Prelude as P
 
 sim :: IO ()
-sim = putStr simResult
+sim = do
+  peConfig <- peConfigSim
+  putStr $ simResult peConfig
 
-simResult :: String
-simResult = chr . fromIntegral <$> catMaybes uartStream
+simResult :: PeConfig 4 -> String
+simResult peConfig = chr . fromIntegral <$> catMaybes uartStream
  where
-  uartStream = sampleC def $ withClockResetEnable clockGen (resetGenN d2) enableGen $ dut @System
+  uartStream =
+    sampleC def
+      $ withClockResetEnable clockGen (resetGenN d2) enableGen
+      $ dut @System peConfig
 
 -- | Test whether we can read the DNA from the DNA port peripheral.
 case_dna_port_self_test :: Assertion
-case_dna_port_self_test = assertBool msg (receivedDna == simDna2)
- where
-  msg =
-    "Received dna "
-      <> showHex receivedDna ""
-      <> " not equal to expected dna "
-      <> showHex simDna2 ""
-  receivedDna = parseResult simResult
+case_dna_port_self_test = do
+  peConfig <- peConfigSim
+  let
+    msg =
+      "Received dna "
+        <> showHex receivedDna ""
+        <> " not equal to expected dna "
+        <> showHex simDna2 ""
+    receivedDna = parseResult $ simResult peConfig
+  assertBool msg (receivedDna == simDna2)
 
 {- | A simple instance containing just VexRisc with UART and the DNA peripheral which
 runs the `dna_port_e2_test` binary from `firmware-binaries`.
@@ -58,8 +62,9 @@ runs the `dna_port_e2_test` binary from `firmware-binaries`.
 dut ::
   forall dom.
   (HiddenClockResetEnable dom) =>
+  PeConfig 4 ->
   Circuit () (Df dom (BitVector 8))
-dut = withLittleEndian $ circuit $ \_unit -> do
+dut peConfig = withLittleEndian $ circuit $ \_unit -> do
   (uartRx, jtag) <- idleSource
   [uartBus, dnaBus] <-
     processingElement @dom NoDumpVcd peConfig -< (mm, jtag)
@@ -67,26 +72,22 @@ dut = withLittleEndian $ circuit $ \_unit -> do
   mm <- ignoreMM
   _dna <- readDnaPortE2Wb simDna2 -< dnaBus
   idC -< uartTx
- where
-  peConfig = unsafePerformIO $ do
-    root <- findParentContaining "cabal.project"
-    let elfPath = root </> firmwareBinariesDir "riscv32imc" Release </> "dna_port_e2_test"
-    (iMem, dMem) <- vecsFromElf @IMemWords @DMemWords elfPath Nothing
-    pure
-      PeConfig
-        { cpu = Riscv32imc.vexRiscv0
-        , depthI = SNat @IMemWords
-        , depthD = SNat @DMemWords
-        , initI = Just (Vec iMem)
-        , initD = Just (Vec dMem)
-        , iBusTimeout = d0 -- No timeouts on the instruction bus
-        , dBusTimeout = d0 -- No timeouts on the data bus
-        , includeIlaWb = False
-        }
 {-# OPAQUE dut #-}
 
 type IMemWords = DivRU (4 * 1024) 4
 type DMemWords = DivRU (4 * 1024) 4
+
+peConfigSim :: IO (PeConfig 4)
+peConfigSim =
+  peConfigFromElf
+    (SNat @IMemWords)
+    (SNat @DMemWords)
+    (NameOnly "dna_port_e2_test")
+    Release
+    d0
+    d0
+    False
+    Riscv32imc.vexRiscv0
 
 parseResult :: String -> BitVector 96
 parseResult = pack . (read :: String -> Unsigned 96) . P.head . lines
