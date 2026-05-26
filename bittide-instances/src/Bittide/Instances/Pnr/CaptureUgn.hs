826f7c9b3c8348a6f8cbbd9746690bbd2667b9ca
Hidde Moll <hidde@qbaylogic.com>
2026-05-26 10:05:13 +0200

    Add high-frequency pnr targets for `captureUgn` and `sendUgn`

---
+++ bittide-instances/src/Bittide/Instances/Pnr/CaptureUgn.hs
@@ -0,0 +1,36 @@
+-- SPDX-FileCopyrightText: 2026 Google LLC
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+module Bittide.Instances.Pnr.CaptureUgn where
+
+import Clash.Prelude
+import Protocols
+
+import Bittide.Instances.Domains (Basic400)
+import Bittide.Instances.Hacks (reducePins)
+import Bittide.SharedTypes (withLittleEndian)
+import Protocols.MemoryMap (unMemmap)
+
+import qualified Bittide.CaptureUgn as Ugn
+
+captureUgnFast ::
+  Clock Basic400 -> Reset Basic400 -> Signal Basic400 Bit -> Signal Basic400 Bit
+captureUgnFast clk rst = withClock clk $ reducePins dut
+ where
+  dut (unbundle -> (localCounter, linkIn, wbIn)) =
+    bundle
+      $ toSignals
+        ( unMemmap
+            $ withLittleEndian
+            $ withClockResetEnable clk rst enableGen
+            $ Ugn.captureUgn @_ @32 localCounter linkIn
+        )
+        (wbIn, ())
+
+sendUgnFast ::
+  Clock Basic400 -> Reset Basic400 -> Signal Basic400 Bit -> Signal Basic400 Bit
+sendUgnFast clk rst = withClock clk $ reducePins dut
+ where
+  dut (unbundle -> (localCounter, sampling, linkIn)) =
+    withClockResetEnable clk rst enableGen $ Ugn.sendUgn localCounter sampling linkIn
--- bittide-shake/exe/Main.hs
+++ bittide-shake/exe/Main.hs
@@ -183,7 +183,9 @@ targets =
     , defTarget $ mkName "Bittide.Instances.Pnr.Si539xSpi.si5391Spi"
     , defTarget $ mkName "Bittide.Instances.Pnr.Synchronizer.safeDffSynchronizer"
     , -- High-frequency Pnr targets
-      defTarget $ mkName "Bittide.Instances.Pnr.Counter.counterFast"
+      defTarget $ mkName "Bittide.Instances.Pnr.CaptureUgn.captureUgnFast"
+    , defTarget $ mkName "Bittide.Instances.Pnr.CaptureUgn.sendUgnFast"
+    , defTarget $ mkName "Bittide.Instances.Pnr.Counter.counterFast"
     , defTarget $ mkName "Bittide.Instances.Pnr.Freeze.freezeFast"
     ]
       <> (testTarget <$> Bittide.Instances.Hitl.Tests.hitlTests)
