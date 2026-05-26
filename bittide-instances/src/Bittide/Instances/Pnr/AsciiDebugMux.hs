5ef3f246d3c326cf8d03b998f4cf910b9ae622d1
Hidde Moll <hidde@qbaylogic.com>
2026-05-26 11:52:25 +0200

    Add high-frequency pnr target for `asciiDebugMux`

--- bittide-instances/src/Bittide/Instances/Pnr/AsciiDebugMux.hs
+++ bittide-instances/src/Bittide/Instances/Pnr/AsciiDebugMux.hs
@@ -7,17 +7,38 @@ module Bittide.Instances.Pnr.AsciiDebugMux where
 import Clash.Prelude
 import Protocols
 
+import Bittide.Instances.Domains (Basic400)
+import Bittide.Instances.Hacks (reducePins)
 import Bittide.SharedTypes (Byte)
+import Data.Bifunctor (first)
 
 import qualified Bittide.Df as Df
+
+asciiDebugMuxExample ::
+  (KnownDomain dom) =>
+  Clock dom ->
+  Reset dom ->
+  Enable dom ->
+  Circuit (Vec 3 (Df dom Byte)) (Df dom Byte)
+asciiDebugMuxExample clk rst ena =
+  withClockResetEnable clk rst ena
+    $ Df.asciiDebugMux
+      (SNat @1024)
+      ((0xA :> Nil) :> (0xB :> Nil) :> (0xC :> Nil) :> Nil)
 
 asciiDebugMux ::
   Clock System ->
   Reset System ->
   Enable System ->
   Circuit (Vec 3 (Df System Byte)) (Df System Byte)
-asciiDebugMux clk rst ena =
-  withClockResetEnable @System clk rst ena
-    $ Df.asciiDebugMux
-      (SNat @1024)
-      ((0xA :> Nil) :> (0xB :> Nil) :> (0xC :> Nil) :> Nil)
+asciiDebugMux = asciiDebugMuxExample
+
+asciiDebugMuxFast :: Clock Basic400 -> Reset Basic400 -> Signal Basic400 Bit -> Signal Basic400 Bit
+asciiDebugMuxFast clk rst = withClock clk $ reducePins dut
+ where
+  dut (unbundle -> (inps, ack)) =
+    bundle
+      $ first bundle
+      $ toSignals
+        (asciiDebugMuxExample clk rst enableGen)
+        (unbundle inps, ack)
