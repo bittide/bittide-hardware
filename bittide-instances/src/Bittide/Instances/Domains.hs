e85c90620bb95936bb383fdd4c3d1cf07a4d2a29
Hidde Moll <hidde@qbaylogic.com>
2026-06-08 10:16:07 +0200

    Decrease frequency of fast pnr `transmitRingBuffer` from 350 to 340 MHz
    The `transmitRingBufferFast` pnr test did run at 350 MHz when it was added, but
    does not anymore. Instead of decreasing its frequency to 300 MHz, a step of
    50 MHz like the other targets, we decrease it with 10 MHz. This is the maximum
    frequency this component would consistently run at (over 10 runs).
    
    This is a temporary solution until we find why the component no longer runs at
    350 MHz.

--- bittide-instances/src/Bittide/Instances/Domains.hs
+++ bittide-instances/src/Bittide/Instances/Domains.hs
@@ -15,6 +15,7 @@ createDomain vXilinxSystem{vName="Basic125B", vPeriod=hzToPeriod 125e6}
 createDomain vXilinxSystem{vName="Basic199",  vPeriod=hzToPeriod 199e6}
 createDomain vXilinxSystem{vName="Basic200",  vPeriod=hzToPeriod 200e6}
 createDomain vXilinxSystem{vName="Basic300",  vPeriod=hzToPeriod 300e6}
+createDomain vXilinxSystem{vName="Basic340",  vPeriod=hzToPeriod 340e6}
 createDomain vXilinxSystem{vName="Basic350",  vPeriod=hzToPeriod 350e6}
 createDomain vXilinxSystem{vName="Basic400",  vPeriod=hzToPeriod 400e6}
 createDomain vXilinxSystem{vName="Basic625",  vPeriod=hzToPeriod 625e6, vResetKind=Asynchronous}
