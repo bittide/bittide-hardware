d76cbc55e1b2ae7f44d2bd0f39f245bd1e8d7aaf
Martijn Bastiaan <martijn@qbaylogic.com>
2026-06-12 16:57:53 +0200

    Use `sport`

--- bittide-extra/tests/doctests/Main.hs
+++ bittide-extra/tests/doctests/Main.hs
@@ -5,19 +5,10 @@
 module Main where
 
 import System.Environment (getArgs)
-import Test.DocTest (mainFromLibrary)
-import Test.DocTest.Helpers (Library (..), extractCabalLibrary, findCabalPackage)
-
-import qualified Distribution.ModuleName as ModuleName
+import Test.DocTest (mainFromCabal)
 
 main :: IO ()
 main = do
-  lib <- extractCabalLibrary =<< findCabalPackage "bittide-extra"
-  -- 'System.Hardware.Serial' is a .hsc file; doctest-parallel resolves modules
-  -- to .hs sources and can't find it (the hsc2hs output lives in dist-newstyle).
-  -- It has no doctests anyway, so drop it.
-  let lib' = lib{libModules = filter (/= serialModule) (libModules lib)}
-      serialModule = ModuleName.fromString "System.Hardware.Serial"
   -- We use Nix to setup tooling, not to provide GHC packages so we need to set --no-nix
   args <- getArgs
-  mainFromLibrary lib' ("--no-nix" : args)
+  mainFromCabal "bittide-extra" ("--no-nix" : args)
