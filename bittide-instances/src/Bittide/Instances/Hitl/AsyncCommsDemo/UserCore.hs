5eed24b02384613d89cc0da8c0344d31a2bc4621
Lucas Bollen <lucas@qbaylogic.com>
2026-06-12 16:43:24 +0200

    Allow configuring management unit configuration per demo One demo needs significantly larger memories, this way we dont have to do that for each demo

--- bittide-instances/src/Bittide/Instances/Hitl/AsyncCommsDemo/UserCore.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/AsyncCommsDemo/UserCore.hs
@@ -8,12 +8,17 @@ module Bittide.Instances.Hitl.AsyncCommsDemo.UserCore (
   RingBufferDepth,
   ringBufferDepth,
   mkUserCore,
+  muConfig,
 ) where
 
 import Clash.Explicit.Prelude
 import Protocols
 
 import Bittide.Instances.Hitl.GenericDemo.BringUp (NmuRemBusWidth, UserCoreCircuit)
+import Bittide.Instances.Hitl.GenericDemo.Core (NmuInternalBusses)
+import Bittide.ProcessingElement (PeConfig (..), PrefixWidth)
+
+import qualified Bittide.Cpus.Riscv32imc as Riscv32imc
 
 type UserCoreBusses = 0
 
@@ -21,6 +26,28 @@ type RingBufferDepth = 128
 
 ringBufferDepth :: SNat RingBufferDepth
 ringBufferDepth = SNat
+
+{- | Management unit configuration for the async comms demo. Its firmware is
+much larger than that of the other demos (it links in smoltcp), so it needs
+more memory than the default 'Bittide.Instances.Hitl.GenericDemo.Core.muConfig'
+provides.
+-}
+muConfig ::
+  ( KnownNat n
+  , PrefixWidth (n + NmuInternalBusses) <= 30
+  ) =>
+  PeConfig (n + NmuInternalBusses)
+muConfig =
+  PeConfig
+    { cpu = Riscv32imc.vexRiscv1
+    , depthI = SNat @(Div (10 * 16 * 1024) 4) -- One RAMB18E2 is 16KB, this uses 10 of them.
+    , depthD = SNat @(Div (10 * 16 * 1024) 4) -- One RAMB18E2 is 16KB, this uses 10 of them.
+    , initI = Nothing
+    , initD = Nothing
+    , iBusTimeout = d0
+    , dBusTimeout = d0
+    , includeIlaWb = False
+    }
 
 mkUserCore :: UserCoreCircuit UserCoreBusses (NmuRemBusWidth UserCoreBusses)
 mkUserCore _bitClk _bitRst _bitEna _localCounter _maybeDna =
--- bittide-instances/src/Bittide/Instances/Hitl/SoftUgnDemo/MemoryMaps.hs
+++ bittide-instances/src/Bittide/Instances/Hitl/SoftUgnDemo/MemoryMaps.hs
@@ -4,9 +4,10 @@
 
 module Bittide.Instances.Hitl.SoftUgnDemo.MemoryMaps where
 
+import Bittide.Instances.Hitl.GenericDemo.Core (muConfig)
 import Bittide.Instances.Hitl.GenericDemo.MemoryMaps (extractMemoryMaps)
 import Bittide.Instances.Hitl.SoftUgnDemo.UserCore (mkUserCore, ringBufferDepth)
 import Protocols.MemoryMap (MemoryMap)
 
 boot, managementUnit, clockControl :: MemoryMap
-(boot, managementUnit, clockControl) = extractMemoryMaps ringBufferDepth mkUserCore
+(boot, managementUnit, clockControl) = extractMemoryMaps ringBufferDepth muConfig mkUserCore
