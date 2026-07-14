bea4b627b2daaba2009d52e73b7c709d859094e8
Martijn Bastiaan <martijn@qbaylogic.com>
2026-07-14 20:53:38 +0200

    Bump Clash to `perf/more-minor-patches`

--- bittide/src/Bittide/ClockControl/Si539xSpi.hs
+++ bittide/src/Bittide/ClockControl/Si539xSpi.hs
@@ -310,7 +310,7 @@ si539xSpi
       _ -> False
 
     romOut = rom (configPreamble ++ config ++ configPostamble) romAddress
-    romAddress = bitCoerce . getStateAddress <$> configState
+    romAddress = getStateAddress <$> configState
 
     (configState, spiOperation, configBusy, configByte) =
       mealyB go (WaitForReady False) (romOut, externalOperation, driverByte, driverBusy)
